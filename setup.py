6ae6ea931639b2a390dc6393b7288470736444f1
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-10-18 00:59:15 +0000

    add empty zcml files
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@25721 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -7,36 +7,31 @@ entry_points = {
 }
 
 setup(
-    name = 'nti.app.products.courseware',
-    version = VERSION,
-    author = 'Jason Madden',
-    author_email = 'jason@nextthought.com',
-    description = "Umbrella product to support integrated courseware",
-    long_description = codecs.open('README.rst', encoding='utf-8').read(),
-    license = 'Proprietary',
-    keywords = 'pyramid preference',
-    #url = 'https://github.com/NextThought/nti.nose_traceback_info',
-    classifiers = [
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
+	name='nti.app.products.courseware',
+	version=VERSION,
+	author='Jason Madden',
+	author_email='jason@nextthought.com',
+	description="Umbrella product to support integrated courseware",
+	long_description=codecs.open('README.rst', encoding='utf-8').read(),
+	license='Proprietary',
+	keywords='pyramid preference',
+	# url = 'https://github.com/NextThought/nti.nose_traceback_info',
+	classifiers=[
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
 		'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 2.7',
 		'Programming Language :: Python :: 3',
 		'Programming Language :: Python :: 3.3',
-        'Topic :: Software Development :: Testing'
+		'Topic :: Software Development :: Testing'
 		'Framework :: Pyramid',
-        ],
+		],
 	packages=find_packages('src'),
 	package_dir={'': 'src'},
 	namespace_packages=['nti', 'nti.app', 'nti.app.products'],
 	install_requires=[
 		'setuptools',
-		# NOTE: We actually depend on nti.dataserver
-		# as well, but for the sake of legacy
-		# deployments, we do not yet declare that.
-		# We will declare it when everything is in
-		# buildout
 	],
 	entry_points=entry_points
 )
