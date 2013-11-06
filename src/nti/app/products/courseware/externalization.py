7455c3a3cb0870151a2f29057ab672794ffa7252
Jason Madden <jason.madden@nextthought.com>
2013-11-06 19:13:09 +0000

    and for courseware
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@26700 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/externalization.py
+++ src/nti/app/products/courseware/externalization.py
@@ -10,21 +10,3 @@ from __future__ import print_function, unicode_literals, absolute_import, divisi
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
-
-from nti.externalization.autopackage import AutoPackageSearchingScopedInterfaceObjectIO
-
-from . import interfaces
-
-class _CoursewareObjectIO(AutoPackageSearchingScopedInterfaceObjectIO):
-
-	@classmethod
-	def _ap_enumerate_externalizable_root_interfaces(cls, cwr_interfaces):
-		return (cwr_interfaces.ICourseCatalogEntry,
-				cwr_interfaces.ICourseCatalogInstructorInfo,
-				cwr_interfaces.ICourseCreditLegacyInfo)
-
-	@classmethod
-	def _ap_enumerate_module_names( cls ):
-		return ('catalog', 'legacy')
-
-_CoursewareObjectIO.__class_init__()
