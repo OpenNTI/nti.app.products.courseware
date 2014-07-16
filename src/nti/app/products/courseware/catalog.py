3c28ac79e4a6889faa5aa0b372cd801e65d8f6d5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-07-16 22:10:00 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@43354 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/catalog.py
+++ src/nti/app/products/courseware/catalog.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Implementations of course catalogs.
-
 .. $Id$
 """
 
@@ -10,31 +8,6 @@ from __future__ import print_function, unicode_literals, absolute_import, divisi
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
-
-from functools import total_ordering
-
-from zope import interface
-
-from nti.contenttypes.courses.interfaces import ICourseInstance
-
-from nti.dataserver.links import Link
-from nti.dataserver.authorization import ACT_READ
-from nti.dataserver.authorization_acl import ace_allowing
-from nti.dataserver.authorization_acl import acl_from_aces
-from nti.dataserver.interfaces import AUTHENTICATED_GROUP_NAME
-from nti.dataserver.containers import CheckingLastModifiedBTreeContainer
-
-from nti.externalization.persistence import NoPickle
-from nti.externalization.externalization import WithRepr
-
-from nti.schema.schema import EqHash
-from nti.schema.fieldproperty import createDirectFieldProperties
-from nti.schema.schema import PermissiveSchemaConfigured as SchemaConfigured
-
-from nti.utils.property import alias
-from nti.utils.property import LazyOnClass
-
-from . import interfaces
 
 import zope.deferredimport
 zope.deferredimport.initialize()
