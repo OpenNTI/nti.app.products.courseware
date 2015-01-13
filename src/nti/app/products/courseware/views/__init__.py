5a6b210e12f57a9ab5334293ad6d7221c2a23c44
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-13 18:10:13 +0000

    add a path adpapter for Course admin views, but don't use it yet
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@57521 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/views/__init__.py
+++ src/nti/app/products/courseware/views/__init__.py
@@ -10,16 +10,31 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 from .. import MessageFactory
+
+from zope import interface
+from zope.container.contained import Contained
+from zope.traversing.interfaces import IPathAdapter
+
 from .. import VIEW_CONTENTS
-from .. import VIEW_COURSE_ACTIVITY
 from .. import VIEW_CATALOG_ENTRY
+from .. import VIEW_COURSE_ACTIVITY
 from .. import VIEW_COURSE_RECURSIVE
-from .. import VIEW_COURSE_ENROLLMENT_ROSTER
 from .. import VIEW_COURSE_RECURSIVE_BUCKET
+from .. import VIEW_COURSE_ENROLLMENT_ROSTER
 
 VIEW_CONTENTS = VIEW_CONTENTS
-VIEW_COURSE_ACTIVITY = VIEW_COURSE_ACTIVITY
 VIEW_CATALOG_ENTRY = VIEW_CATALOG_ENTRY
+VIEW_COURSE_ACTIVITY = VIEW_COURSE_ACTIVITY
 VIEW_COURSE_RECURSIVE = VIEW_COURSE_RECURSIVE
-VIEW_COURSE_ENROLLMENT_ROSTER = VIEW_COURSE_ENROLLMENT_ROSTER
 VIEW_COURSE_RECURSIVE_BUCKET = VIEW_COURSE_RECURSIVE_BUCKET
+VIEW_COURSE_ENROLLMENT_ROSTER = VIEW_COURSE_ENROLLMENT_ROSTER
+
+@interface.implementer(IPathAdapter)
+class CourseAdminPathAdapter(Contained):
+
+    __name__ = 'CourseAdmin'
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.__parent__ = context
