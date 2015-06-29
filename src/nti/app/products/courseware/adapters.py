53f33d3a201e4c041596e6f339d872d85ba8a924
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-06-29 13:22:02 +0000

    remove unused import
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@67877 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/adapters.py
+++ src/nti/app/products/courseware/adapters.py
@@ -9,8 +9,6 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from pyramid.traversal import find_interface
-
 import zope.intid
 from zope import interface
 from zope import component
@@ -47,177 +45,175 @@ from .interfaces import ILegacyCourseConflatedContentPackageUsedAsCourse
 @interface.implementer(IContentPackageBundle)
 @component.adapter(ILegacyCommunityBasedCourseInstance)
 def _legacy_course_to_content_package_bundle(course):
-    return course.ContentPackageBundle
+	return course.ContentPackageBundle
 
 @interface.implementer(IContentPackageBundle)
 @component.adapter(IContentCourseInstance)
 def _course_content_to_package_bundle(course):
-    return course.ContentPackageBundle
+	return course.ContentPackageBundle
 
 @interface.implementer(IContentPackageBundle)
 @component.adapter(ICourseCatalogEntry)
 def _entry_to_content_package_bundle(entry):
-    course = ICourseInstance(entry, None)
-    return IContentPackageBundle(course, None)
+	course = ICourseInstance(entry, None)
+	return IContentPackageBundle(course, None)
 
 @interface.implementer(ICourseInstance)
 @component.adapter(ILegacyCourseConflatedContentPackageUsedAsCourse)
 def _course_content_package_to_course(package):
-    # Both the catalog entry and the content package are supposed to
-    # be non-persistent (in the case we actually get a course) or the
-    # course doesn't exist (in the case that the package is persistent
-    # and installed in a sub-site, which shouldn't happen with this
-    # registration, though could if we used a plain
-    # ConflatedContentPackage), so it should be safe to cache this on
-    # the package. Be extra careful though, just in case.
-    cache_name = '_v_course_content_package_to_course'
-    course_intid = getattr(package, cache_name, cache_name)
-    course = None
-    intids = component.getUtility(zope.intid.IIntIds)
+	# Both the catalog entry and the content package are supposed to
+	# be non-persistent (in the case we actually get a course) or the
+	# course doesn't exist (in the case that the package is persistent
+	# and installed in a sub-site, which shouldn't happen with this
+	# registration, though could if we used a plain
+	# ConflatedContentPackage), so it should be safe to cache this on
+	# the package. Be extra careful though, just in case.
+	cache_name = '_v_course_content_package_to_course'
+	course_intid = getattr(package, cache_name, cache_name)
+	course = None
+	intids = component.getUtility(zope.intid.IIntIds)
 
-    if course_intid is not cache_name:
-        course = intids.queryObject(course_intid)
+	if course_intid is not cache_name:
+		course = intids.queryObject(course_intid)
 
-    if course is not None:
-        return course
+	if course is not None:
+		return course
 
-    # We go via the defined adapter from the catalog entry,
-    # which we should have directly cached
-    try:
-        entry = package._v_global_legacy_catalog_entry
-    except AttributeError:
-        logger.warn("Consistency issue? No attribute on global package %s",
-                    package)
-        entry = None
+	# We go via the defined adapter from the catalog entry,
+	# which we should have directly cached
+	try:
+		entry = package._v_global_legacy_catalog_entry
+	except AttributeError:
+		logger.warn("Consistency issue? No attribute on global package %s",
+					package)
+		entry = None
 
-    course = ICourseInstance(entry, None)
-    course_intid = intids.queryId(course, None)
+	course = ICourseInstance(entry, None)
+	course_intid = intids.queryId(course, None)
 
-    setattr(package, cache_name, course_intid)
-    return course
+	setattr(package, cache_name, course_intid)
+	return course
 
 def _content_unit_to_courses(unit, include_sub_instances=True):
-    # XXX JAM These heuristics aren't well tested.
+	# XXX JAM These heuristics aren't well tested.
 
-    # First, try the true legacy case. This involves
-    # a direct mapping between courses and a catalog entry. It may be
-    # slightly more reliable, but only works for true legacy cases.
-    package = find_interface(unit, ILegacyCourseConflatedContentPackageUsedAsCourse)
-    if package is not None:
-        result = ICourseInstance(package, None)
-        if result is not None:
-            return (result,)
+	# First, try the true legacy case. This involves
+	# a direct mapping between courses and a catalog entry. It may be
+	# slightly more reliable, but only works for true legacy cases.
+	package = find_interface(unit, ILegacyCourseConflatedContentPackageUsedAsCourse)
+	if package is not None:
+		result = ICourseInstance(package, None)
+		if result is not None:
+			return (result,)
 
-    # Nothing true legacy. find all courses that match this pacakge
-    result = []
-    package = find_interface(unit, IContentPackage)
-    course_catalog = component.getUtility(ICourseCatalog)
-    for entry in course_catalog.iterCatalogEntries():
-        instance = ICourseInstance(entry, None)
-        if instance is None:
-            continue
-        if not include_sub_instances and ICourseSubInstance.providedBy(instance):
-            continue
-        try:
-            packages = instance.ContentPackageBundle.ContentPackages
-        except AttributeError:
-            packages = (instance.legacy_content_package,)
+	# Nothing true legacy. find all courses that match this pacakge
+	result = []
+	package = find_interface(unit, IContentPackage)
+	course_catalog = component.getUtility(ICourseCatalog)
+	for entry in course_catalog.iterCatalogEntries():
+		instance = ICourseInstance(entry, None)
+		if instance is None:
+			continue
+		if not include_sub_instances and ICourseSubInstance.providedBy(instance):
+			continue
+		try:
+			packages = instance.ContentPackageBundle.ContentPackages
+		except AttributeError:
+			packages = (instance.legacy_content_package,)
 
-        if package in packages:
-            result.append(instance)
-    return result
+		if package in packages:
+			result.append(instance)
+	return result
 
 @interface.implementer(ICourseInstance)
 @component.adapter(IContentUnit)
 def _content_unit_to_course(unit):
-    # get all courses, don't include sections
-    courses = _content_unit_to_courses(unit, False)
+	# get all courses, don't include sections
+	courses = _content_unit_to_courses(unit, False)
 
-    # XXX: We probably need to check and see who's enrolled
-    # to find the most specific course instance to return?
-    # As it stands, we promise to return only a root course,
-    # not a subinstance (section)
-    # XXX: FIXME: This requires a one-to-one mapping
-    return courses[0] if courses else None
+	# XXX: We probably need to check and see who's enrolled
+	# to find the most specific course instance to return?
+	# As it stands, we promise to return only a root course,
+	# not a subinstance (section)
+	# XXX: FIXME: This requires a one-to-one mapping
+	return courses[0] if courses else None
 
 from .utils import is_course_instructor as is_instructor  # BWC
 
 @interface.implementer(ICourseInstance)
 @component.adapter(IContentUnit, IUser)
 def _content_unit_and_user_to_course(unit, user):
-    # # get all courses
-    courses = _content_unit_to_courses(unit, True)
-    for instance in courses or ():
-        # check enrollment
-        enrollments = ICourseEnrollments(instance)
-        record = enrollments.get_enrollment_for_principal(user)
-        if record is not None:
-            return instance
+	# # get all courses
+	courses = _content_unit_to_courses(unit, True)
+	for instance in courses or ():
+		# check enrollment
+		enrollments = ICourseEnrollments(instance)
+		record = enrollments.get_enrollment_for_principal(user)
+		if record is not None:
+			return instance
 
-        # check role
-        if is_instructor(instance, user):
-            return instance
+		# check role
+		if is_instructor(instance, user):
+			return instance
 
-    # nothing found return first course
-    return courses[0] if courses else None
+	# nothing found return first course
+	return courses[0] if courses else None
 
 @interface.implementer(IJoinableContextProvider)
 @component.adapter(interface.Interface)
-def _catalog_entry_from_container_object( obj ):
-    """
-    Using the container index, look for catalog entries that contain
-    the given object.
-    """
-    courses = _courses_from_container_object( obj )
-    results = set()
-    for course in courses or ():
-        catalog_entry = ICourseCatalogEntry( course, None )
+def _catalog_entry_from_container_object(obj):
+	"""
+	Using the container index, look for catalog entries that contain
+	the given object.
+	"""
+	courses = _courses_from_container_object(obj)
+	results = set()
+	for course in courses or ():
+		catalog_entry = ICourseCatalogEntry(course, None)
 
-        # We only want to add publicly available entries.
-        if         catalog_entry is not None \
-            and is_readable( catalog_entry ):
-            results.add( catalog_entry )
-    return results
+		# We only want to add publicly available entries.
+		if catalog_entry is not None and is_readable(catalog_entry):
+			results.add(catalog_entry)
+	return results
 
 @interface.implementer(ITopLevelContainerContextProvider)
 @component.adapter(interface.Interface)
-def _courses_from_container_object( obj ):
-    """
-    Try to return the preferred best-fit course
-    for the given object.
-    """
-    # 1. Deterministic on obj
-    container_context = IContainerContext( obj, None )
-    if container_context is not None:
-        context_id = container_context.context_id
-        course = find_object_with_ntiid( context_id )
-        if course is not None:
-            return ( course, )
+def _courses_from_container_object(obj):
+	"""
+	Try to return the preferred best-fit course
+	for the given object.
+	"""
+	# 1. Deterministic on obj
+	container_context = IContainerContext(obj, None)
+	if container_context is not None:
+		context_id = container_context.context_id
+		course = find_object_with_ntiid(context_id)
+		if course is not None:
+			return (course,)
 
-    # 2. Check lineage
-    course = find_interface( obj, ICourseInstance, strict=False )
-    if course is not None:
-        return ( course, )
+	# 2. Check lineage
+	course = find_interface(obj, ICourseInstance, strict=False)
+	if course is not None:
+		return (course,)
 
-    # 3. Ok, perhaps a contained object; use our index. We
-    # could attempt to return the best fit, according to enrollment etc.
-    catalog = get_catalog()
-    results = set()
-    container_id = getattr( obj, 'containerId', None )
-    if      catalog is not None \
-        and container_id:
-        obj = find_object_with_ntiid( container_id )
-        containers = catalog.get_containers( obj )
-        for container in containers:
-            container = find_object_with_ntiid( container )
-            course = ICourseInstance( container, None )
-            if course is not None:
-                results.add( course )
+	# 3. Ok, perhaps a contained object; use our index. We
+	# could attempt to return the best fit, according to enrollment etc.
+	catalog = get_catalog()
+	results = set()
+	container_id = getattr(obj, 'containerId', None)
+	if catalog is not None and container_id:
+		obj = find_object_with_ntiid(container_id)
+		containers = catalog.get_containers(obj)
+		for container in containers:
+			container = find_object_with_ntiid(container)
+			course = ICourseInstance(container, None)
+			if course is not None:
+				results.add(course)
 
-    # 4. Adapt
-    if not results:
-        course = ICourseInstance( obj, None )
-        if course:
-            results.add( course )
+	# 4. Adapt
+	if not results:
+		course = ICourseInstance(obj, None)
+		if course:
+			results.add(course)
 
-    return results
+	return results
