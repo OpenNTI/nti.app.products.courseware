d6acb2594e433974603f01a713abd7c2b0d7ba89
Josh Zuech <josh.zuech@nextthought.com>
2015-07-13 07:37:31 +0000

    Add forum path lookup test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@68832 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/tests/test_discussions.py
+++ src/nti/app/products/courseware/tests/test_discussions.py
@@ -139,9 +139,24 @@ class TestDiscussions(ApplicationLayerTest):
 
 			f4ds = get_forums_for_discussion(discussion, course)
 			assert_that(f4ds, has_length(2))
-			discussion_ntiid = tuple(f4ds.values()[0].values())[0].NTIID
+			forum = f4ds.values()[0]
+			discussion_ntiid = tuple(forum.values())[0].NTIID
+			forum_ntiid = forum.NTIID
 
 		# Path lookup
+		# Forum -> (Course,Board)
+		path = '/dataserver2/LibraryPath?objectId=%s' % forum_ntiid
+		res = self.testapp.get( path )
+		res = res.json_body
+
+		assert_that( res, has_length( 1 ))
+		res = res[0]
+		assert_that( res, has_length( 2 ))
+
+		assert_that( res[0], has_entry( 'Class', 'CourseInstance' ))
+		assert_that( res[1], has_entries( 'Class', 'CommunityBoard',
+										'title', 'Discussions' ))
+
 		# Topic -> (Course,Board,Forum)
 		path = '/dataserver2/LibraryPath?objectId=%s' % discussion_ntiid
 		res = self.testapp.get( path )
