1864d5d72d4f54673209e99e5f5c72f85273cbb7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-07-16 19:07:46 +0000

    check origin and context for search course filtering
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@69048 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/search/cache.py
+++ src/nti/app/products/courseware/search/cache.py
@@ -13,7 +13,9 @@ from brownie.caching import LFUCache
 
 from zope import component
 from zope import interface
+
 from zope.container.contained import Contained
+
 from zope.traversing.interfaces import IEtcNamespace
 
 from nti.common.property import CachedProperty
@@ -23,7 +25,7 @@ from nti.contentlibrary.indexed_data.interfaces import CONTAINER_IFACES
 
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
-	
+
 from nti.ntiids.ntiids import TYPE_OID
 from nti.ntiids.ntiids import TYPE_UUID
 from nti.ntiids.ntiids import TYPE_INTID
@@ -55,14 +57,14 @@ def _flatten_outline(outline):
 					break
 			if not ntiid:
 				continue
-			if ntiid not in result:	
+			if ntiid not in result:
 				result[ntiid] = (beginning, is_outline_stub_only)
 			else:
 				_begin, _stub = result[ntiid]
-				_begin = max(beginning, _begin) 		# max date
-				_stub = is_outline_stub_only or _stub	# stub true
+				_begin = max(beginning, _begin)  # max date
+				_stub = is_outline_stub_only or _stub  # stub true
 				result[ntiid] = (_begin, _stub)
-				
+
 	def _recur(node, result):
 		content_ntiid = getattr(node, 'ContentNTIID', None)
 		if content_ntiid:
@@ -75,11 +77,11 @@ def _flatten_outline(outline):
 				unit = paths[-1] if paths else None
 				for iface in CONTAINER_IFACES:
 					_indexed_data(iface, result, unit, beginning, is_outline_stub_only)
-					
+
 		# parse children
 		for child in node.values():
 			_recur(child, result)
-	
+
 	if outline is not None:
 		_recur(outline, result)
 	return result
@@ -97,17 +99,19 @@ def _get_content_path(pacakge_paths_cache, ntiid):
 
 def _get_course_from_search_query(query):
 	context = query.context or {}
-	course_id = context.get('course')
-	if course_id:
+	for course_id in (query.origin, context.get('course')):
+		if not course_id:
+			continue
 		course = find_object_with_ntiid(course_id)
 		course = ICourseInstance(course, None)
 		entry = ICourseCatalogEntry(course, None)
-		course_id = getattr(entry, 'ntiid', None)
-		return course, course_id
+		if entry is not None:
+			course_id = entry.ntiid
+			return course, course_id
 	return None, None
 
 class _OutlineCacheEntry(Contained):
-	
+
 	def __init__(self , ntiid):
 		self.ntiid = ntiid
 
@@ -115,13 +119,13 @@ class _OutlineCacheEntry(Contained):
 	def lastSynchronized(self):
 		result = self.__parent__.lastSynchronized
 		return result
-	
+
 	@property
 	def Outline(self):
 		context = find_object_with_ntiid(self.ntiid)
 		course = ICourseInstance(context, None)
 		return course.Outline if course is not None else None
-		
+
 	@CachedProperty('lastSynchronized')
 	def csPackagePaths(self):
 		return dict()
@@ -130,21 +134,21 @@ class _OutlineCacheEntry(Contained):
 	def csFlattenOutline(self):
 		result = _flatten_outline(self.Outline)
 		return result
-	
+
 @interface.implementer(ICourseOutlineCache)
 class _CourseOutlineCache(object):
-	
+
 	max_cache_size = 10
-	
+
 	def __init__(self, *args):
 		self.outline_cache = LFUCache(maxsize=self.max_cache_size)
-	
+
 	@property
 	def lastSynchronized(self):
 		hostsites = component.queryUtility(IEtcNamespace, name='hostsites')
 		result = getattr(hostsites, 'lastSynchronized', 0)
 		return result
-	
+
 	def _get_cached_entry(self, ntiid):
 		entry = self.outline_cache.get(ntiid)
 		if entry is None:
@@ -153,14 +157,14 @@ class _CourseOutlineCache(object):
 			self.outline_cache[ntiid] = entry
 		return entry
 
-	def _check_against_course_outline(self, course_id, course, ntiid, now=None): 
+	def _check_against_course_outline(self, course_id, course, ntiid, now=None):
 		entry = self._get_cached_entry(course_id)
 		nodes = entry.csFlattenOutline
 		pacakge_paths_cache = entry.csPackagePaths
 		ntiids = _get_content_path(pacakge_paths_cache, ntiid) or (ntiid,)
 		# perform checking
 		for content_ntiid, data in nodes.items():
-			_, is_outline_stub_only= data
+			_, is_outline_stub_only = data
 			if content_ntiid in ntiids:
 				result = bool(not is_outline_stub_only)
 				return result
@@ -168,11 +172,11 @@ class _CourseOutlineCache(object):
 
 	def is_allowed(self, ntiid, query=None, now=None):
 		if query is None:
-			return True # allow by default
-	
+			return True  # allow by default
+
 		course, course_id = _get_course_from_search_query(query)
 		if course is None or course_id is None:
-			return True # allow by default
-	
+			return True  # allow by default
+
 		result = self._check_against_course_outline(course_id, course, ntiid)
 		return result
