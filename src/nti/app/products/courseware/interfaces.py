44454a193903f6aadc23f29490962fa96bf0e07d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-09-03 00:22:46 +0000

    miss this checkin
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@72255 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/interfaces.py
+++ src/nti/app/products/courseware/interfaces.py
@@ -364,42 +364,6 @@ class IUserAdministeredCourses(interface.Interface):
 		return an interable with the users administered courses
 		"""
 
-# assesments
-
-class ICourseAssessmentUserFilter(interface.Interface):
-	"""
-	A filter to determine if a user should be able to see
-	an assessment.
-
-	These will typically be registered as subscription adapters
-	from the user and the course.
-	"""
-
-	def allow_assessment_for_user_in_course(assignment, user, course):
-		"""
-		Given a user and an :class:`.ICourseInstance` the user is enrolled in, return a
-		callable that takes an assessment and returns True if the
-		assignment should be visible to the user and False otherwise.
-		"""
-
-def get_course_assessment_predicate_for_user(user, course):
-	"""
-	Given a user and an :class:`.ICourseInstance` the user is enrolled in, return a
-	callable that takes an assessment and returns True if the
-	assignment should be visible to the user and False otherwise.
-
-	Delegates to :class:`.ICourseAssessmentUserFilter` subscribers.
-
-	.. note:: Those subscribers probably implicitly assume that
-		the assessment passed to them is actually hosted within the
-		course.
-	"""
-	filters = component.subscribers((user, course), ICourseAssessmentUserFilter)
-	filters = list(filters)  # Does that return a generator? We need to use it many times
-	def uber_filter(asg):
-		return all((f.allow_assessment_for_user_in_course(asg, user, course) for f in filters))
-	return uber_filter
-
 # deprecations
 
 import zope.deferredimport
