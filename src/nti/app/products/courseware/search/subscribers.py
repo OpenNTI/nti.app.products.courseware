e75660cf01eea1484156b2488c9bea4cf41ff015
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-10-14 19:47:05 +0000

    use index to get all content pacakges accessible in a root search
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@74725 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/search/subscribers.py
+++ src/nti/app/products/courseware/search/subscribers.py
@@ -12,12 +12,20 @@ logger = __import__('logging').getLogger(__name__)
 from zope import component
 from zope import interface
 
+from zope.intid import IIntIds
+
 from nti.contentsearch.interfaces import ISearchPackageResolver
 
+from nti.contenttypes.courses.index import IX_SITE
+from nti.contenttypes.courses.index import IX_USERNAME
+from nti.contenttypes.courses import get_enrollment_catalog
+
 from nti.contenttypes.courses.interfaces import ICourseInstance
-from nti.contenttypes.courses.interfaces import IPrincipalEnrollments
+from nti.contenttypes.courses.interfaces import ICourseInstanceEnrollmentRecord
 
 from nti.ntiids.ntiids import ROOT
+
+from nti.site.site import get_component_hierarchy_names
 
 @interface.implementer(ISearchPackageResolver)
 class _RootSearchPacakgeResolver(object):
@@ -28,18 +36,27 @@ class _RootSearchPacakgeResolver(object):
 	def resolve(self, user, ntiid=None):
 		result = set()
 		if ntiid == ROOT:
-			for enrollments in component.subscribers((user,), IPrincipalEnrollments):
-				for enrollment in enrollments.iter_enrollments():
-					course = ICourseInstance(enrollment, None)
-					if course is None:  # dupped enrollment
-						continue
-					try:
-						packages = course.ContentPackageBundle.ContentPackages
-					except AttributeError:
-						packages = (course.legacy_content_package,)
+			catalog = get_enrollment_catalog()
+			intids = component.getUtility(IIntIds)
+			site_names = get_component_hierarchy_names()
+			query = {
+				IX_SITE:{'any_of': site_names},
+				IX_USERNAME:{'any_of':(user.username,)}
+			}
+			for uid in catalog.apply(query) or ():
+				context = intids.queryObject(uid)
+				if ICourseInstanceEnrollmentRecord.providedBy(context):
+					context = context.CourseInstance
+				elif not ICourseInstance.providedBy(context):
+					continue
 
-					for package in packages:
-						ntiid = package.ntiid
-						if ntiid:  # make sure we a valid ntiid
-							result.add(package.ntiid)
+				try:
+					packages = context.ContentPackageBundle.ContentPackages
+				except AttributeError:
+					packages = (context.legacy_content_package,)
+
+				for package in packages:
+					ntiid = package.ntiid
+					if ntiid:  # make sure we a valid ntiid
+						result.add(package.ntiid)
 		return result
