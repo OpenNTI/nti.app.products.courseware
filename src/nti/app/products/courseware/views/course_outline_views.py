4c0f0bbaa786635fa517c2b2c378a3465022f306
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-10-30 22:33:10 +0000

    add admin views to reset outlines
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@75941 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/products/courseware/views/course_outline_views.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+import six
+import time
+
+from zope import component
+
+from zope.component.hooks import site as current_site
+
+from zope.security.management import endInteraction
+from zope.security.management import restoreInteraction
+
+from zope.traversing.interfaces import IEtcNamespace
+
+from pyramid.view import view_config
+from pyramid.view import view_defaults
+from pyramid import httpexceptions as hexc
+
+from nti.app.base.abstract_views import AbstractAuthenticatedView
+
+from nti.app.externalization.internalization import read_body_as_external_object
+from nti.app.externalization.view_mixins import ModeledContentUploadRequestUtilsMixin
+
+from nti.common.maps import CaseInsensitiveDict
+
+from nti.contenttypes.courses.interfaces import COURSE_OUTLINE_NAME
+
+from nti.contenttypes.courses._outline_parser import outline_nodes
+from nti.contenttypes.courses._outline_parser import unregister_nodes
+from nti.contenttypes.courses._outline_parser import fill_outline_from_key
+
+from nti.contenttypes.courses.interfaces import ICourseCatalog
+from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseSubInstance
+from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
+from nti.contenttypes.courses.legacy_catalog import ILegacyCourseInstance
+
+from nti.contenttypes.courses.utils import get_parent_course
+
+from nti.dataserver import authorization as nauth
+
+from nti.externalization.interfaces import LocatedExternalDict
+from nti.externalization.interfaces import StandardExternalFields
+
+from nti.ntiids.ntiids import find_object_with_ntiid
+
+from nti.recorder.record import remove_transaction_history
+
+from nti.site.site import get_component_hierarchy_names
+
+from . import CourseAdminPathAdapter
+
+ITEMS = StandardExternalFields.ITEMS
+
+def _parse_courses(values):
+	ntiids = values.get('ntiid') or values.get('ntiids') or \
+			 values.get('entry') or values.get('entries') or \
+			 values.get('course') or values.get('courses')
+	if not ntiids:
+		raise hexc.HTTPUnprocessableEntity(detail='No course entry identifier')
+
+	if isinstance(ntiids, six.string_types):
+		ntiids = ntiids.split()
+
+	result = list()
+	catalog = component.getUtility(ICourseCatalog)
+	for ntiid in ntiids:
+		context = find_object_with_ntiid(ntiid)
+		if context is None:
+			try:
+				context = catalog.getCatalogEntry(ntiid)
+			except (KeyError, LookupError):
+				context = None
+		context = ICourseInstance(context, None)
+		if context is not None:
+			result.append(context)
+	return result
+
+def _parse_course(values):
+	result = _parse_courses(values)
+	if not result:
+		raise hexc.HTTPUnprocessableEntity(detail='Course not found')
+	return result[0]
+
+def _reverse(data):
+	data.reverse()
+	return data
+
+def get_sites(sites_names):
+	result = []
+	hostsites = component.getUtility(IEtcNamespace, name='hostsites')
+	for name in sites_names or ():
+		try:
+			folder = hostsites[name]
+			result.append(folder)
+		except KeyError:
+			pass
+	return result
+
+def read_input(request):
+	if request.body:
+		values = read_body_as_external_object(request)
+	else:
+		values = request.params
+	result = CaseInsensitiveDict(values)
+	return result
+
+@view_config(name='ResetCourseOutline')
+@view_defaults(route_name='objects.generic.traversal',
+			   renderer='rest',
+			   context=CourseAdminPathAdapter,
+			   permission=nauth.ACT_NTI_ADMIN)
+class ResetCourseOutlineView(AbstractAuthenticatedView,
+				 			 ModeledContentUploadRequestUtilsMixin):
+
+	def readInput(self, value=None):
+		result = read_input(self.request)
+		return result
+
+	def _do_call(self, result, courses=None):
+		if courses is None:
+			values = self.readInput()
+			courses = _parse_courses(values)
+
+		to_process = set()
+		items = result[ITEMS] = {}
+		site_names = _reverse(get_component_hierarchy_names())
+		sites = get_sites(site_names)
+		
+		for course in courses:
+			course = ICourseInstance(course)
+			if ILegacyCourseInstance.providedBy(course):
+				continue
+			if ICourseSubInstance.providedBy(course):
+				parent = get_parent_course(course)
+				if parent.Outline == course.Outline:
+					course = parent
+			to_process.add(course)
+
+		for course in to_process:
+			removed = []
+			outline = course.Outline
+			for site in sites:
+				registry = site.getSiteManager()
+				removed.extend(unregister_nodes(outline,
+												registry=registry,
+												force=True))
+			for node in removed:
+				remove_transaction_history(node)
+			outline.reset()
+
+			root = course.root
+			outline_xml_node = None
+			outline_xml_key = root.getChildNamed(COURSE_OUTLINE_NAME)
+			if not outline_xml_key:
+				if course.ContentPackageBundle:
+					for package in course.ContentPackageBundle.ContentPackages:
+						outline_xml_key = package.index
+						outline_xml_node = 'course'
+						break
+
+			with current_site(sites[0]):  # sync in root
+				fill_outline_from_key(course.Outline,
+								  	  outline_xml_key,
+								  	  xml_parent_name=outline_xml_node,
+								  	  force=True)
+				
+			ntiid = ICourseCatalogEntry(course).ntiid
+			items[ntiid] = [x.ntiid for x in outline_nodes(course.Outline)]
+		return to_process
+
+	def __call__(self):
+		now = time.time()
+		result = LocatedExternalDict()
+		endInteraction()
+		try:
+			self._do_call(result)
+		finally:
+			restoreInteraction()
+			result['TimeElapsed'] = time.time() - now
+		return result
+
+@view_config(name='ResetAllCoursesOutlines')
+@view_defaults(route_name='objects.generic.traversal',
+			   renderer='rest',
+			   context=CourseAdminPathAdapter,
+			   permission=nauth.ACT_NTI_ADMIN)
+class ResetAllCoursesOutlinesView(ResetCourseOutlineView):
+
+	def _do_call(self, result, courses=None):
+		catalog = component.getUtility(ICourseCatalog)
+		courses = list(catalog.iterCatalogEntries())
+		return super(ResetAllCoursesOutlinesView, self)._do_call(result, courses)
