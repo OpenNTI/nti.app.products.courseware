b8d8191faf9f808f771b17ecd732814699fc6c5a
Josh Zuech <josh.zuech@nextthought.com>
2015-11-23 16:39:49 +0000

    Test course emails output
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@77636 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/tests/test_mail_views.py
+++ src/nti/app/products/courseware/tests/test_mail_views.py
@@ -7,9 +7,24 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
+from hamcrest import is_
+from hamcrest import is_not
+from hamcrest import not_none
+from hamcrest import has_length
+from hamcrest import assert_that
+from hamcrest import has_property
+from hamcrest import contains_string
+does_not = is_not
+
+from quopri import decodestring
+
+from zope import component
+
 from nti.app.mail.interfaces import Email
 
 from nti.app.products.courseware import VIEW_COURSE_MAIL
+
+from nti.app.testing.testing import ITestMailDelivery
 
 from nti.app.testing.decorators import WithSharedApplicationMockDS
 from nti.app.testing.application_webtest import ApplicationLayerTest
@@ -30,6 +45,68 @@ class TestMailViews(ApplicationLayerTest):
 	default_origin = b'http://janux.ou.edu'
 	enrolled_courses_href = '/dataserver2/users/sjohnson@nextthought.com/Courses/EnrolledCourses'
 	expected_enrollment_href = '/dataserver2/users/sjohnson%40nextthought.com/Courses/EnrolledCourses/tag%3Anextthought.com%2C2011-10%3AOU-HTML-CLC3403_LawAndJustice.course_info'
+
+
+	def _test_mail_payload(self, mail, instructor_env, link):
+		# Test basic text
+		mailer = component.getUtility(ITestMailDelivery)
+		del mailer.queue[:]
+		self.testapp.post_json(link, mail, extra_environ=instructor_env)
+
+		to_check = mail.get( 'Body' )
+		assert_that( mailer.queue, has_length(1) )
+		msg = mailer.queue[0]
+		assert_that( msg, has_property( 'body'))
+		body = decodestring(msg.body)
+		assert_that( body, contains_string( to_check ) )
+		assert_that( msg, has_property('html'))
+		html = decodestring(msg.html)
+		assert_that( html, contains_string( to_check ) )
+
+		# Test encoding
+		mail['Body'] = '哈哈....Zürich is a hub'
+		del mailer.queue[:]
+		self.testapp.post_json(link, mail, extra_environ=instructor_env)
+
+		to_check = mail.get( 'Body' ).encode( 'utf-8' )
+		assert_that( mailer.queue, has_length(1) )
+		msg = mailer.queue[0]
+		body = decodestring(msg.body)
+		assert_that( body, contains_string( to_check ) )
+		html = decodestring(msg.html)
+		assert_that( html, contains_string( to_check ) )
+
+		# Test html
+		mail['Body'] = 'Test <br /> <br /> with line breaks. <br />'
+		del mailer.queue[:]
+		self.testapp.post_json(link, mail, extra_environ=instructor_env)
+
+		to_check = 'with line breaks.'
+		assert_that( mailer.queue, has_length(1) )
+		msg = mailer.queue[0]
+		body = decodestring(msg.body)
+		assert_that( body, contains_string( to_check ) )
+		assert_that( body, does_not( contains_string( '<br>' ) ) )
+		html = decodestring(msg.html)
+		assert_that( html, contains_string( to_check ) )
+		assert_that( html, contains_string( 'Test <br>' ) )
+
+		# Test script
+		mail['Body'] = '<div><script><p>should be ignored</p> Other stuff.</script><p>test output</p>'
+		del mailer.queue[:]
+		self.testapp.post_json(link, mail, extra_environ=instructor_env)
+
+		to_check = 'test output'
+		script = '<script><p>should be ignored</p> Other stuff.</script>'
+		assert_that( mailer.queue, has_length(1) )
+		msg = mailer.queue[0]
+		body = decodestring(msg.body)
+		assert_that( body, contains_string( to_check ) )
+		assert_that( body, does_not( contains_string( '</p>' ) ) )
+		assert_that( body, does_not( contains_string( script ) ) )
+		html = decodestring(msg.html)
+		assert_that( html, contains_string( 'test output</p>' ) )
+		assert_that( html, does_not( contains_string( script ) ) )
 
 	@WithSharedApplicationMockDS(users=('aaa_nextthought_com',),
 								 testapp=True,
@@ -89,3 +166,5 @@ class TestMailViews(ApplicationLayerTest):
 		self.testapp.get(roster_link + '/not_enrolled/Mail',
 						 status=404,
 						 extra_environ=instructor_env)
+
+		self._test_mail_payload( mail, instructor_env, email_link )
