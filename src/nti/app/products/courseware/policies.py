57e49fb2c46a4b30969a682f16114c3dd7a1d134
Chris Utz <chris.utz@nextthought.com>
2015-12-31 15:47:54 +0000

    expose a capability for users that have access to advanced editing capabilities for otherwise editable courses
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@79559 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/products/courseware/policies.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Policies and components that are related to courseware.
+
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.appserver.interfaces import IUserCapabilityFilter
+from nti.appserver.pyramid_authorization import has_permission
+
+from nti.dataserver import authorization as nauth
+from nti.dataserver.interfaces import IUser
+
+@component.adapter(IUser)
+@interface.implementer(IUserCapabilityFilter)
+class AdvancedEditingCapabilityFilter(object):
+	"""
+	Removes the Advanced Editing capability for users that don't have
+	global edit permissions.
+	"""
+	def __init__(self, context=None):
+		self.user = context
+
+	def filterCapabilities(self, capabilities):
+		result = set(capabilities)
+		if self.user is None or not has_permission(nauth.ACT_CONTENT_EDIT, self.user.__parent__):
+			result.discard('nti.platform.courseware.advanced_editing')
+		return result
+
