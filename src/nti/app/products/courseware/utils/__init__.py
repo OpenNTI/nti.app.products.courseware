427c29dc4ac25019dbe883a7f11a418102b8393e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-02-10 18:29:05 +0000

    Sphinx Docs
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@82653 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/utils/__init__.py
+++ src/nti/app/products/courseware/utils/__init__.py
@@ -76,10 +76,10 @@ from nti.site.site import get_component_hierarchy_names
 
 from nti.traversal.traversal import find_interface
 
-# : Default memcache expiration time
+#: Default memcache expiration time
 DEFAULT_EXP_TIME = 86400
 
-# : 1970-01-1
+#: 1970-01-1
 ZERO_DATETIME = datetime.utcfromtimestamp(0)
 
 def last_synchronized(context=None):
