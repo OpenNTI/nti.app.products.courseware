aa72f6c3c17a88a360a98ea3c693695553be2bf3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-02-16 17:30:19 +0000

    plone formatting
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@83000 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/acl.py
+++ src/nti/app/products/courseware/acl.py
@@ -14,17 +14,17 @@ from zope import interface
 
 from zope.security.interfaces import IPrincipal
 
-from nti.common.property import Lazy
+from nti.app.products.courseware.interfaces import IEnrolledCoursesCollection
+from nti.app.products.courseware.interfaces import IAdministeredCoursesCollection
 
-from nti.dataserver.interfaces import IACLProvider
-from nti.dataserver.interfaces import ACE_DENY_ALL
-from nti.dataserver.interfaces import ALL_PERMISSIONS
+from nti.common.property import Lazy
 
 from nti.dataserver.authorization_acl import ace_allowing
 from nti.dataserver.authorization_acl import acl_from_aces
 
-from .interfaces import IEnrolledCoursesCollection
-from .interfaces import IAdministeredCoursesCollection
+from nti.dataserver.interfaces import IACLProvider
+from nti.dataserver.interfaces import ACE_DENY_ALL
+from nti.dataserver.interfaces import ALL_PERMISSIONS
 
 @interface.implementer(IACLProvider)
 class CollectionACLProviderMixin(object):
