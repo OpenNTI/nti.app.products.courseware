0f0f51653da285347fd0bc2210d67315641bde0c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-02-16 16:44:45 +0000

    update script
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@82988 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/scripts/nti_unregister_invalid_nodes.py
+++ src/nti/app/products/courseware/scripts/nti_unregister_invalid_nodes.py
@@ -15,45 +15,64 @@ import argparse
 
 from zope import component
 
-from zope.component.hooks import site as current_site
+from zope.interface.adapter import _lookupAll as zopeLookupAll
 
-from zope.intid import IIntIds
+from zope.intid.interfaces import IIntIds
+
+from nti.contenttypes.courses.interfaces import ALL_COURSE_OUTLINE_INTERFACES
 
 from nti.contenttypes.courses.interfaces import iface_of_node
-from nti.contenttypes.courses.interfaces import ICourseOutlineNode
 
 from nti.dataserver.utils import run_with_dataserver
 from nti.dataserver.utils.base_script import create_context
 
-from nti.site.utils import unregisterUtility
 from nti.site.hostpolicy import get_all_host_sites
 
-def unregister(registry, name, provided):
-	return unregisterUtility(registry=registry,
-					  		 provided=provided,
-					  		 name=name)
+from nti.site.utils import unregisterUtility
 
-def clean(verbose=False):
-	result = 0
-	intids = component.getUtility(IIntIds)
-	for site in get_all_host_sites():
-		with current_site(site):
-			count = 0
-			name = site.__name__
-			registry = component.getSiteManager()
-			for ntiid, obj in list(registry.getUtilitiesFor(ICourseOutlineNode)):
-				if 	intids.queryId(obj) is None and \
-					unregister(registry, ntiid, iface_of_node(obj)):
-					count += 1
-					if verbose:
-						logger.warn("Removed %s from %s", ntiid, name)
-
-			if verbose:
-				logger.warn("Removed %s node(s) from %s", count, name)
-			result += count
-	if verbose:
-		logger.info("Remove %s node(s)", result)
+def _lookupAll(main):
+	result = {}
+	required = ()
+	order = len(required)
+	for registry in main.utilities.ro: # must keep order
+		byorder = registry._adapters
+		if order >= len(byorder):
+			continue
+		components = byorder[order]
+		extendors = ALL_COURSE_OUTLINE_INTERFACES
+		zopeLookupAll(components, required, extendors, result, 0, order)  
+		break # break on first
 	return result
+
+def _process_site(current, intids, seen):
+	site_name = current.__name__
+	registry = current.getSiteManager()
+	site_components = _lookupAll(registry)
+	logger.info("%s outline node(s) found in %s", len(site_components), site_name)
+
+	for ntiid, item in site_components.items():
+		provided = iface_of_node(item)
+		doc_id = intids.queryId(item)
+
+		# registration for a removed outline
+		if doc_id is None:
+			logger.warn("Removing invalid registration %s from site %s",
+						ntiid, site_name)
+			unregisterUtility(registry, item, provided=provided, name=ntiid, force=True)
+			continue
+
+		# registration not in base site
+		if ntiid in seen:
+			logger.warn("Removing %s from site %s", ntiid, site_name)
+			unregisterUtility(registry, provided=provided, name=ntiid)
+
+		seen.add(ntiid)
+
+def _process_args(verbose=False):
+	seen = set()
+	intids = component.getUtility(IIntIds)
+	for current in get_all_host_sites():
+		_process_site(current, intids, seen)
 
 def main():
 	arg_parser = argparse.ArgumentParser(description="Unregister invalid outline nodes")
@@ -72,7 +91,7 @@ def main():
 						xmlconfig_packages=conf_packages,
 						verbose=args.verbose,
 						context=context,
-						function=lambda: clean(verbose=args.verbose))
+						function=lambda: _process_args(verbose=args.verbose))
 	sys.exit(0)
 
 if __name__ == '__main__':
