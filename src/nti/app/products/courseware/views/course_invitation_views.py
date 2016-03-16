fcab49f0b09e872f7764a24be58c29ddb6055e25
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-03-16 19:42:55 +0000

    do validation for inviations
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@84770 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/views/course_invitation_views.py
+++ src/nti/app/products/courseware/views/course_invitation_views.py
@@ -9,16 +9,27 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import csv
+import six
+
 from zope import component
+
+from zope.i18n import translate
+
+from z3c.schema.email import isValidMailAddress
 
 from pyramid import httpexceptions as hexc
 
 from pyramid.view import view_config
 from pyramid.view import view_defaults
 
+from nti.app.base.abstract_views import get_source 
 from nti.app.base.abstract_views import AbstractAuthenticatedView
 
+from nti.app.externalization.error import raise_json_error
 from nti.app.externalization.view_mixins import ModeledContentUploadRequestUtilsMixin
+
+from nti.app.products.courseware import MessageFactory as _
 
 from nti.app.products.courseware.views import SEND_COURSE_INVITATIONS
 from nti.app.products.courseware.views import VIEW_COURSE_INVITATIONS
@@ -29,7 +40,11 @@ from nti.appserver.dataserver_pyramid_views import GenericGetView
 
 from nti.appserver.pyramid_authorization import has_permission
 
+from nti.common.maps import CaseInsensitiveDict
+
 from nti.common.property import Lazy
+
+from nti.common.string import TRUE_VALUES
 
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
@@ -39,7 +54,11 @@ from nti.contenttypes.courses.utils import is_course_instructor
 	
 from nti.dataserver import authorization as nauth
 
+from nti.dataserver.interfaces import IUser
 from nti.dataserver.interfaces import IDataserverFolder
+
+from nti.dataserver.users import User
+from nti.dataserver.users.interfaces import IUserProfile
 
 from nti.externalization.externalization import to_external_ntiid_oid
 
@@ -98,10 +117,118 @@ class SendCourseInvitationsView(AbstractAuthenticatedView,
 	def _course(self):
 		return ICourseInstance(self.context)
 
+	def readInput(self, value=None):
+		result = ModeledContentUploadRequestUtilsMixin.readInput(self, value=value)
+		result = CaseInsensitiveDict(result)
+		return result
+
+	def get_invitation(self, values):
+		code = values.get('code')
+		if not code:
+			raise hexc.HTTPUnprocessableEntity(_("Must provide a inviation code."))
+		invitation = component.getUtility(IJoinCourseInvitation, name=code)
+		if invitation is None:
+			raise hexc.HTTPUnprocessableEntity(_("Invalid inviation code."))
+
+	def get_direct_users(self, values, warnings=()):
+		usernames = values.get('user') or values.get('username') or values.get('usernames')
+		if isinstance(usernames, six.string_types):
+			usernames = usernames.split(",")
+		usernames = set(usernames or ())
+		
+		result = {}
+		for username in usernames:
+			user = User.get_user(username)
+			if not IUser.providedBy(user):
+				msg = translate(_("Could not find user ${user}.", 
+								mapping={'user': username}))
+				warnings.add(msg)
+				continue
+			profile = IUserProfile(user)
+			realname = profile.realname
+			email = getattr(profile, 'email', None)
+			if not email or not realname:
+				msg = translate(_("User ${user} does not have a valid email.", 
+								mapping={'user': username}))
+				warnings.add(msg)
+				continue
+			result[email] = realname
+		return result
+		
+	def get_csv_users(self, warnings=()):
+		result = {}
+		source = get_source(self.request, 'csv')
+		if source is not None:
+			for idx, row in enumerate(csv.reader(source)):
+				if not row or row[0].startswith("#"):
+					continue
+				realname = row[0]
+				email = row[1] if len(row) > 1 else None
+				if not realname or not email:
+					msg = translate(_("Missing name or email in line ${line}.", 
+									mapping={'line': idx+1}))
+					warnings.add(msg)
+					continue
+				if not isValidMailAddress(email):
+					msg = translate(_("Invalid email ${email}.", 
+									mapping={'email': email}))
+					warnings.add(msg)
+					continue
+				result[email] = realname
+		return result
+
+	def send_invitations(self, invitation, users):
+		pass
+
 	def __call__(self):
 		if 		not is_course_instructor(self._course, self.remoteUser) \
 			and not has_permission(nauth.ACT_NTI_ADMIN, self._course, self.request):
 			raise hexc.HTTPForbidden()
+		
+		values = self.readInput()
+		force = (values.get('force') or u'').lower() in TRUE_VALUES
+
+		warnings = []
+		invitation = self.get_invitation(values)
+		csv_users = self.get_csv_users(warnings)
+		if not force and warnings:
+			raise_json_error(
+					self.request,
+					hexc.HTTPUnprocessableEntity,
+					{
+						u'warnings':  warnings,
+						u'message': _('There are errors in invitation csv source.') ,
+						u'code': 'SendCourseInvitationCSVError',
+					},
+					None)
+		
+		warnings = []
+		direct_users = self.get_direct_users(values, warnings)
+		if not force and warnings:
+			raise_json_error(
+					self.request,
+					hexc.HTTPUnprocessableEntity,
+					{
+						u'warnings':  warnings,
+						u'message': _('Could not process all user invitations.') ,
+						u'code': 'SendCourseInvitationUserError',
+					},
+					None)
+		
+		all_users = direct_users
+		all_users.update(csv_users)
+		if not all_users:
+			raise_json_error(
+					self.request,
+					hexc.HTTPUnprocessableEntity,
+					{
+						u'message': _('There are no invitation to process.') ,
+						u'code': 'SendCourseInvitationError',
+					},
+					None)
+		# send
+		self.send_invitations(invitation, direct_users)
+		return hexc.HTTPNoContent()
 		
 @view_config(context=IDataserverFolder)
 @view_config(context=CourseAdminPathAdapter)
