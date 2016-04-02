cfa65be4985da08f48128ea7e5b12151cd17220e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-04-02 22:18:36 +0000

    use a common func
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@85828 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/resources/filer.py
+++ src/nti/app/products/courseware/resources/filer.py
@@ -40,11 +40,11 @@ from nti.contentfolder.utils import TraversalException
 
 from nti.contenttypes.courses.interfaces import ICourseInstance
 
+from nti.common.file import safe_filename
+
 from nti.common.random import generate_random_hex_string
 
 from nti.coremetadata.interfaces import SYSTEM_USER_ID
-
-from nti.namedfile.file import safe_filename
 
 from nti.traversal.traversal import find_interface
 
