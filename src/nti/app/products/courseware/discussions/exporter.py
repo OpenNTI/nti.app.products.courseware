27699cdb4cb465b82bf387ec1f1a3fac410700f7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-04-07 19:40:53 +0000

    set props
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@86167 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/discussions/exporter.py
+++ src/nti/app/products/courseware/discussions/exporter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-.. $Id: exporter.py 85599 2016-03-30 20:38:59Z carlos.sanchez $
+.. $Id$
 """
 
 from __future__ import print_function, unicode_literals, absolute_import, division
