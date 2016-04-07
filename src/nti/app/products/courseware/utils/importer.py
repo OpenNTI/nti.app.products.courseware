c41aa9c102513c61cf20c999333a77c5d823294d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-04-07 20:02:19 +0000

    move func to its own module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@86172 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/products/courseware/utils/importer.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+import os
+import six
+from urlparse import urlparse
+
+from nti.common import mimetypes
+
+from nti.contenttypes.courses.interfaces import NTI_COURSE_FILE_SCHEME
+
+def transfer_resources_from_filer(provided, obj, source_filer, target_filer):
+	"""
+	parse the provided interface field and look for internal resources to
+	be gotten from the specified source filer and saved to the target filer
+	"""
+	result = {}
+	for name in provided:
+		if name.startswith('_'):
+			continue
+		value = getattr(obj, name, None)
+		if 		value is not None \
+			and isinstance(value, six.string_types) \
+			and value.startswith(NTI_COURSE_FILE_SCHEME):
+
+			path = urlparse(value).path
+			bucket, name = os.path.split(path)
+			bucket = None if not bucket else bucket
+			
+			source  = source_filer.get(path)
+			contentType = getattr(source, 'contentType', None) or mimetypes.guess_type(name)
+			if source is not None:
+				href = target_filer.save(name, source, bucket=bucket, 
+										 contentType=contentType,
+								  		 overwrite=True)
+				setattr(obj, name, href)
+				result[name] = href
+	return result
