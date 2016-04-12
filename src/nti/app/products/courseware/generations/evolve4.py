69e5d260d58cb25dca85aead81fc93ce4541442b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-04-12 18:10:47 +0000

    sketch evol4
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@86509 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/products/courseware/generations/evolve4.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+generation = 3
+
+from zope import component
+from zope import interface
+
+from zope.component.hooks import site as current_site
+
+from nti.contenttypes.courses.discussions.interfaces import ICourseDiscussions
+
+from nti.contenttypes.courses.discussions.utils import get_topic_key
+
+from nti.contenttypes.courses.interfaces import ICourseCatalog
+from nti.contenttypes.courses.interfaces import ICourseInstance
+
+from nti.dataserver.interfaces import IDataserver
+from nti.dataserver.interfaces import IOIDResolver
+
+from nti.ntiids.ntiids import make_specific_safe
+
+from nti.site.hostpolicy import get_all_host_sites
+
+@interface.implementer(IDataserver)
+class MockDataserver(object):
+
+	root = None
+
+	def get_by_oid(self, oid, ignore_creator=False):
+		resolver = component.queryUtility(IOIDResolver)
+		if resolver is None:
+			logger.warn("Using dataserver without a proper ISiteManager configuration.")
+		else:
+			return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
+		return None
+
+def _decode(s):
+	if not s:
+		s = u''
+	elif not isinstance(s, unicode):
+		s = s.decode('utf-8', 'ignore')
+	return s
+
+def _migrate(current, seen):
+	with current_site(current):
+		catalog = component.queryUtility(ICourseCatalog)
+		if catalog is None or catalog.isEmpty():
+			return
+		for entry in catalog.iterCatalogEntries():
+			if entry.ntiid in seen:
+				continue
+			seen.add(entry.ntiid)
+			course = ICourseInstance(entry)
+			course_discussions = ICourseDiscussions(course)
+			if not course_discussions:
+				continue
+			for course_discussion in course_discussions.values():
+				key = get_topic_key(course_discussion)
+				title = make_specific_safe(_decode(course_discussion.title))
+				# search in board fourms
+				for board in course.Discussions:
+					for forum in board.values():
+						if title in forum:
+							from IPython.core.debugger import Tracer; Tracer()()
+							logger.info("Moving %s to %s", title, key)
+							item = forum._delitemf(title, event=False)
+							forum._setitemf(key, item)
+
+def do_evolve(context, generation=generation):
+	conn = context.connection
+	ds_folder = conn.root()['nti.dataserver']
+
+	mock_ds = MockDataserver()
+	mock_ds.root = ds_folder
+	component.provideUtility(mock_ds, IDataserver)
+
+	with current_site(ds_folder):
+		assert	component.getSiteManager() == ds_folder.getSiteManager(), \
+				"Hooks not installed?"
+
+		seen = set()
+		for current in get_all_host_sites():
+			_migrate(current, seen)
+
+	component.getGlobalSiteManager().unregisterUtility(mock_ds, IDataserver)
+	logger.info('Evolution %s done.', generation)
+
+def evolve(context):
+	"""
+	Evolve to generation 2 by adjusting bundled discussion ACLs.
+	"""
+	do_evolve(context, generation)
