387d8d1972a0787980e47702e4196152f76e5445
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-04-18 16:28:23 +0000

    complete adapter registration
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@86938 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/discussions/adapters.py
+++ src/nti/app/products/courseware/discussions/adapters.py
@@ -10,11 +10,15 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 from zope import component
+from zope import interface
 
 from nti.contenttypes.courses.discussions.interfaces import ICourseDiscussion
 
 from nti.namedfile.constraints import FileConstraints
 
+from nti.namedfile.interfaces import IFileConstraints
+
 @component.adapter(ICourseDiscussion)
+@interface.implementer(IFileConstraints)
 class _CourseDiscussionFileConstraints(FileConstraints):
 	max_file_size = 10485760  # 10 MB
