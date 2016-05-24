df3cb967f84de8be80259a27bd3f9360eb04e60a
Josh Zuech <josh.zuech@nextthought.com>
2016-05-24 19:39:02 +0000

    Use implied scopes when fetching notable items by instructors
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@89187 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/notables.py
+++ src/nti/app/products/courseware/notables.py
@@ -14,23 +14,19 @@ logger = __import__('logging').getLogger(__name__)
 from zope import component
 from zope import interface
 
-from zope.intid.interfaces import IIntIds
-
 from BTrees.LFBTree import LFSet as Set
 
 from nti.app.notabledata.interfaces import IUserPriorityCreatorNotableProvider
 
 from nti.common.property import CachedProperty
 
-from nti.contenttypes.courses.index import IX_SITE
-from nti.contenttypes.courses.index import IX_USERNAME
+from nti.contenttypes.courses.utils import get_enrollments
+
+from nti.contenttypes.courses.interfaces import ENROLLMENT_SCOPE_MAP
 
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
 from nti.contenttypes.courses.interfaces import IPrincipalEnrollments
-from nti.contenttypes.courses.interfaces import ICourseInstanceEnrollmentRecord
-
-from nti.contenttypes.courses.utils import get_enrollment_catalog
 
 from nti.dataserver.contenttypes.forums.interfaces import IPersonalBlogComment
 
@@ -41,9 +37,21 @@ from nti.dataserver.metadata_index import isTopLevelContentObjectFilter
 
 from nti.metadata import dataserver_metadata_catalog
 
-from nti.site.site import get_component_hierarchy_names
-
 _FEEDBACK_MIME_TYPE = "application/vnd.nextthought.assessment.userscourseassignmenthistoryitemfeedback"
+
+def _get_implied_course_scopes( course, scope ):
+	"""
+	For the given enrollment scope and course, return all implied scopes.
+	"""
+	scope_term = ENROLLMENT_SCOPE_MAP[scope]
+	results = []
+	course_scope = course.SharingScopes.get( scope )
+	results.append( course_scope )
+	for scope in scope_term.implies:
+		course_scope = course.SharingScopes.get( scope )
+		if course_scope is not None:
+			results.append( course_scope )
+	return results
 
 @interface.implementer(INotableFilter)
 class TopLevelPriorityNotableFilter(object):
@@ -59,7 +67,6 @@ class TopLevelPriorityNotableFilter(object):
 	def is_notable(self, obj, user):
 		obj_creator = getattr(obj, 'creator', None)
 		obj_creator = getattr(obj_creator, 'username', obj_creator)
-
 		# Filter out blog comments that might cause confusion.
 		if 		obj_creator is None \
 			or 	IPersonalBlogComment.providedBy(obj) \
@@ -86,10 +93,10 @@ class TopLevelPriorityNotableFilter(object):
 						continue
 
 					if obj_creator in (x.id for x in course.instructors):
-						# TODO: Like in the provider below, implies?
-						course_scope = course.SharingScopes[ enrollment.Scope ]
-						if obj.isSharedDirectlyWith(course_scope):
-							return True
+						scopes = _get_implied_course_scopes( course, enrollment.Scope )
+						for scope in scopes:
+							if obj.isSharedDirectlyWith(scope):
+								return True
 		return False
 
 @component.adapter(IUser, interface.Interface)
@@ -122,31 +129,23 @@ class _UserPriorityCreatorNotableProvider(object):
 	def get_notable_intids(self):
 		results = Set()
 		catalog = self._catalog
-		intids = component.getUtility(IIntIds)
-		enrollment_catalog = get_enrollment_catalog()
-		site_names = get_component_hierarchy_names()
-		query = {
-			IX_SITE:{'any_of': site_names},
-			IX_USERNAME:{'any_of':(self.context.username,)}
-		}
-		for uid in enrollment_catalog.apply(query) or ():
-			enrollment = intids.queryObject(uid)
-			if not ICourseInstanceEnrollmentRecord.providedBy(enrollment):
-				continue
+		enrollments = get_enrollments( self.context.username ) or ()
 
+		for enrollment in enrollments:
 			course = ICourseInstance(enrollment, None)
 			catalog_entry = ICourseCatalogEntry(course, None)
 			if 		course is None \
-				or 	catalog_entry is None \
+				or 	catalog_entry is None  \
 				or 	not catalog_entry.isCourseCurrentlyActive():  # pragma: no cover
 				continue
 
 			course_instructors = {x.id for x in course.instructors}
 			instructor_intids = catalog['creator'].apply({'any_of': course_instructors})
 
-			# TODO: Do we need implies?
-			course_scope = course.SharingScopes[ enrollment.Scope ]
-			scope_ntiids = (course_scope.NTIID,)
+			# Gather the implied course scopes.
+			scopes = _get_implied_course_scopes( course, enrollment.Scope )
+			scope_ntiids = [scope.NTIID for scope in scopes]
+
 			course_shared_with_intids = catalog['sharedWith'].apply({'any_of': scope_ntiids})
 			course_results = catalog.family.IF.intersection(instructor_intids,
 															course_shared_with_intids)
