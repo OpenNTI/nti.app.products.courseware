50b701d8a54f7e3365c28ba0de44c948089e6c57
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-07-03 00:24:38 +0000

    make it course based
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@91699 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/views/course_export_views.py
+++ src/nti/app/products/courseware/views/course_export_views.py
@@ -15,6 +15,7 @@ import tempfile
 from zope import component
 
 from pyramid.view import view_config
+from pyramid.view import view_defaults
 
 from nti.app.base.abstract_views import AbstractAuthenticatedView
 
@@ -29,49 +30,68 @@ from nti.common.maps import CaseInsensitiveDict
 from nti.contenttypes.courses.interfaces import ICourseExporter
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseExportFiler
+from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
 
 from nti.dataserver import authorization as nauth
 
+def export_course(context):
+	course = ICourseInstance(context)
+	filer = ICourseExportFiler(course)
+	try:
+		# prepare filer
+		filer.prepare()
+
+		# export course
+		exporter = component.getUtility(ICourseExporter)
+		exporter.export(course, filer)
+
+		# zip contents
+		zip_file = filer.asZip(path=tempfile.mkdtemp())
+		return zip_file
+	finally:
+		filer.reset()
+
+def _export_course_response(context, response):
+	zip_file = None
+	try:
+		zip_file = export_course(context)
+		filename = os.path.split(zip_file)[1]
+		response.content_encoding = str('identity')
+		response.content_type = str('application/zip; charset=UTF-8')
+		response.content_disposition = str('attachment; filename="%s"' % filename)
+		response.body_file = open(zip_file, "rb")
+		return response
+	finally:
+		if zip_file:
+			os.remove(zip_file)
+
+@view_config(context=ICourseInstance)
+@view_config(context=ICourseCatalogEntry)
+@view_defaults(route_name='objects.generic.traversal',
+			   renderer='rest',
+			   name='Export',
+			   permission=nauth.ACT_NTI_ADMIN)
+class CourseExportView(AbstractAuthenticatedView):
+
+	def __call__(self):
+		return _export_course_response(self.context, self.request.response)
+	
 @view_config(route_name='objects.generic.traversal',
 			 renderer='rest',
 			 name='ExportCourse',
 			 context=CourseAdminPathAdapter,
 			 permission=nauth.ACT_NTI_ADMIN)
-class ExportCourseView(AbstractAuthenticatedView,
-				 	   ModeledContentUploadRequestUtilsMixin):
+class AdminExportCourseView(AbstractAuthenticatedView,
+				 			ModeledContentUploadRequestUtilsMixin):
 
 	def readInput(self, value=None):
 		if self.request.body:
 			result = ModeledContentUploadRequestUtilsMixin.readInput(self, value=value)
-			return CaseInsensitiveDict(result)
 		else:
-			return CaseInsensitiveDict(self.request.params)
+			result = self.request.params
+		return CaseInsensitiveDict(result)
 
 	def __call__(self):
-		zip_file = None
 		values = self.readInput()
 		context = _parse_course(values)
-		course = ICourseInstance(context)
-		filer = ICourseExportFiler(course)
-		try:
-			# prepare filer
-			filer.prepare()
-
-			# export course
-			exporter = component.getUtility(ICourseExporter)
-			exporter.export(course, filer)
-
-			# zip contents
-			zip_file = filer.asZip(path=tempfile.mkdtemp())
-			filename = os.path.split(zip_file)[1]
-
-			response = self.request.response
-			response.content_encoding = str('identity')
-			response.content_type = str('application/zip; charset=UTF-8')
-			response.content_disposition = str('attachment; filename="%s"' % filename)
-			response.body_file = open(zip_file, "rb")
-			return response
-		finally:
-			filer.reset()
-			if zip_file:
-				os.remove(zip_file)
+		return _export_course_response(context, self.request.response)
