b749f37171957ad426265d262f09b637a4229d46
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-08-21 16:15:31 +0000

    remove deprecation
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@94899 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/views/course_views.py
+++ src/nti/app/products/courseware/views/course_views.py
@@ -169,10 +169,10 @@ from nti.appserver.interfaces import IIntIdUserSearchPolicy
 
 from nti.app.externalization.view_mixins import BatchingUtilsMixin
 
-from nti.common.property import alias
-
 from nti.dataserver.interfaces import IUser
 from nti.dataserver.users.interfaces import IFriendlyNamed
+
+from nti.property.property import alias
 
 @interface.implementer(IPathAdapter)
 @component.adapter(ICourseInstance, IRequest)
