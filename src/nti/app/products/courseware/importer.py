caf3c6aac3e411589552e7f7d1f69d3074d23c48
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-09-20 15:01:55 +0000

    remove setting site manager during import
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@97085 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/importer.py
+++ src/nti/app/products/courseware/importer.py
@@ -35,8 +35,6 @@ from nti.contenttypes.courses.interfaces import ICourseCatalog
 from nti.contenttypes.courses.interfaces import ICourseImporter
 from nti.contenttypes.courses.interfaces import ICourseInstance
 
-from nti.contenttypes.courses.utils import set_course_site_manager
-
 from nti.contenttypes.presentation.interfaces import INTIMedia
 from nti.contenttypes.presentation.interfaces import IConcreteAsset
 from nti.contenttypes.presentation.interfaces import INTILessonOverview
@@ -164,7 +162,6 @@ def create_course(admin, key, archive_path, catalog=None, writeout=True, lockout
 			course = ContentCourseInstance()
 			course.root = course_root
 			administrative_level[key] = course # gain intid
-			set_course_site_manager(course) # add site manager
 
 			# let's check for subinstances
 			archive_sec_path = os.path.expanduser(tmp_path or archive_path)
@@ -199,7 +196,6 @@ def create_course(admin, key, archive_path, catalog=None, writeout=True, lockout
 					subinstance = ContentCourseSubInstance()
 					subinstance.root = sub_section_root
 					course.SubInstances[name] = subinstance # register
-					set_course_site_manager(course) # add site manager
 
 		# process
 		_execute(course, tmp_path or archive_path, writeout, lockout)
