c3127643b5331b1757b683c3f2dc0060ed5c7a78
Josh Zuech <josh.zuech@nextthought.com>
2016-10-12 18:26:31 +0000

    Skip anon test for now
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@98582 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/tests/test_catalog_views.py
+++ src/nti/app/products/courseware/tests/test_catalog_views.py
@@ -9,6 +9,8 @@ __docformat__ = "restructuredtext en"
 from hamcrest import has_entry
 from hamcrest import assert_that
 
+import unittest
+
 from nti.app.products.courseware.tests import PersistentInstructedCourseApplicationTestLayer
 
 from nti.app.testing.application_webtest import ApplicationLayerTest
@@ -23,23 +25,24 @@ class TestCatalogViews(ApplicationLayerTest):
 
 	default_origin = b'http://janux.ou.edu'
 
+	@unittest.skip("Still work-in-progress")
 	@WithSharedApplicationMockDS(testapp=True, users=True, default_authenticate=False)
 	def test_anonymously_available_courses_view(self):
 		anonymous_instances_url = '/dataserver2/_AnonymouslyButNotPubliclyAvailableCourseInstances'
 
 
-		#Anonymous requests that aren't our special classifier prompt for auth
+		# Anonymous requests that aren't our special classifier prompt for auth
 		unauthed_environ = self._make_extra_environ(username=None)
 		self.testapp.get(anonymous_instances_url, extra_environ=unauthed_environ, status=401)
 
-		#authed users also can't login
+		# authed users also can't login
 		with mock_dataserver.mock_db_trans(self.ds):
 			self._create_user('ichigo')
 
 		unauthed_environ = self._make_extra_environ(username='ichigo')
 		self.testapp.get(anonymous_instances_url, extra_environ=unauthed_environ, status=403)
 
-		#unauthed requests that have our special classifier are allowed
+		# unauthed requests that have our special classifier are allowed
 		extra_environ = self._make_extra_environ(username=None)
 		extra_environ['nti.paste.testing.classification'] = 'application-tvos'
 		result = self.testapp.get(anonymous_instances_url, extra_environ=extra_environ, status=200)
