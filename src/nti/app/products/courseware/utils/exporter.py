8d2e7974f61e147f3989d9ff32d4c3be25ea173c
Josh Zuech <josh.zuech@nextthought.com>
2016-12-19 19:33:41 +0000

    Do not append ASSETS to path if already starts with ASSETS
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@102882 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/utils/exporter.py
+++ src/nti/app/products/courseware/utils/exporter.py
@@ -49,13 +49,14 @@ def save_resources_to_filer(provided, obj, filer, ext_obj=None):
 				path = os.path.split(path)[0]  # remove resource name
 				path = path[1:] if path.startswith('/') else path
 				if 		not path.startswith(IMAGES_FOLDER) \
-					and not path.startswith(DOCUMENTS_FOLDER):
+					and not path.startswith(DOCUMENTS_FOLDER) \
+					and not path.startswith(ASSETS_FOLDER):
 					path = os.path.join(ASSETS_FOLDER, path)
 			else:
 				path = ASSETS_FOLDER
 			# save resource
-			filer.save(resource.name, 
-					   resource, 
+			filer.save(resource.name,
+					   resource,
 					   bucket=path,
 					   context=obj,
 					   overwrite=True,
