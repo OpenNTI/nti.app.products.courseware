3d98c8a28909a71a14739d33c0d60cb27facf625
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-15 05:31:37 +0000

    pylint;pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@104370 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/legacy_principalrole.py
+++ src/nti/app/products/courseware/legacy_principalrole.py
@@ -27,37 +27,38 @@ from nti.contenttypes.courses.principalrole import CourseInstancePrincipalRoleMa
 
 from nti.property.property import CachedProperty
 
+
 @interface.implementer(IPrincipalRoleMap)
 @component.adapter(ILegacyCommunityBasedCourseInstance)
 class LegacyCourseInstancePrincipalRoleMap(CourseInstancePrincipalRoleMap):
 
-	@CachedProperty
-	def _mapped_principals(self):
-		# Sometimes we might be hit in a site that doesn't have
-		# courses registered in the catalog (e.g., decorating some
-		# object in the stream of a user who can use multiple sites).
-		# In that case, no one has any roles or access to data.
-		# This should only happen in test environments.
-		entry = ICourseCatalogEntry(self.context, None)
-		if entry is None:
-			return ()
+    TA = 'Teaching Assistant'
 
-		# We prefer to iterate across the Instructors
-		# in the entry, as that's a list, possibly where
-		# order matters
-		result = []
-		principals = {x.id.lower(): x for x in self.context.instructors}
-		for info in entry.Instructors:
-			username = info.username.lower()
-			if username in principals:
-				result.append((principals[username],
-							   RID_TA if info.JobTitle == 'Teaching Assistant' else RID_INSTRUCTOR))
-		return result
+    @CachedProperty
+    def _mapped_principals(self):
+        # Sometimes we might be hit in a site that doesn't have
+        # courses registered in the catalog (e.g., decorating some
+        # object in the stream of a user who can use multiple sites).
+        # In that case, no one has any roles or access to data.
+        # This should only happen in test environments.
+        entry = ICourseCatalogEntry(self.context, None)
+        if entry is None:
+            return ()
 
-	def _principals_for_ta(self):
-		result = [x[0] for x in self._mapped_principals if x[1] == RID_TA]
-		return result
+        # We prefer to iterate across the Instructors
+        # in the entry, as that's a list, possibly where
+        # order matters
+        result = []
+        principals = {x.id.lower(): x for x in self.context.instructors}
+        for info in entry.Instructors:
+            username = info.username.lower()
+            if username in principals:
+                result.append((principals[username],
+                               RID_TA if info.JobTitle == self.TA else RID_INSTRUCTOR))
+        return result
 
-	def _principals_for_instructor(self):
-		result = [x[0] for x in self._mapped_principals if x[1] == RID_INSTRUCTOR]
-		return result
+    def _principals_for_ta(self):
+        return [x[0] for x in self._mapped_principals if x[1] == RID_TA]
+
+    def _principals_for_instructor(self):
+        return [x[0] for x in self._mapped_principals if x[1] == RID_INSTRUCTOR]
--- src/nti/app/products/courseware/policies.py
+++ src/nti/app/products/courseware/policies.py
@@ -22,20 +22,21 @@ from nti.dataserver import authorization as nauth
 
 from nti.dataserver.interfaces import IUser
 
+
 @component.adapter(IUser)
 @interface.implementer(IUserCapabilityFilter)
 class AdvancedEditingCapabilityFilter(object):
-	"""
-	Removes the Advanced Editing capability for users that don't have
-	global edit permissions.
-	"""
+    """
+    Removes the Advanced Editing capability for users that don't have
+    global edit permissions.
+    """
 
-	def __init__(self, context=None):
-		self.user = context
+    def __init__(self, context=None):
+        self.user = context
 
-	def filterCapabilities(self, capabilities):
-		result = set(capabilities)
-		if 		self.user is None \
-			or	not has_permission(nauth.ACT_CONTENT_EDIT, self.user.__parent__):
-			result.discard('nti.platform.courseware.advanced_editing')
-		return result
+    def filterCapabilities(self, capabilities):
+        result = set(capabilities)
+        if 		self.user is None \
+                or not has_permission(nauth.ACT_CONTENT_EDIT, self.user.__parent__):
+            result.discard('nti.platform.courseware.advanced_editing')
+        return result
