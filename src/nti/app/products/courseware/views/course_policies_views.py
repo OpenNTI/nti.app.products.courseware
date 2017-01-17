fa2d57928bfb1e0544e132a639a8e98d9f9b631f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-17 17:27:14 +0000

    add post view
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@104594 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/views/course_policies_views.py
+++ src/nti/app/products/courseware/views/course_policies_views.py
@@ -9,6 +9,8 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from zope import lifecycleevent
+
 from pyramid import httpexceptions as hexc
 
 from pyramid.view import view_config
@@ -20,7 +22,12 @@ from nti.app.products.courseware.views import VIEW_COURSE_GRADING_POLICY
 
 from nti.appserver.pyramid_authorization import has_permission
 
-from nti.app.products.gradebook.grading import find_grading_policy_for_course
+from nti.appserver.ugd_edit_views import UGDPostView
+
+from nti.contenttypes.courses.grading import set_grading_policy_for_course
+from nti.contenttypes.courses.grading import find_grading_policy_for_course
+
+from nti.contenttypes.courses.grading.interfaces import ICourseGradingPolicy
 
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
@@ -30,6 +37,7 @@ from nti.contenttypes.courses.utils import is_course_instructor
 from nti.dataserver import authorization as nauth
 
 from nti.property.property import Lazy
+
 
 @view_config(context=ICourseInstance)
 @view_config(context=ICourseCatalogEntry)
@@ -52,4 +60,35 @@ class CourseGradingPolicyGetView(AbstractAuthenticatedView):
         policy = find_grading_policy_for_course(self._course)
         if policy is None:
             raise hexc.HTTPNotFound()
+        return policy
+
+
+@view_config(context=ICourseInstance)
+@view_config(context=ICourseCatalogEntry)
+@view_defaults(route_name='objects.generic.traversal',
+               renderer='rest',
+               request_method='POST',
+               permission=nauth.ACT_CONTENT_EDIT)
+class CourseDiscussionsPostView(UGDPostView):
+
+    content_predicate = ICourseGradingPolicy.providedBy
+
+    @Lazy
+    def _course(self):
+        return ICourseInstance(self.context)
+
+    def _do_call(self):
+        if      not is_course_instructor(self._course, self.remoteUser) \
+            and not has_permission(nauth.ACT_NTI_ADMIN, self._course, self.request):
+            raise hexc.HTTPForbidden()
+
+        creator = self.remoteUser
+        policy = self.readCreateUpdateContentObject(creator,
+                                                    search_owner=False)
+        policy.creator = creator.username
+
+        set_grading_policy_for_course(self._course, policy)
+        lifecycleevent.created(self._course)
+
+        self.request.response.status_int = 201
         return policy
