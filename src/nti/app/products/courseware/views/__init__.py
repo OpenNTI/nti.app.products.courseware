73e6c89af14a54c13f79dd5149df62da8eaac2b5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-17 16:51:43 +0000

    simple get view
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@104588 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/views/__init__.py
+++ src/nti/app/products/courseware/views/__init__.py
@@ -41,10 +41,10 @@ from nti.app.products.courseware import VIEW_LESSONS_CONTAINERS
 from nti.app.products.courseware import ACCEPT_COURSE_INVITATION
 from nti.app.products.courseware import ACCEPT_COURSE_INVITATIONS
 from nti.app.products.courseware import VIEW_COURSE_ACCESS_TOKENS
+from nti.app.products.courseware import VIEW_COURSE_GRADING_POLICY
 from nti.app.products.courseware import CHECK_COURSE_INVITATIONS_CSV
 from nti.app.products.courseware import VIEW_COURSE_RECURSIVE_BUCKET
 from nti.app.products.courseware import VIEW_COURSE_ENROLLMENT_ROSTER
-
 
 @interface.implementer(IPathAdapter)
 class CourseAdminPathAdapter(Contained):
--- src/nti/app/products/courseware/views/course_policies_views.py
+++ src/nti/app/products/courseware/views/course_policies_views.py
@@ -9,3 +9,47 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from pyramid import httpexceptions as hexc
+
+from pyramid.view import view_config
+from pyramid.view import view_defaults
+
+from nti.app.base.abstract_views import AbstractAuthenticatedView
+
+from nti.app.products.courseware.views import VIEW_COURSE_GRADING_POLICY
+
+from nti.appserver.pyramid_authorization import has_permission
+
+from nti.app.products.gradebook.grading import find_grading_policy_for_course
+
+from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
+
+from nti.contenttypes.courses.utils import is_course_instructor
+
+from nti.dataserver import authorization as nauth
+
+from nti.property.property import Lazy
+
+@view_config(context=ICourseInstance)
+@view_config(context=ICourseCatalogEntry)
+@view_defaults(route_name='objects.generic.traversal',
+               renderer='rest',
+               request_method='GET',
+               name=VIEW_COURSE_GRADING_POLICY,
+               permission=nauth.ACT_CONTENT_EDIT)
+class CourseGradingPolicyGetView(AbstractAuthenticatedView):
+
+    @Lazy
+    def _course(self):
+        return ICourseInstance(self.context)
+
+    def __call__(self):
+        if      not is_course_instructor(self._course, self.remoteUser) \
+            and not has_permission(nauth.ACT_NTI_ADMIN, self._course, self.request):
+            raise hexc.HTTPForbidden()
+
+        policy = find_grading_policy_for_course(self._course)
+        if policy is None:
+            raise hexc.HTTPNotFound()
+        return policy
