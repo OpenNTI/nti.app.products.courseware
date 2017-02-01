79d4a53b9cf66b3e1fef47ad572801a180e89e54
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-01 20:41:07 +0000

    untested view to update a course pkg bundle
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@105658 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/products/courseware/views/course_bundle_views.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope.event import notify as z_notify
+
+from pyramid import httpexceptions as hexc
+
+from pyramid.view import view_config
+from pyramid.view import view_defaults
+
+from nti.appserver.ugd_edit_views import UGDPutView
+
+from nti.app.externalization.error import raise_json_error
+
+from nti.app.products.courseware import MessageFactory as _
+
+from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseContentPackageBundle
+
+from nti.contenttypes.courses.interfaces import CourseBundleUpdatedEvent
+
+from nti.dataserver import authorization as nauth
+
+from nti.traversal.traversal import find_interface
+
+
+@view_config(context=ICourseContentPackageBundle)
+@view_defaults(route_name='objects.generic.traversal',
+               renderer='rest',
+               request_method='PUT',
+               permission=nauth.ACT_UPDATE)
+class ContentPackageBundlePutView(UGDPutView):
+
+    def __call__(self):
+        old_packages = set(self.context.ContentPackages or ())
+        try:
+            result = UGDPutView.__call__(self)
+        except KeyError:
+            raise_json_error(
+                self.request,
+                hexc.HTTPConflict,
+                {
+                    u'message': _("Content Package Not Found."),
+                    u'code': 'ContentPackageNotFound',
+                },
+                None)
+        new_packages = set(self.context.ContentPackages or ())
+        removed = old_packages.difference(new_packages)
+        added = new_packages.difference(old_packages)
+        if added or removed:
+            course = find_interface(result, ICourseInstance, stric=False)
+            z_notify(CourseBundleUpdatedEvent(course, added, removed))
+        return result
