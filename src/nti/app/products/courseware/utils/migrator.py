f71f1dac99a36e1a3cf1c1d8cdf4adf2d2ec5fe0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-08 03:13:53 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@108227 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/utils/migrator.py
+++ src/nti/app/products/courseware/utils/migrator.py
@@ -29,88 +29,90 @@ from nti.dataserver.users import User
 
 from nti.ntiids.ntiids import find_object_with_ntiid
 
+
 def course_enrollment_migrator(context=None, ntiid=None, scope=ES_PUBLIC,
-							   max_seat_count=25, sections=(),
-							   dry_run=False, events=True, verbose=False):
+                               max_seat_count=25, sections=(),
+                               dry_run=False, events=True, verbose=False):
 
-	if context is None:
-		context = find_object_with_ntiid(ntiid or u'')
-		instance = ICourseInstance(context, None)
-		if instance is None:
-			catalog = component.getUtility(ICourseCatalog)
-			try:
-				context = catalog.getCatalogEntry(ntiid)
-			except KeyError:
-				pass
-	instance = ICourseInstance(context, None)
-	if instance is None:
-		raise ValueError("Course cannot be found")
+    if context is None:
+        context = find_object_with_ntiid(ntiid or u'')
+        instance = ICourseInstance(context, None)
+        if instance is None:
+            catalog = component.getUtility(ICourseCatalog)
+            try:
+                context = catalog.getCatalogEntry(ntiid)
+            except KeyError:
+                pass
+    instance = ICourseInstance(context, None)
+    if instance is None:
+        raise ValueError("Course cannot be found")
 
-	parent = course = instance
-	if ICourseSubInstance.providedBy(course):
-		parent = course.__parent__.__parent__
+    parent = course = instance
+    if ICourseSubInstance.providedBy(course):
+        parent = course.__parent__.__parent__
 
-	if not sections:
-		sections = list(parent.SubInstances.keys())
+    if not sections:
+        sections = list(parent.SubInstances.keys())
 
-	items = []
-	for section in sections:
-		if section not in parent.SubInstances:
-			raise KeyError("Invalid section", section)
-		sub_instance = parent.SubInstances[section]
-		count = ICourseEnrollments(sub_instance).count_enrollments()
-		items.append(SectionSeat(section, count))
+    items = []
+    for section in sections:
+        if section not in parent.SubInstances:
+            raise KeyError("Invalid section", section)
+        sub_instance = parent.SubInstances[section]
+        count = ICourseEnrollments(sub_instance).count_enrollments()
+        items.append(SectionSeat(section, count))
 
-	items.sort()
-	source_enrollments = IDefaultCourseInstanceEnrollmentStorage(course)
+    items.sort()
+    source_enrollments = IDefaultCourseInstanceEnrollmentStorage(course)
 
-	count = 0
-	log = logger.warn if not verbose else logger.info
+    count = 0
+    log = logger.warn if not verbose else logger.info
 
-	for source_prin_id in list(source_enrollments):
+    for source_prin_id in list(source_enrollments):
 
-		if not source_prin_id or User.get_user(source_prin_id) is None:
-			# dup enrollment
-			continue
+        if not source_prin_id or User.get_user(source_prin_id) is None:
+            # dup enrollment
+            continue
 
-		source_enrollment = source_enrollments[source_prin_id]
-		if source_enrollment is None or source_enrollment.Scope != scope:
-			continue
+        source_enrollment = source_enrollments[source_prin_id]
+        if source_enrollment is None or source_enrollment.Scope != scope:
+            continue
 
-		if IPrincipal(source_enrollment.Principal, None) is None:
-			log("Ignoring dup enrollment for %s", source_prin_id)
-			continue
+        if IPrincipal(source_enrollment.Principal, None) is None:
+            log("Ignoring dup enrollment for %s", source_prin_id)
+            continue
 
-		index = 0
-		section = None
-		for idx, item in enumerate(items):
-			section_name, estimated_seat_count = item.section_name, item.seat_count
-			if estimated_seat_count < max_seat_count:
-				index = idx
-				section = parent.SubInstances[section_name]
-				break
+        index = 0
+        section = None
+        for idx, item in enumerate(items):
+            section_name, estimated_seat_count = item.section_name, item.seat_count
+            if estimated_seat_count < max_seat_count:
+                index = idx
+                section = parent.SubInstances[section_name]
+                break
 
-		if section is None:
-			index = 0
-			items.sort()
-			section_name = items[0].section_name
-			section = parent.SubInstances[section_name]
+        if section is None:
+            index = 0
+            items.sort()
+            section_name = items[0].section_name
+            section = parent.SubInstances[section_name]
 
-		dest_enrollments = IDefaultCourseInstanceEnrollmentStorage(section)
-		if source_prin_id in dest_enrollments:
-			continue
+        dest_enrollments = IDefaultCourseInstanceEnrollmentStorage(section)
+        if source_prin_id in dest_enrollments:
+            continue
 
-		if not dry_run:
-			if events:
-				lifecycleevent.removed(source_enrollment)
-			mover = IObjectMover(source_enrollment)  # notify ObjectMovedEvent
-			mover.moveTo(dest_enrollments)
-			if events:
-				lifecycleevent.added(source_enrollment)
+        if not dry_run:
+            if events:
+                lifecycleevent.removed(source_enrollment)
+            mover = IObjectMover(source_enrollment)  # notify ObjectMovedEvent
+            mover.moveTo(dest_enrollments)
+            if events:
+                lifecycleevent.added(source_enrollment)
 
-		count += 1
-		items[index].seat_count += 1
-		log("Move enrollment for principal %s to section %s", source_prin_id,
-			section_name)
+        count += 1
+        items[index].seat_count += 1
+        log("Move enrollment for principal %s to section %s", 
+            source_prin_id,
+            section_name)
 
-	return (items, count)
+    return (items, count)
