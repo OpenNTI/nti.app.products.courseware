4dad9ad1524f85cde3facc6e443372e4776c9ecc
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-07 02:59:02 +0000

    use six
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@112213 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/views/_utils.py
+++ src/nti/app/products/courseware/views/_utils.py
@@ -44,169 +44,179 @@ from nti.ntiids.ntiids import find_object_with_ntiid
 
 from nti.site.site import get_component_hierarchy_names
 
+
 def _get_self_assessments_for_course(course):
-	"""
-	Given an :class:`.ICourseInstance`, return a list of all
-	the \"self assessments\" in the course. Self-assessments are
-	defined as top-level question sets that are not used within an assignment
-	in the course.
-	"""
+    """
+    Given an :class:`.ICourseInstance`, return a list of all
+    the \"self assessments\" in the course. Self-assessments are
+    defined as top-level question sets that are not used within an assignment
+    in the course.
+    """
 
-	# NOTE: This is pretty tightly coupled to the implementation
-	# and the use of one content package (?). See NonAssignmentsByOutlineNodeDecorator
-	# (TODO: Find a way to unify this)
-	catalog = ICourseAssessmentItemCatalog(course)
+    # NOTE: This is pretty tightly coupled to the implementation
+    # and the use of one content package (?). See NonAssignmentsByOutlineNodeDecorator
+    # (TODO: Find a way to unify this)
+    catalog = ICourseAssessmentItemCatalog(course)
 
-	# Not only must we filter out assignments, we must filter out the
-	# question sets that they refer to; we assume such sets are only
-	# used by the assignment.
+    # Not only must we filter out assignments, we must filter out the
+    # question sets that they refer to; we assume such sets are only
+    # used by the assignment.
 
-	result = list()
-	qsids_to_strip = set()
+    result = list()
+    qsids_to_strip = set()
 
-	for item in catalog.iter_assessment_items():
-		if IQAssignment.providedBy(item):
-			qsids_to_strip.add(item.ntiid)
-			for assignment_part in item.parts:
-				question_set = assignment_part.question_set
-				qsids_to_strip.add(question_set.ntiid)
-				for question in question_set.questions:
-					qsids_to_strip.add(question.ntiid)
-		elif not IQuestionSet.providedBy(item):
-			qsids_to_strip.add(item.ntiid)
-		else:
-			result.append(item)
+    for item in catalog.iter_assessment_items():
+        if IQAssignment.providedBy(item):
+            qsids_to_strip.add(item.ntiid)
+            for assignment_part in item.parts:
+                question_set = assignment_part.question_set
+                qsids_to_strip.add(question_set.ntiid)
+                for question in question_set.questions:
+                    qsids_to_strip.add(question.ntiid)
+        elif not IQuestionSet.providedBy(item):
+            qsids_to_strip.add(item.ntiid)
+        else:
+            result.append(item)
 
-	# Now remove the forbidden
-	result = [x for x in result if x.ntiid not in qsids_to_strip]
-	return result
+    # Now remove the forbidden
+    return [x for x in result if x.ntiid not in qsids_to_strip]
+
 
 def _get_self_polls_for_course(course):
-	"""
-	Given an :class:`.ICourseInstance`, return a list of all
-	the \"self polls\" in the course. Self-polls are
-	defined as top-level polls sets that are not used within an survey
-	in the course.
-	"""
+    """
+    Given an :class:`.ICourseInstance`, return a list of all
+    the \"self polls\" in the course. Self-polls are
+    defined as top-level polls sets that are not used within an survey
+    in the course.
+    """
 
-	result = list()
-	qsids_to_strip = set()
-	catalog = ICourseAssessmentItemCatalog(course)
+    result = list()
+    qsids_to_strip = set()
+    catalog = ICourseAssessmentItemCatalog(course)
 
-	for item in catalog.iter_assessment_items():
-		if IQSurvey.providedBy(item):
-			qsids_to_strip.add(item.ntiid)
-			for poll in item.questions:
-				qsids_to_strip.add(poll.ntiid)
-		elif not IQPoll.providedBy(item):
-			qsids_to_strip.add(item.ntiid)
-		else:
-			result.append(item)
+    for item in catalog.iter_assessment_items():
+        if IQSurvey.providedBy(item):
+            qsids_to_strip.add(item.ntiid)
+            for poll in item.questions:
+                qsids_to_strip.add(poll.ntiid)
+        elif not IQPoll.providedBy(item):
+            qsids_to_strip.add(item.ntiid)
+        else:
+            result.append(item)
 
-	# Now remove the forbidden
-	result = [x for x in result if x.ntiid not in qsids_to_strip]
-	return result
+    # Now remove the forbidden
+    return [x for x in result if x.ntiid not in qsids_to_strip]
+
 
 def _do_get_containers_in_course(context):
-	course = ICourseInstance(context)
-	packages = get_course_packages(course)
+    course = ICourseInstance(context)
+    packages = get_course_packages(course)
 
-	def _recur(node, accum):
-		# Get our embedded ntiids and recursively
-		# fetch our children's ntiids
-		ntiid = node.ntiid
-		try:
-			accum.update(node.embeddedContainerNTIIDs)
-		except AttributeError:
-			pass
+    def _recur(node, accum):
+        # Get our embedded ntiids and recursively
+        # fetch our children's ntiids
+        ntiid = node.ntiid
+        try:
+            accum.update(node.embeddedContainerNTIIDs)
+        except AttributeError:
+            pass
 
-		if ntiid:
-			accum.add(ntiid)
+        if ntiid:
+            accum.add(ntiid)
 
-		for n in node.children:
-			_recur(n, accum)
+        for n in node.children:
+            _recur(n, accum)
 
-	containers_in_course = set()
-	for package in packages:
-		_recur(package, containers_in_course)
+    containers_in_course = set()
+    for package in packages:
+        _recur(package, containers_in_course)
 
-	# Now fetch from our index
-	catalog = get_library_catalog()
-	if catalog is not None:
-		sites = get_component_hierarchy_names()
-		package_ntiids = (x.ntiid for x in packages)
-		contained_objs = catalog.search_objects(container_ntiids=package_ntiids,
-												container_all_of=False,
-												sites=sites)
-		# Do we need target_ntiid here?
-		contained_ntiids = (x.ntiid for x in contained_objs)
-		containers_in_course.update(contained_ntiids)
+    # Now fetch from our index
+    catalog = get_library_catalog()
+    if catalog is not None:
+        sites = get_component_hierarchy_names()
+        package_ntiids = (x.ntiid for x in packages)
+        contained_objs = catalog.search_objects(container_ntiids=package_ntiids,
+                                                container_all_of=False,
+                                                sites=sites)
+        # Do we need target_ntiid here?
+        contained_ntiids = (x.ntiid for x in contained_objs)
+        containers_in_course.update(contained_ntiids)
 
-	# Add in our self-assessments
-	catalog = ICourseAssessmentItemCatalog(course)
-	ntiids = (x.ntiid for x in catalog.iter_assessment_items())
-	containers_in_course = containers_in_course.union(ntiids)
+    # Add in our self-assessments
+    catalog = ICourseAssessmentItemCatalog(course)
+    ntiids = (x.ntiid for x in catalog.iter_assessment_items())
+    containers_in_course = containers_in_course.union(ntiids)
 
-	self_assessments = _get_self_assessments_for_course(course)
-	self_assessment_qsids = (x.ntiid for x in self_assessments)
-	self_assessment_containerids = (x.__parent__.ntiid for x in self_assessments
-									if hasattr(x.__parent__, 'ntiid'))
-	containers_in_course = containers_in_course.union(self_assessment_containerids)
-	containers_in_course = containers_in_course.union(self_assessment_qsids)
+    self_assessments = _get_self_assessments_for_course(course)
+    self_assessment_qsids = (x.ntiid for x in self_assessments)
+    self_assessment_containerids = (x.__parent__.ntiid for x in self_assessments
+                                    if hasattr(x.__parent__, 'ntiid'))
+    containers_in_course = containers_in_course.union(
+        self_assessment_containerids)
+    containers_in_course = containers_in_course.union(self_assessment_qsids)
 
-	# Add in our assignments
-	assignment_catalog = ICourseAssignmentCatalog(course)
-	assignment_ntiids = (asg.ntiid for asg in assignment_catalog.iter_assignments())
-	containers_in_course = containers_in_course.union(assignment_ntiids)
-	containers_in_course.discard(None)
-	return containers_in_course
+    # Add in our assignments
+    assignment_catalog = ICourseAssignmentCatalog(course)
+    assignment_ntiids = (
+        asg.ntiid for asg in assignment_catalog.iter_assignments()
+    )
+    containers_in_course = containers_in_course.union(assignment_ntiids)
+    containers_in_course.discard(None)
+    return containers_in_course
+
 
 def _get_containers_in_course(context):
-	site = getSite().__name__
-	entry = ICourseCatalogEntry(context)
-	key = "/course/%s" % encode_keys(site, entry.ntiid, "containers", last_synchronized())
-	containers = memcache_get(key)
-	if containers is None:
-		containers = _do_get_containers_in_course(context)
-		memcache_set(key, containers)
-	return containers
+    site = getSite().__name__
+    entry = ICourseCatalogEntry(context)
+    key = "/course/%s" % encode_keys(site, entry.ntiid,
+                                     "containers", last_synchronized())
+    containers = memcache_get(key)
+    if containers is None:
+        containers = _do_get_containers_in_course(context)
+        memcache_set(key, containers)
+    return containers
+
 
 def _tx_string(s):
-	if s and isinstance(s, unicode):
-		s = s.encode('utf-8')
-	return s
+    if s and isinstance(s, six.text_type):
+        s = s.encode('utf-8')
+    return s
+
 
 def _parse_user(values):
-	username = values.get('username') or values.get('user')
-	if not username:
-		raise hexc.HTTPUnprocessableEntity(detail='No username')
+    username = values.get('username') or values.get('user')
+    if not username:
+        raise hexc.HTTPUnprocessableEntity(detail='No username')
 
-	user = User.get_user(username)
-	if not user or not IUser.providedBy(user):
-		raise hexc.HTTPUnprocessableEntity(detail='User not found')
+    user = User.get_user(username)
+    if not user or not IUser.providedBy(user):
+        raise hexc.HTTPUnprocessableEntity(detail='User not found')
 
-	return username, user
+    return username, user
+
 
 def _parse_courses(values):
-	# get validate course entry
-	ntiids = values.get('ntiid') or values.get('ntiids')
-	if not ntiids:
-		raise hexc.HTTPUnprocessableEntity(detail='No course entry identifier')
+    # get validate course entry
+    ntiids = values.get('ntiid') or values.get('ntiids')
+    if not ntiids:
+        raise hexc.HTTPUnprocessableEntity(detail='No course entry identifier')
 
-	if isinstance(ntiids, six.string_types):
-		ntiids = ntiids.split()
+    if isinstance(ntiids, six.string_types):
+        ntiids = ntiids.split()
 
-	result = []
-	for ntiid in ntiids:
-		context = find_object_with_ntiid(ntiid)
-		if not ILegacyCourseInstance.providedBy(context):
-			context = ICourseCatalogEntry(context, None)
-		if context is not None:
-			result.append(context)
-	return result
+    result = []
+    for ntiid in ntiids:
+        context = find_object_with_ntiid(ntiid)
+        if not ILegacyCourseInstance.providedBy(context):
+            context = ICourseCatalogEntry(context, None)
+        if context is not None:
+            result.append(context)
+    return result
+
 
 def _parse_course(values):
-	result = _parse_courses(values)
-	if not result:
-		raise hexc.HTTPUnprocessableEntity(detail='Course not found')
-	return result[0]
+    result = _parse_courses(values)
+    if not result:
+        raise hexc.HTTPUnprocessableEntity(detail='Course not found')
+    return result[0]
