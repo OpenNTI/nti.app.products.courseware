610a3cbaba11c5fad92a35f97ff8562e9d53776e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-03 03:34:14 +0000

    add back course content unit associations... thx to JZ
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@116428 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/products/courseware/predicates.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.contentlibrary.interfaces import IContentUnit
+from nti.contentlibrary.interfaces import IContentPackage
+from nti.contentlibrary.interfaces import IContentUnitAssociations
+
+from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
+
+from nti.contenttypes.courses.utils import get_courses_for_packages
+
+from nti.traversal.traversal import find_interface
+
+
+@component.adapter(IContentUnit)
+@interface.implementer(IContentUnitAssociations)
+class _CourseContentUnitAssociations(object):
+
+    def __init__(self, *args):
+        pass
+
+    def associations(self, context):
+        result = []
+        package = find_interface(context, IContentPackage, strict=False)
+        if package is not None:
+            courses = get_courses_for_packages(packages=(package.ntiid,))
+            for course in courses or ():
+                # favor catalog entries
+                entry = ICourseCatalogEntry(course, None)
+                if entry is not None:
+                    result.append(entry)
+                else:
+                    result.append(course)
+        return result or ()
