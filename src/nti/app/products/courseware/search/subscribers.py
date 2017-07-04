0d8dce41461000e8c1197a6dd88c50101239d31f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-04 18:28:10 +0000

    correctly resolve course search pkgs
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@116570 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/search/subscribers.py
+++ src/nti/app/products/courseware/search/subscribers.py
@@ -4,7 +4,7 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -26,36 +26,33 @@ from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
 
 from nti.contenttypes.courses.utils import get_enrollment_catalog
 
-from nti.ntiids.ntiids import ROOT
-
 from nti.site.site import get_component_hierarchy_names
 
 
 @interface.implementer(ISearchPackageResolver)
-class _RootSearchPacakgeResolver(object):
+class _CourseSearchPacakgeResolver(object):
 
     __slots__ = ()
 
     def __init__(self, *args):
         pass
 
-    def resolve(self, user, ntiid=None):
+    def resolve(self, user, unused_ntiid=None):
         result = set()
-        if ntiid == ROOT:
-            catalog = get_enrollment_catalog()
-            intids = component.getUtility(IIntIds)
-            site_names = get_component_hierarchy_names()
-            query = {
-                IX_SITE: {'any_of': site_names},
-                IX_USERNAME: {'any_of': (user.username,)}
-            }
-            for uid in catalog.apply(query) or ():
-                context = intids.queryObject(uid)
-                context = ICourseInstance(context, None)
-                for package in get_course_packages(context):
-                    ntiid = package.ntiid or u''
-                    result.add(package.ntiid)
+        catalog = get_enrollment_catalog()
+        intids = component.getUtility(IIntIds)
+        site_names = get_component_hierarchy_names()
+        query = {
+            IX_SITE: {'any_of': site_names},
+            IX_USERNAME: {'any_of': (user.username,)}
+        }
+        for uid in catalog.apply(query) or ():
+            context = intids.queryObject(uid)
+            context = ICourseInstance(context, None)
+            if context is not None:
+                result.update(x.ntiid for x in get_course_packages(context))
                 entry = ICourseCatalogEntry(context, None)
-                result.add(getattr(entry, 'ntiid', None) or u'')
-        result.discard(u'')  # valid ntiids
+                # include catalog entry
+                result.add(getattr(entry, 'ntiid', None))
+        result.discard(None)
         return result
