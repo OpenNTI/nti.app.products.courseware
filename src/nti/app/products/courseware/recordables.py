4c06d03ee0ddeaea55f38d965b87ae61ce7a5ac3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-22 19:32:00 +0000

    add course catalog recordables utility
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@117975 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/products/courseware/recordables.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id: recordables.py 117974 2017-07-22 19:00:33Z carlos.sanchez $
+"""
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.contenttypes.courses.interfaces import ICourseCatalog
+from nti.contenttypes.courses.interfaces import ICourseInstance
+
+from nti.contenttypes.presentation.interfaces import IPresentationAssetContainer
+
+from nti.recorder.interfaces import IRecordable
+from nti.recorder.interfaces import IRecordables
+
+
+@interface.implementer(IRecordables)
+class CourseCatalogRecordables(object):
+
+    __slots__ = ()
+
+    def _process_course(self, course, result):
+        # assets
+        container = IPresentationAssetContainer(course)
+        for asset in container.assets():
+            if IRecordable.providedBy(asset):
+                result.append(asset)
+        # nodes
+        def _recur(node):
+            if IRecordable.providedBy(node):
+                result.append(node)
+            for child in node.values():
+                _recur(child)
+        outline = course.Outline
+        if outline is not None:
+            _recur(outline)
+        return result
+
+    def iter_objects(self):
+        result = []
+        catalog = component.queryUtility(ICourseCatalog)
+        if catalog is not None:
+            for entry in catalog.iterCatalogEntries():
+                course = ICourseInstance(entry)
+                self._process_course(course, result)
+        return result
---
+++ src/nti/app/products/courseware/tests/test_recordables.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import none
+from hamcrest import is_not
+from hamcrest import has_length
+from hamcrest import assert_that
+from hamcrest import greater_than
+does_not = is_not
+
+from zope import component
+
+from nti.recorder.interfaces import IRecordables
+
+from nti.app.products.courseware.tests import PersistentInstructedCourseApplicationTestLayer
+
+from nti.app.testing.application_webtest import ApplicationLayerTest
+
+from nti.app.testing.decorators import WithSharedApplicationMockDS
+
+from nti.dataserver.tests import mock_dataserver
+
+class TestRecordables(ApplicationLayerTest):
+
+    layer = PersistentInstructedCourseApplicationTestLayer
+
+    default_origin = 'http://janux.ou.edu'
+
+    @WithSharedApplicationMockDS(users=False, testapp=False)
+    def test_recordables(self):
+        with mock_dataserver.mock_db_trans(self.ds, site_name='janux.ou.edu'):
+            recordables = component.queryUtility(IRecordables, name="courses")
+            assert_that(recordables, is_not(none()))
+            assert_that(list(recordables.iter_objects()),
+                        has_length(greater_than(500)))
