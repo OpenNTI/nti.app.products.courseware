3f815099a65cfbb1e74e2ee86139e950fb982fb6
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-08-26 16:16:07 +0000

    pep8;ensure unicode
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@120546 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware/notables.py
+++ src/nti/app/products/courseware/notables.py
@@ -6,7 +6,7 @@ Functions and architecture for general activity streams.
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -118,7 +118,7 @@ class _UserPriorityCreatorNotableProvider(object):
     We rely on permissioning to filter out non-relevant entries.
     """
 
-    def __init__(self, user, request):
+    def __init__(self, user, unused_request):
         self.context = user
 
     @CachedProperty
--- src/nti/app/products/courseware/ntiids.py
+++ src/nti/app/products/courseware/ntiids.py
@@ -6,7 +6,7 @@ NTIID support.
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -41,176 +41,186 @@ from nti.ntiids.ntiids import get_provider
 from nti.ntiids.ntiids import escape_provider
 from nti.ntiids.ntiids import find_object_with_ntiid
 
-COURSE_NTIID_PREFIX = 'tag:nextthought.com,2011-10:NTI-CourseInfo-'
+COURSE_NTIID_PREFIX = u'tag:nextthought.com,2011-10:NTI-CourseInfo-'
+
 
 @interface.implementer(INTIIDResolver)
 @interface.named(NTIID_TYPE_COURSE_SECTION_TOPIC)
 class _EnrolledCourseSectionTopicNTIIDResolver(object):
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	# Whether, when we find a match to a course, we return the sub
-	# instance we're actually enrolled in or the parent instance
-	# (the main course)
-	allow_section_match = True
+    # Whether, when we find a match to a course, we return the sub
+    # instance we're actually enrolled in or the parent instance
+    # (the main course)
+    allow_section_match = True
 
-	def _sort_enrollments(self, enrollments):
-		# returns a list of (course, catalog_entry) objects,
-		# with any subinstances I'm enrolled in coming first
-		# if section matches are allowed, otherwise those are
-		# excluded. We also make sure the most recent things are first
-		records = []
-		for record in enrollments.iter_enrollments():
-			try:
-				course = ICourseInstance(record)
-			except TypeError:
-				# Never seen this, being proactive
-				logger.warn("User enrolled %s in stale course",
-							record)
-				continue
+    def _sort_enrollments(self, enrollments):
+        # returns a list of (course, catalog_entry) objects,
+        # with any subinstances I'm enrolled in coming first
+        # if section matches are allowed, otherwise those are
+        # excluded. We also make sure the most recent things are first
+        records = []
+        for record in enrollments.iter_enrollments():
+            try:
+                course = ICourseInstance(record)
+            except TypeError:
+                # Never seen this, being proactive
+                logger.warn("User enrolled %s in stale course",
+                            record)
+                continue
 
-			try:
-				catalog_entry = ICourseCatalogEntry(course, None)
-			except TypeError:
-				# Seen this is alpha, possible due to the early content
-				# shifting before enrollment cleanup was correct?
-				# maybe it can go away
-				logger.warn("User enrolled %r in course %r that no longer has CCE",
-							record, course)
-				continue
+            try:
+                catalog_entry = ICourseCatalogEntry(course, None)
+            except TypeError:
+                # Seen this is alpha, possible due to the early content
+                # shifting before enrollment cleanup was correct?
+                # maybe it can go away
+                logger.warn("User enrolled %r in course %r that no longer has CCE",
+                            record, course)
+                continue
 
-			if catalog_entry is not None:
-				records.append((course, catalog_entry, record))
+            if catalog_entry is not None:
+                records.append((course, catalog_entry, record))
 
-		# stable sort, the key will be the same for all the subinstances
-		# and then all the parent
-		def key(record):
-			ts = 0
-			# admin roles do not have created time, in that case
-			if hasattr(record[2], 'createdTime'):
-				ts = -record[2].createdTime
-			if not ts and catalog_entry is not None and catalog_entry.StartDate:
-				ts = -calendar.timegm(catalog_entry.StartDate.utctimetuple())
-			result = (0 if ICourseSubInstance.providedBy(record[0]) else 1, ts)
-			return result
+        # stable sort, the key will be the same for all the subinstances
+        # and then all the parent
+        def key(record):
+            ts = 0
+            # admin roles do not have created time, in that case
+            if hasattr(record[2], 'createdTime'):
+                ts = -record[2].createdTime
+            if not ts and catalog_entry is not None and catalog_entry.StartDate:
+                ts = -calendar.timegm(catalog_entry.StartDate.utctimetuple())
+            result = (0 if ICourseSubInstance.providedBy(record[0]) else 1, ts)
+            return result
 
-		records.sort(key=key)
-		return records
+        records.sort(key=key)
+        return records
 
-	def _escape_entry_provider(self, entry):
-		result = escape_provider(entry.ProviderUniqueID) if entry is not None else None
-		return result
+    def _escape_entry_provider(self, entry):
+        result = escape_provider(
+            entry.ProviderUniqueID) if entry is not None else None
+        return result
 
-	def _solve_for_iface(self, ntiid, iface, provider_name, user, catalog_entry_matches=None):
+    def _solve_for_iface(self, ntiid, iface, provider_name, user, catalog_entry_matches=None):
 
-		def _catalog_entry_matches(catalog_entry, provider_name):
-			return self._escape_entry_provider(catalog_entry) == provider_name
+        def _catalog_entry_matches(catalog_entry, provider_name):
+            return self._escape_entry_provider(catalog_entry) == provider_name
 
-		catalog_entry_matches = catalog_entry_matches if catalog_entry_matches else _catalog_entry_matches
+        if not catalog_entry_matches:
+            catalog_entry_matches = _catalog_entry_matches
 
-		def _get_ntiid_for_subinstance(ntiid, subinstance, main_course):
-			result = None
-			# First, check section if we do not force them to
-			# use the parent.
-			if 	self.allow_section_match or \
-				INonPublicCourseInstance.providedBy(main_course):
-				result = self._find_in_course(subinstance, ntiid)
+        def _get_ntiid_for_subinstance(ntiid, subinstance, main_course):
+            result = None
+            # First, check section if we do not force them to
+            # use the parent.
+            if     self.allow_section_match \
+                or INonPublicCourseInstance.providedBy(main_course):
+                result = self._find_in_course(subinstance, ntiid)
 
-			# Check main course if we need to.
-			if result is None:
-				result = self._find_in_course(main_course, ntiid)
-			return result
+            # Check main course if we need to.
+            if result is None:
+                result = self._find_in_course(main_course, ntiid)
+            return result
 
-		for enrollments in component.subscribers((user,), iface):
-			for course, catalog_entry, _ in self._sort_enrollments(enrollments):
-				# The webapp is passing the course context here as the provider_name.
-				# This may be necessary to avoid topic name collisions; especially with
-				# instructors enrolled in many sections.
-				# Otherwise, the client will be passing the content specified
-				# section.
-				result = None
-				if catalog_entry_matches(catalog_entry, provider_name):
-					if ICourseSubInstance.providedBy(course):
-						main_course = get_parent_course(course)
-						main_cce = ICourseCatalogEntry(main_course, None)
-						result = _get_ntiid_for_subinstance(ntiid, course, main_course)
-					else:
-						# The ntiid references a top-level course.
-						result = self._find_in_course(course, ntiid)
-				# No? Is it a subcourse? Check the main course to see if it matches.
-				# If it does, we still want to return the most specific
-				# discussions allowed (either our section or, if not allowed, the parent)
-				if result is None and ICourseSubInstance.providedBy(course):
-					main_course = course.__parent__.__parent__
-					main_cce = ICourseCatalogEntry(main_course, None)
-					if catalog_entry_matches(main_cce, provider_name):
-						# Some legacy cases (CHEM_4970) may match provider names.
-						result = _get_ntiid_for_subinstance(ntiid, course, main_course)
+        for enrollments in component.subscribers((user,), iface):
+            for course, catalog_entry, _ in self._sort_enrollments(enrollments):
+                # The webapp is passing the course context here as the provider_name.
+                # This may be necessary to avoid topic name collisions; especially with
+                # instructors enrolled in many sections.
+                # Otherwise, the client will be passing the content specified
+                # section.
+                result = None
+                if catalog_entry_matches(catalog_entry, provider_name):
+                    if ICourseSubInstance.providedBy(course):
+                        main_course = get_parent_course(course)
+                        main_cce = ICourseCatalogEntry(main_course, None)
+                        result = _get_ntiid_for_subinstance(ntiid, course,
+                                                            main_course)
+                    else:
+                        # The ntiid references a top-level course.
+                        result = self._find_in_course(course, ntiid)
+                # No? Is it a subcourse? Check the main course to see if it matches.
+                # If it does, we still want to return the most specific
+                # discussions allowed (either our section or, if not allowed,
+                # the parent)
+                if result is None and ICourseSubInstance.providedBy(course):
+                    main_course = course.__parent__.__parent__
+                    main_cce = ICourseCatalogEntry(main_course, None)
+                    if catalog_entry_matches(main_cce, provider_name):
+                        # Some legacy cases (CHEM_4970) may match provider
+                        # names.
+                        result = _get_ntiid_for_subinstance(ntiid, course,
+                                                            main_course)
 
-				if result is not None:
-					return result
-		return None
+                if result is not None:
+                    return result
+        return None
 
-	def _do_resolve(self, ntiid, user, provider_name, catalog_entry_matches=None):
-		result = self._solve_for_iface(ntiid,
-									   IPrincipalAdministrativeRoleCatalog,
-									   provider_name,
-									   user,
-									   catalog_entry_matches=catalog_entry_matches)
+    def _do_resolve(self, ntiid, user, provider_name, catalog_entry_matches=None):
+        result = self._solve_for_iface(ntiid,
+                                       IPrincipalAdministrativeRoleCatalog,
+                                       provider_name,
+                                       user,
+                                       catalog_entry_matches=catalog_entry_matches)
 
-		# Enrolled
-		if result is None:
-			result = self._solve_for_iface(ntiid,
-										   IPrincipalEnrollments,
-										   provider_name,
-										   user,
-										   catalog_entry_matches=catalog_entry_matches)
-		return result
+        # Enrolled
+        if result is None:
+            result = self._solve_for_iface(ntiid,
+                                           IPrincipalEnrollments,
+                                           provider_name,
+                                           user,
+                                           catalog_entry_matches=catalog_entry_matches)
+        return result
 
-	def resolve(self, ntiid):
-		user = get_remote_user()
-		if user is None:
-			return
+    def resolve(self, ntiid):
+        user = get_remote_user()
+        if user is None:
+            return
 
-		provider_name = get_provider(ntiid)
-		provider_name_ntiid = COURSE_NTIID_PREFIX + provider_name
+        provider_name = get_provider(ntiid)
+        provider_name_ntiid = COURSE_NTIID_PREFIX + provider_name
 
-		result = find_object_with_ntiid(provider_name_ntiid)
-		if result is not None:
-			# Ok we have a course, we should be able to definitively return based
-			# on enrollments.
-			def catalog_entry_matches(catalog_entry, _):
-				return catalog_entry.ntiid == provider_name_ntiid
-			result = self._do_resolve(ntiid,
-									  user,
-									  provider_name,
-									  catalog_entry_matches=catalog_entry_matches)
-		else:
-			# The legacy approach, which may collide across semesters.
-			result = self._do_resolve(ntiid, user, provider_name)
-		return result
+        result = find_object_with_ntiid(provider_name_ntiid)
+        if result is not None:
+            # Ok we have a course, we should be able to definitively return based
+            # on enrollments.
+            def catalog_entry_matches(catalog_entry, _):
+                return catalog_entry.ntiid == provider_name_ntiid
+            result = self._do_resolve(ntiid,
+                                      user,
+                                      provider_name,
+                                      catalog_entry_matches=catalog_entry_matches)
+        else:
+            # The legacy approach, which may collide across semesters.
+            result = self._do_resolve(ntiid, user, provider_name)
+        return result
 
-	def _find_in_course(self, course, ntiid):
-		result = resolve_ntiid_in_board(ntiid, course.Discussions)
-		return result
+    def _find_in_course(self, course, ntiid):
+        result = resolve_ntiid_in_board(ntiid, course.Discussions)
+        return result
+
 
 @interface.implementer(INTIIDResolver)
 @interface.named(NTIID_TYPE_COURSE_TOPIC)
 class _EnrolledCourseRootTopicNTIIDResolver(_EnrolledCourseSectionTopicNTIIDResolver):
 
-	allow_section_match = False  # always the root
+    allow_section_match = False  # always the root
+
 
 @interface.implementer(INTIIDResolver)
 @interface.named(NTIID_TYPE_COURSE_SECTION_FORUM)
 class _EnrolledCourseSectionForumNTIIDResolver(_EnrolledCourseSectionTopicNTIIDResolver):
 
-	def _find_in_course(self, course, ntiid):
-		result = resolve_forum_ntiid_in_board(ntiid, course.Discussions)
-		return result
+    def _find_in_course(self, course, ntiid):
+        result = resolve_forum_ntiid_in_board(ntiid, course.Discussions)
+        return result
+
 
 @interface.implementer(INTIIDResolver)
 @interface.named(NTIID_TYPE_COURSE_FORUM)
 class _EnrolledCourseRootForumNTIIDResolver(_EnrolledCourseSectionForumNTIIDResolver):
 
-	allow_section_match = False  # always the root
+    allow_section_match = False  # always the root
