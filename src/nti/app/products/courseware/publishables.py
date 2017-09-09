9e038b6df01c81e20aa503e90a224e855cea4203
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-09-09 22:59:29 +0000

    add IPublishables utility for courses
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware/trunk@121423 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/products/courseware/publishables.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.contenttypes.courses.interfaces import ICourseCatalog
+from nti.contenttypes.courses.interfaces import ICourseInstance
+
+from nti.contenttypes.presentation.interfaces import IPresentationAssetContainer
+
+from nti.publishing.interfaces import IPublishable
+from nti.publishing.interfaces import IPublishables
+
+
+@interface.implementer(IPublishables)
+class CourseCatalogPublishables(object):
+
+    __slots__ = ()
+
+    def __init__(self, *args):
+        pass
+
+    def _process_course(self, course, result):
+        # assets
+        container = IPresentationAssetContainer(course)
+        for asset in container.assets():
+            if IPublishable.providedBy(asset):
+                result.append(asset)
+        # nodes
+        def _recur(node):
+            if IPublishable.providedBy(node):
+                result.append(node)
+            for child in node.values():
+                _recur(child)
+        outline = course.Outline
+        if outline is not None:
+            _recur(outline)
+        # forums
+        for forum in course.Discussions.values():
+            for topic in forum.values():
+                if IPublishable.providedBy(forum):
+                    result.append(topic)
+        return result
+
+    def iter_objects(self):
+        result = []
+        catalog = component.queryUtility(ICourseCatalog)
+        if catalog is not None:
+            for entry in catalog.iterCatalogEntries():
+                course = ICourseInstance(entry)
+                self._process_course(course, result)
+        return result
