f40ab460bdb56882b8d8002e0b180de145d6e85e
Carlos Sanchez <papachoco@gmail.com>
2017-10-16 15:15:02 -0500

    pylint

--- src/nti/app/products/courseware/tests/test_workspaces_links.py
+++ src/nti/app/products/courseware/tests/test_workspaces_links.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
@@ -18,7 +19,7 @@ does_not = is_not
 
 from nti.testing.matchers import verifiably_provides
 
-from urllib import unquote
+from six.moves.urllib_parse import unquote
 
 from nti.app.products.courseware.interfaces import ICoursesWorkspace
 
