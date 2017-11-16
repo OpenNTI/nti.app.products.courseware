c31e393ac5e9a48c02d3a3dd22f5ab24ba2f5c7e
Josh Zuech <josh.zuech@nextthought.com>
2017-11-16 10:06:52 -0600

    Give site admins access to roster; return admin role in preferred access view for site admins

--- src/nti/app/products/courseware/views/course_views.py
+++ src/nti/app/products/courseware/views/course_views.py
@@ -89,7 +89,7 @@ from nti.contenttypes.presentation.interfaces import INTILessonOverview
 
 from nti.dataserver import authorization as nauth
 
-from nti.dataserver.authorization import is_admin_or_content_admin
+from nti.dataserver.authorization import is_admin_or_content_admin_or_site_admin
 
 from nti.dataserver.interfaces import IUser
 
@@ -713,7 +713,7 @@ class UserCourseAccessView(AbstractAuthenticatedView):
 
     @Lazy
     def _is_admin(self):
-        return is_admin_or_content_admin(self.remoteUser) \
+        return is_admin_or_content_admin_or_site_admin(self.remoteUser) \
             or is_course_instructor_or_editor(self._course, self.remoteUser)
 
     def __call__(self):
