6fc97f96d47666ae93dc8b05a3de2f82d59dfe5f
Carlos Sanchez <papachoco@gmail.com>
2017-11-28 13:51:18 -0600

    pylint;pep8; remove unicode literals

--- src/nti/app/products/courseware/scripts/nti_acl_forum_creator.py
+++ src/nti/app/products/courseware/scripts/nti_acl_forum_creator.py
@@ -4,10 +4,9 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 import os
 import sys
@@ -48,169 +47,180 @@ from nti.dataserver.utils.base_script import create_context
 from nti.ntiids.ntiids import make_specific_safe
 from nti.ntiids.ntiids import find_object_with_ntiid
 
+logger = __import__('logging').getLogger(__name__)
+
+
 def _get_instructors(instance):
-	result = set(IPrincipal(x, None) for x in get_course_instructors(instance))
-	result.discard(None)
-	return result
+    result = set(IPrincipal(x, None) for x in get_course_instructors(instance))
+    result.discard(None)
+    return result
+
 
 def _get_acl(instance, permissions, ntiids):
-	instructors = _get_instructors(instance)
+    instructors = _get_instructors(instance)
 
-	# Our instance instructors get all permissions.
-	acl = [ForumACE(Permissions=("All",),
-					Entities=[i for i in instructors],
-					Action='Allow'),
-		   ForumACE(Permissions=permissions,
-					Entities=list(ntiids),
-					Action='Allow')]
+    # Our instance instructors get all permissions.
+    acl = [
+        ForumACE(Permissions=("All",),
+                 Entities=[i for i in instructors],
+                 Action='Allow'),
+        ForumACE(Permissions=permissions,
+                 Entities=list(ntiids),
+                 Action='Allow')
+    ]
 
-	# SubInstance instructors get the same permissions as their students.
-	if not ICourseSubInstance.providedBy(instance):
-		for subinstance in instance.SubInstances.values():
-			instructors = _get_instructors(subinstance)
-			acl.append(ForumACE(Permissions=permissions,
-								Entities=[i.id for i in instructors],
-								Action='Allow'))
-	return acl
+    # SubInstance instructors get the same permissions as their students.
+    if not ICourseSubInstance.providedBy(instance):
+        for subinstance in instance.SubInstances.values():
+            instructors = _get_instructors(subinstance)
+            acl.append(ForumACE(Permissions=permissions,
+                                Entities=[i.id for i in instructors],
+                                Action='Allow'))
+    return acl
+
 
 def _assign_iface(obj, iface=None):
-	if iface is not None and not iface.providedBy(obj):
-		interface.alsoProvides(obj, iface)
-		logger.info("Added interface to object %s %s", iface, obj)
+    if iface is not None and not iface.providedBy(obj):
+        interface.alsoProvides(obj, iface)
+        logger.info("Added interface to object %s %s", iface, obj)
+
 
 def _assign_acl(obj, acl, iface=None):
-	if iface is not None and not iface.providedBy(obj):
-		interface.alsoProvides(obj, iface)
-		logger.info("Added interface to object %s %s", iface, obj)
+    if iface is not None and not iface.providedBy(obj):
+        interface.alsoProvides(obj, iface)
+        logger.info("Added interface to object %s %s", iface, obj)
 
-	if not hasattr(obj, 'ACL') or obj.ACL != acl:
-		obj.ACL = acl
-		logger.info("Set ACL on object %s to %s", obj, acl)
+    if not hasattr(obj, 'ACL') or obj.ACL != acl:
+        obj.ACL = acl
+        logger.info("Set ACL on object %s to %s", obj, acl)
+
 
 def _creator(course, scope, name, title, permissions, site=None):
-	set_site(site)
+    set_site(site)
 
-	context = find_object_with_ntiid(course)
-	instance = ICourseInstance(context, None)
-	if instance is None:
-		catalog = component.getUtility(ICourseCatalog)
-		try:
-			context = catalog.getCatalogEntry(course)
-			instance = ICourseInstance(context, None)
-		except KeyError:
-			pass
-	if instance is None:
-		raise ValueError("Course cannot be found")
+    context = find_object_with_ntiid(course)
+    instance = ICourseInstance(context, None)
+    if instance is None:
+        catalog = component.getUtility(ICourseCatalog)
+        try:
+            context = catalog.getCatalogEntry(course)
+            instance = ICourseInstance(context, None)
+        except KeyError:
+            pass
+    if instance is None:
+        raise ValueError("Course cannot be found")
 
-	# decode title
-	title = title.decode('utf-8', 'ignore')
+    # decode title
+    title = title.decode('utf-8', 'ignore')
 
-	# Always created by the public community
-	# (because legacy courses might have a DFL
-	# for the non-public)
-	creator = instance.SharingScopes['Public'].NTIID
-	creator = Entity.get_entity(creator)
+    # Always created by the public community
+    # (because legacy courses might have a DFL
+    # for the non-public)
+    creator = instance.SharingScopes['Public'].NTIID
+    creator = Entity.get_entity(creator)
 
-	# get fourm interface
-	if scope == ES_PUBLIC:
-		forum_interface = ICourseInstancePublicScopedForum
-	elif scope == ES_PURCHASED:
-		forum_interface = ICourseInstancePurchasedScopedForum
-	else:
-		forum_interface = ICourseInstanceForCreditScopedForum
+    # get fourm interface
+    if scope == ES_PUBLIC:
+        forum_interface = ICourseInstancePublicScopedForum
+    elif scope == ES_PURCHASED:
+        forum_interface = ICourseInstancePurchasedScopedForum
+    else:
+        forum_interface = ICourseInstanceForCreditScopedForum
 
-	discussions = instance.Discussions
+    discussions = instance.Discussions
 
-	ntiid = instance.SharingScopes[scope].NTIID
-	acl = _get_acl(instance, permissions, (ntiid,))
+    ntiid = instance.SharingScopes[scope].NTIID
+    acl = _get_acl(instance, permissions, (ntiid,))
 
-	name = make_specific_safe(name)
-	try:
-		forum = discussions[name]
-		logger.info("Found existing forum %s", name)
-		_assign_iface(forum, forum_interface)
-		_assign_acl(forum, acl, IACLCommunityForum)
-		if forum.creator is not creator:
-			forum.creator = creator
-	except KeyError:
-		forum = ACLCommunityForum()
-		forum.title = title
-		forum.creator = creator
-		_assign_iface(forum, forum_interface)
-		_assign_acl(forum, acl, IACLCommunityForum)
+    name = make_specific_safe(name)
+    try:
+        forum = discussions[name]
+        logger.info("Found existing forum %s", name)
+        _assign_iface(forum, forum_interface)
+        _assign_acl(forum, acl, IACLCommunityForum)
+        if forum.creator is not creator:
+            forum.creator = creator
+    except KeyError:
+        forum = ACLCommunityForum()
+        forum.title = title
+        forum.creator = creator
+        _assign_iface(forum, forum_interface)
+        _assign_acl(forum, acl, IACLCommunityForum)
 
-		discussions[name] = forum
-		logger.debug('Created forum %s', forum)
-	return forum.NTIID
+        discussions[name] = forum
+        logger.debug('Created forum %s', forum)
+    return forum.NTIID
+
 
 def main():
-	arg_parser = argparse.ArgumentParser(description="ACL forum creator")
-	arg_parser.add_argument('-v', '--verbose', help="Be verbose", action='store_true',
-							dest='verbose')
-	arg_parser.add_argument('-c', '--course',
-							dest='course',
-							help="Course entry identifier/ntiid")
-	arg_parser.add_argument('-n', '--name',
-							dest='name',
-							help="Forum name")
-	arg_parser.add_argument('-t', '--title',
-							dest='title',
-							help="Forum title")
-	arg_parser.add_argument('-s', '--scope',
-							 dest='scope',
-							 help="Scope")
-	arg_parser.add_argument('-p', '--permissions',
-							 dest='permissions',
-							 nargs="+",
-							 default=(READ_PERMISSION, WRITE_PERMISSION),
-							 help="The permissions")
-	arg_parser.add_argument('--site',
-							dest='site',
-							help="Application SITE.")
-	args = arg_parser.parse_args()
+    arg_parser = argparse.ArgumentParser(description="ACL forum creator")
+    arg_parser.add_argument('-v', '--verbose', help="Be verbose", action='store_true',
+                                                    dest='verbose')
+    arg_parser.add_argument('-c', '--course',
+                            dest='course',
+                            help="Course entry identifier/ntiid")
+    arg_parser.add_argument('-n', '--name',
+                            dest='name',
+                            help="Forum name")
+    arg_parser.add_argument('-t', '--title',
+                            dest='title',
+                            help="Forum title")
+    arg_parser.add_argument('-s', '--scope',
+                            dest='scope',
+                            help="Scope")
+    arg_parser.add_argument('-p', '--permissions',
+                            dest='permissions',
+                            nargs="+",
+                            default=(READ_PERMISSION, WRITE_PERMISSION),
+                            help="The permissions")
+    arg_parser.add_argument('--site',
+                            dest='site',
+                            help="Application SITE.")
+    args = arg_parser.parse_args()
 
-	env_dir = os.getenv('DATASERVER_DIR')
-	if not env_dir or not os.path.exists(env_dir) and not os.path.isdir(env_dir):
-		raise IOError("Invalid dataserver environment root directory")
+    env_dir = os.getenv('DATASERVER_DIR')
+    if not env_dir or not os.path.exists(env_dir) and not os.path.isdir(env_dir):
+        raise IOError("Invalid dataserver environment root directory")
 
-	site = args.site
-	course = args.course
-	if not course:
-		raise ValueError("Please specify a course/catalog entry identifier")
+    site = args.site
+    course = args.course
+    if not course:
+        raise ValueError("Please specify a course/catalog entry identifier")
 
-	scope = args.scope
-	if not scope:
-		raise ValueError("Please specify a scope")
-	if scope not in ENROLLMENT_SCOPE_VOCABULARY.by_token.keys():
-		raise ValueError("Please specify a valid scope")
+    scope = args.scope
+    if not scope:
+        raise ValueError("Please specify a scope")
+    if scope not in ENROLLMENT_SCOPE_VOCABULARY.by_token.keys():
+        raise ValueError("Please specify a valid scope")
 
-	name = args.name
-	if not name:
-		raise ValueError("Please specify a fourum name")
-	title = args.title or name
+    name = args.name
+    if not name:
+        raise ValueError("Please specify a fourum name")
+    title = args.title or name
 
-	permissions = args.permissions or ()
-	permissions = [x.capitalize() for x in permissions if x]
-	for perm in permissions:
-		if perm not in PERMISSIONS:
-			raise ValueError("%s is an invalid permission" % perm)
-	if not permissions:
-		raise ValueError("Please specify forum permissions")
+    permissions = args.permissions or ()
+    permissions = [x.capitalize() for x in permissions if x]
+    for perm in permissions:
+        if perm not in PERMISSIONS:
+            raise ValueError("%s is an invalid permission" % perm)
+    if not permissions:
+        raise ValueError("Please specify forum permissions")
 
-	conf_packages = ('nti.appserver',)
-	context = create_context(env_dir, with_library=True)
+    conf_packages = ('nti.appserver',)
+    context = create_context(env_dir, with_library=True)
 
-	run_with_dataserver(environment_dir=env_dir,
-						xmlconfig_packages=conf_packages,
-						verbose=args.verbose,
-						context=context,
-						function=lambda: _creator(site=site,
-												  course=course,
-												  name=name,
-												  title=title,
-												  scope=scope,
-												  permissions=permissions))
-	sys.exit(0)
+    run_with_dataserver(environment_dir=env_dir,
+                        xmlconfig_packages=conf_packages,
+                        verbose=args.verbose,
+                        context=context,
+                        function=lambda: _creator(site=site,
+                                                  course=course,
+                                                  name=name,
+                                                  title=title,
+                                                  scope=scope,
+                                                  permissions=permissions))
+    sys.exit(0)
+
 
 if __name__ == '__main__':
-	main()
+    main()
--- src/nti/app/products/courseware/scripts/nti_course_enrollment_migrator.py
+++ src/nti/app/products/courseware/scripts/nti_course_enrollment_migrator.py
@@ -4,10 +4,9 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 import os
 import sys
@@ -21,75 +20,80 @@ from nti.dataserver.utils import run_with_dataserver
 from nti.dataserver.utils.base_script import set_site
 from nti.dataserver.utils.base_script import create_context
 
-def _migrate(ntiid, scope=ES_PUBLIC, max_seat_count=25, sections=(),
-			 site=None, dry_run=False, verbose=False):
+logger = __import__('logging').getLogger(__name__)
 
-	set_site(site)
-	result = course_enrollment_migrator(ntiid=ntiid,
-										scope=scope,
-							 			dry_run=dry_run,
-							 			verbose=verbose,
-							 			sections=sections,
-							 			max_seat_count=max_seat_count)
-	return result
+
+def _migrate(ntiid, scope=ES_PUBLIC, max_seat_count=25, sections=(),
+             site=None, dry_run=False, verbose=False):
+
+    set_site(site)
+    result = course_enrollment_migrator(ntiid=ntiid,
+                                        scope=scope,
+                                        dry_run=dry_run,
+                                        verbose=verbose,
+                                        sections=sections,
+                                        max_seat_count=max_seat_count)
+    return result
+
 
 def main():
-	arg_parser = argparse.ArgumentParser(description="Migrate enrollments from main course to sub-instances")
-	arg_parser.add_argument('ntiid', help="Course entry identifier/ntiid")
-	arg_parser.add_argument('-v', '--verbose', help="Be verbose", action='store_true',
-							dest='verbose')
-	arg_parser.add_argument('-c', '--count',
-							dest='max_seat_count',
-							default=25,
-							type=int,
-							help="Max seat count")
-	arg_parser.add_argument('--sections',
-							 dest='sections',
-							 nargs="+",
-							 default=(),
-							 help="The names of sections to enroll")
-	arg_parser.add_argument('--dry',
-							 dest='dry_run',
-							 action='store_true',
-							 help="Dry run")
-	arg_parser.add_argument('-s', '--scope',
-							 dest='scope',
-							 default=ES_PUBLIC,
-							 help="Scope to migrate")
-	arg_parser.add_argument('--site',
-							dest='site',
-							help="Application SITE.")
-	args = arg_parser.parse_args()
+    arg_parser = argparse.ArgumentParser(description="Migrate enrollments from main course to sub-instances")
+    arg_parser.add_argument('ntiid', help="Course entry identifier/ntiid")
+    arg_parser.add_argument('-v', '--verbose', help="Be verbose", action='store_true',
+                                                    dest='verbose')
+    arg_parser.add_argument('-c', '--count',
+                            dest='max_seat_count',
+                            default=25,
+                            type=int,
+                            help="Max seat count")
+    arg_parser.add_argument('--sections',
+                            dest='sections',
+                            nargs="+",
+                            default=(),
+                            help="The names of sections to enroll")
+    arg_parser.add_argument('--dry',
+                            dest='dry_run',
+                            action='store_true',
+                            help="Dry run")
+    arg_parser.add_argument('-s', '--scope',
+                            dest='scope',
+                            default=ES_PUBLIC,
+                            help="Scope to migrate")
+    arg_parser.add_argument('--site',
+                            dest='site',
+                            help="Application SITE.")
+    args = arg_parser.parse_args()
 
-	env_dir = os.getenv('DATASERVER_DIR')
-	if not env_dir or not os.path.exists(env_dir) and not os.path.isdir(env_dir):
-		raise IOError("Invalid dataserver environment root directory")
+    env_dir = os.getenv('DATASERVER_DIR')
+    if not env_dir or not os.path.exists(env_dir) and not os.path.isdir(env_dir):
+        raise IOError("Invalid dataserver environment root directory")
 
-	site = args.site
-	ntiid = args.ntiid
-	scope = args.scope
-	dry_run = args.dry_run
-	sections = args.sections
-	max_seat_count = args.max_seat_count
+    site = args.site
+    ntiid = args.ntiid
+    scope = args.scope
+    dry_run = args.dry_run
+    sections = args.sections
+    max_seat_count = args.max_seat_count
 
-	if max_seat_count <= 0:
-		raise ValueError("Invalid max seat count")
+    if max_seat_count <= 0:
+        raise ValueError("Invalid max seat count")
 
-	context = create_context(env_dir, with_library=True)
-	conf_packages = ('nti.appserver',)
+    context = create_context(env_dir, with_library=True)
+    conf_packages = ('nti.appserver',)
 
-	run_with_dataserver(environment_dir=env_dir,
-						xmlconfig_packages=conf_packages,
-						verbose=args.verbose,
-						context=context,
-						function=lambda: _migrate(site=site,
-												  ntiid=ntiid,
-												  scope=scope,
-												  dry_run=dry_run,
-												  sections=sections,
-												  verbose=args.verbose,
-												  max_seat_count=max_seat_count))
-	sys.exit(0)
+    run_with_dataserver(environment_dir=env_dir,
+                        xmlconfig_packages=conf_packages,
+                        verbose=args.verbose,
+                        context=context,
+                        function=lambda: _migrate(site=site,
+                                                  ntiid=ntiid,
+                                                  scope=scope,
+                                                  dry_run=dry_run,
+                                                  sections=sections,
+                                                  verbose=args.verbose,
+                                                  max_seat_count=max_seat_count))
+    sys.exit(0)
+
 
 if __name__ == '__main__':
-	main()
+    main()
