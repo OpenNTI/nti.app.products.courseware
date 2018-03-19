817421f5b081a94baa38a8b8b34387156431e3ad
chris.utz <chris.utz@nextthought.com>
2018-03-19 13:26:44 -0500

    Generate a better filename.  Use the user of the enrollment record, not the remote user. Stub out getting the completeditem to get the certificate date. 404 if completion isn't turned on or if the course isn't completed for this context

--- src/nti/app/products/courseware/completion/views.py
+++ src/nti/app/products/courseware/completion/views.py
@@ -8,11 +8,17 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+from slugify import slugify
+
 from zope import component
+
+from pyramid import httpexceptions as hexc
 
 from pyramid.view import view_config
 
 from zc.displayname.interfaces import IDisplayNameGenerator
+
+from zope.cachedescriptors.property import Lazy
 
 from nti.appserver.policies.interfaces import ISitePolicyUserEventListener
 
@@ -20,11 +26,16 @@ from nti.appserver.policies.site_policies import guess_site_display_name
 
 from nti.dataserver import authorization as nauth
 
+from nti.dataserver.users import User
+
 from nti.dataserver.users.interfaces import IFriendlyNamed
 
 from nti.app.base.abstract_views import AbstractAuthenticatedView
 
 from nti.app.products.courseware.interfaces import ICourseInstanceEnrollment
+
+from nti.contenttypes.completion.interfaces import ICompletionContextCompletionPolicy
+from nti.contenttypes.completion.interfaces import ICompletedItem
 
 from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
 
@@ -36,18 +47,27 @@ from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
 	     permission=nauth.ACT_READ)
 class CompletionCertificateView(AbstractAuthenticatedView):
 
-    @property
+    Title = u'Completion Certificate'
+
+    @Lazy
+    def course(self):
+        return self.context.CourseInstance
+
+    @Lazy
+    def user(self):
+        username = self.context.Username
+        return User.get_user(username)
+    
+    @Lazy
     def _name(self):
-        user = self.remoteUser
-        
         # A certificate is fairly formal so try and use realname first
-        name = IFriendlyNamed(self.remoteUser).realname
+        name = IFriendlyNamed(self.user).realname
         if not name:
             # Otherwise just fallback to whatever is our display name generator
-            name = component.getMultiAdapter((user, self.request), IDisplayNameGenerator)()
+            name = component.getMultiAdapter((self.user, self.request), IDisplayNameGenerator)()
         return name
 
-    @property
+    @Lazy
     def _brand(self):
         policy = component.getUtility(ISitePolicyUserEventListener)
 	display_name = getattr(policy, 'BRAND', '')
@@ -56,18 +76,39 @@ class CompletionCertificateView(AbstractAuthenticatedView):
 	else:
 	    display_name = guess_site_display_name(self.request)
         return display_name
+
+    @Lazy
+    def _course_completable_item(self):
+        policy = ICompletionContextCompletionPolicy(self.course, None)
+        if policy is None:
+            return None
+
+        return component.queryMultiAdapter((self.course, self.user, self.course), ICompletedItem) 
+
+    def _filename(self, entry, suffix='completion', ext='pdf'):
+        filename = '%s %s %s' % (self.user, entry.ProviderUniqueID, suffix)
+        slugged = slugify(filename, seperator='_', lowercase=True)
+        return '%s.%s' % (slugged, ext)
+
+    @property
+    def _completion_date_string(self):
+        completed = self._course_completable_item.CompletedDate
+        return completed.strftime('%B %d, %Y')
     
     def __call__(self):
+        if self._course_completable_item is None:
+            raise hexc.HTTPNotFound()
+
+        entry = ICourseCatalogEntry(self.course)
+        
         response = self.request.response
-        #response.content_disposition = 'attachment; filename="completion.pdf"'
-        from IPython.core.debugger import Tracer; Tracer()()
-        entry = ICourseCatalogEntry(self.context.CourseInstance)
+        response.content_disposition = 'attachment; filename="%s"' % self._filename(entry)
+
         
         return {
             u'Brand': self._brand,
             u'Name': self._name,
             u'Course': entry.title,
-            u'Date': 'February 15, 1986',
-            u'Hours': 1,
-            u'Title': 'Completion Certificate'
+            u'Date': self._completion_date_string,
+            u'Hours': None
         }
