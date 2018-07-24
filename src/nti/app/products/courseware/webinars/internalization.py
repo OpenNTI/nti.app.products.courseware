7065276e88713cc88f7b2dddc22261513df7a993
Josh Zuech <josh.zuech@nextthought.com>
2018-07-24 16:33:16 -0500

    Now with 100% more internalization

---
+++ src/nti/app/products/courseware/webinars/internalization.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import six
+
+from pyramid.threadlocal import get_current_request
+
+from zope import component
+from zope import interface
+
+from nti.app.products.courseware.webinars.interfaces import IWebinarAsset
+
+from nti.app.products.webinar.interfaces import IWebinarClient
+from nti.app.products.webinar.interfaces import IGoToWebinarAuthorizedIntegration
+
+from nti.externalization.datastructures import InterfaceObjectIO
+
+from nti.externalization.interfaces import IInternalObjectUpdater
+
+logger = __import__('logging').getLogger(__name__)
+
+
+@interface.implementer(IInternalObjectUpdater)
+class WebinarAssetUpdater(InterfaceObjectIO):
+
+    _ext_iface_upper_bound = IWebinarAsset
+
+    def updateFromExternalObject(self, parsed, *args, **kwargs):
+        webinar_data = parsed.get('webinar')
+        if isinstance(webinar_data, six.string_types):
+            # The client may give us a webinar key here instead of the
+            # actual webinar object; if so, we must resolve it.
+            webinar_integration = component.getUtility(IGoToWebinarAuthorizedIntegration)
+            client = component.queryMultiAdapter((webinar_integration, get_current_request()),
+                                                 IWebinarClient)
+            webinar = client.get_webinar(webinar_data)
+            if webinar is None:
+                # We'll fail validation if none
+                logger.warn('Could not resolve webinar with key (%s)', webinar_data)
+            parsed['webinar'] = webinar
+        return super(WebinarAssetUpdater, self).updateFromExternalObject(parsed, *args, **kwargs)
