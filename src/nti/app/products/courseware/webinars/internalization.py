37a6026822885272a1d9bd9a8c0fd22b216f131a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-08-01 18:57:13 -0500

    pylint

--- src/nti/app/products/courseware/webinars/internalization.py
+++ src/nti/app/products/courseware/webinars/internalization.py
@@ -8,10 +8,10 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-import six
-
 from zope import component
 from zope import interface
+
+import six
 
 from nti.app.products.courseware.webinars.interfaces import IWebinarAsset
 
