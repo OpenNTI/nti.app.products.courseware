c7e664f2dd543fdd68a71059b663abc94a60528d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-08-02 09:47:02 -0500

    trully make solr a soft dep

--- src/nti/app/products/courseware/webinars/__init__.py
+++ src/nti/app/products/courseware/webinars/__init__.py
@@ -8,17 +8,18 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-from nti.solr.presentation import ASSETS_CATALOG
-
-from nti.solr.utils import mimeTypeRegistry
-
 WEBINAR_ASSET_MIME_TYPE = 'application/vnd.nextthought.webinarasset'
 
 logger = __import__('logging').getLogger(__name__)
 
 
 def _register():
-    mimeTypeRegistry.register(WEBINAR_ASSET_MIME_TYPE, ASSETS_CATALOG)
+    try:
+        from nti.solr.presentation import ASSETS_CATALOG
+        from nti.solr.utils import mimeTypeRegistry
+        mimeTypeRegistry.register(WEBINAR_ASSET_MIME_TYPE, ASSETS_CATALOG)
+    except ImportError:
+        logger.warning("Solr registry is not available")
 
 
 _register()
