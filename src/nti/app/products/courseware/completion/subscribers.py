d89c7395dc15d02e67f4cc546c5133d68d218537
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-08-15 17:49:05 -0500

    change event type

--- src/nti/app/products/courseware/completion/subscribers.py
+++ src/nti/app/products/courseware/completion/subscribers.py
@@ -8,7 +8,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-from zc.intid.interfaces import IBeforeIdRemovedEvent
+from zope.lifecycleevent.interfaces import IObjectRemovedEvent
 
 from zope import component
 
@@ -19,7 +19,7 @@ from nti.contenttypes.courses.interfaces import ICourseInstance
 logger = __import__('logging').getLogger(__name__)
 
 
-@component.adapter(ICourseInstance, IBeforeIdRemovedEvent)
-def _on_course_deleted(course, unused_event):
+@component.adapter(ICourseInstance, IObjectRemovedEvent)
+def _on_course_deleted(course, unused_event=None):
     # clear containers. sections have their own
     completion_context_deleted_event(course)
