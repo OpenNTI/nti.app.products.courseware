6d39a0deec4fe0f2342ca4a32b2302e7e9b03db4
Alex Yang <alex.yang@nextthought.com>
2018-09-24 11:51:07 -0500

    just check is_admin

--- src/nti/app/products/courseware/tab_preference.py
+++ src/nti/app/products/courseware/tab_preference.py
@@ -12,18 +12,10 @@ from zope import interface
 
 from nti.app.products.courseware.interfaces import ICourseTabConfigurationUtility
 
-from nti.dataserver.authorization import ACT_CONTENT_EDIT
 from nti.dataserver.authorization import is_admin
-
-from nti.dataserver.authorization_acl import has_permission
 
 @interface.implementer(ICourseTabConfigurationUtility)
 class DefaultCourseTabConfigurationUtility(object):
 
-	def _allow_access(self, user):
-		return is_admin(user)
-
 	def can_edit_tabs(self, user, course):
-		if has_permission(ACT_CONTENT_EDIT, course, user):
-			return self._allow_access(user)
-		return False
+		return is_admin(user)
