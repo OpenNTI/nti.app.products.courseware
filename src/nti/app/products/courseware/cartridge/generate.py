fbc9b5062bda81ea5a54b941db442cb01528f8cc
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-09-24 15:15:03 -0500

    pep8

--- src/nti/app/products/courseware/cartridge/generate.py
+++ src/nti/app/products/courseware/cartridge/generate.py
@@ -8,6 +8,8 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+logger = __import__('logging').getLogger(__name__)
+
 
 def generate_archive(root, archive):
     """
