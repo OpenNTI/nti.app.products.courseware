c003f6096212691f3a87c121cf5a1a82734a6272
Austin Graham <austin.graham@nextthought.com>
2018-09-24 13:51:12 -0500

    outline interfaces

--- src/nti/app/products/courseware/cartridge/interfaces.py
+++ src/nti/app/products/courseware/cartridge/interfaces.py
@@ -14,16 +14,9 @@ from zope import interface
 
 from nti.contenttypes.courses.interfaces import ICourseInstance
 
+from nti.schema.field import Bool
 from nti.schema.field import Object
 from nti.schema.field import DecodingValidTextLine as TextLine
-
-
-class ICommonCartridge(interface.Interface):
-    course = Object(ICourseInstance, title=u"The course")
-
-    archive = TextLine(
-        title=u"A valid directory location for the archive data"
-    )
 
 
 class IManifest(interface.Interface):
@@ -52,3 +45,34 @@ class IElementHandler(interface.Interface):
         """
         Write the necesary files to the archive
         """
+
+
+class IGenerationNode(interface.Interface):
+    """
+    Base for an item to generate XML
+    """
+
+    traversed = Bool(title=u"If this item has been traversed",
+                     required=False,
+                     default=False)
+
+    handler = Object(IElementHandler, title=u"XML generation handler")
+
+    def get_items():
+        """
+        Get all sub-items of this node
+        """
+
+
+class ICourseGenerationNode(IGenerationNode, ICourseInstance):
+    """
+    Implementation of generation logic for a course instance
+    """
+
+
+class ICommonCartridge(interface.Interface):
+    course = Object(ICourseGenerationNode, title=u"The course")
+
+    archive = TextLine(
+        title=u"A valid directory location for the archive data"
+    )
