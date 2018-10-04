45953ef3956af48adb395244bad24889cb537bf0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-10-04 12:24:42 -0500

    clean up interfaces

--- src/nti/app/products/courseware/cartridge/generate.py
+++ src/nti/app/products/courseware/cartridge/generate.py
@@ -14,27 +14,6 @@ from nti.contenttypes.presentation.interfaces import IItemAssetContainer
 logger = __import__('logging').getLogger(__name__)
 
 
-def generate_archive(root, archive):
-    """
-    Generate the archive data for a given item.
-
-    DFS traversal of item structure.
-    """
-    # Generate XML with placeholder for items if necessary
-    xml = root.toXML(archive)
-    # Get child items
-    items = root.get_items()
-    if items:
-        # Recursively generate children
-        for i in items:
-            if not i.traversed:
-                child_xml = generate_archive(i, archive)
-                xml.insert_child(child_xml)
-    # Mark as traversed in case of multiple links
-    root.traversed = True
-    return xml
-
-
 def walk(course):
     nodes = list()
     def _recur_nodes(node):
--- src/nti/app/products/courseware/cartridge/interfaces.py
+++ src/nti/app/products/courseware/cartridge/interfaces.py
@@ -14,7 +14,6 @@ from zope import interface
 
 from nti.contenttypes.courses.interfaces import ICourseInstance
 
-from nti.schema.field import Bool
 from nti.schema.field import Object
 from nti.schema.field import DecodingValidTextLine as TextLine
 
@@ -23,12 +22,6 @@ class IBaseElementHandler(interface.Interface):
     """
     Adapter to handle an asset within a common cartridge
     """
-
-    def iter_items():
-        """
-        returns an iterable of minidom elements for an organization items
-        in the manifest file
-        """
 
     def iter_resources():
         """
@@ -41,31 +34,8 @@ class IBaseElementHandler(interface.Interface):
         """
 
 
-class IGenerationNode(interface.Interface):
-    """
-    Base for an item to generate XML
-    """
-
-    traversed = Bool(title=u"If this item has been traversed",
-                     required=False,
-                     default=False)
-
-    handler = Object(IBaseElementHandler, title=u"XML generation handler")
-
-    def get_items():
-        """
-        Get all sub-items of this node
-        """
-
-
-class ICourseGenerationNode(IGenerationNode, ICourseInstance):
-    """
-    Implementation of generation logic for a course instance
-    """
-
-
 class ICommonCartridge(interface.Interface):
-    course = Object(ICourseGenerationNode, title=u"The course")
+    course = Object(ICourseInstance, title=u"The course")
 
     archive = TextLine(
         title=u"A valid directory location for the archive data"
@@ -82,15 +52,11 @@ class IManifest(interface.Interface):
         """
         mark a resource in this manifest
         """
-        
+
     def has_resource(iden):
         """
         check if the resource w/ the specified iden is in this manifest
         """
-
-
-class IManifestBuilder(interface.Interface):
-    manifest = Object(IManifest, title=u"The manifest")
 
 
 class IElementHandler(IBaseElementHandler):
