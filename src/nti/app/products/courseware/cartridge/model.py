a64137adc14eef1ac67fb1d657f672a2f7156d77
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-10-04 18:00:40 -0500

    add handler factory method

--- src/nti/app/products/courseware/cartridge/model.py
+++ src/nti/app/products/courseware/cartridge/model.py
@@ -12,6 +12,7 @@ from zope import component
 from zope import interface
 
 from nti.app.products.courseware.cartridge.interfaces import IManifest
+from nti.app.products.courseware.cartridge.interfaces import IElementHandler
 from nti.app.products.courseware.cartridge.interfaces import ICommonCartridge
 
 from nti.base._compat import text_
@@ -41,6 +42,10 @@ class Manifest(object):
     def course(self):
         return getattr(self.cartridge, 'course', None)
 
+    @property
+    def archive(self):
+        return getattr(self.cartridge, 'archive', None)
+
     def mark_resource(self, iden):
         if iden is not None:
             self.resources.add(text_(iden))
@@ -48,6 +53,14 @@ class Manifest(object):
 
     def has_resource(self, iden):
         return text_(iden) in self.resources
+    __contains__ = has_resource
+    
+    def handler(self, context):
+        result = IElementHandler(context, None)
+        result.manifest = self
+        return result
+    handler_factory = handler
 
-    def __contains__(self, iden):
-        return self.has_resource(iden)
+    def write_to(self, handler):
+        archive = getattr(self.cartridge, 'archive', None)
+        handler.write_to(archive)
