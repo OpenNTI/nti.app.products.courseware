171a0aa038a86d476caff44c66465619f1de56ce
Sheldon Smith <sheldon.smith@nextthought.com>
2018-11-26 10:30:27 -0600

    Checkpoint - Can export most NTI assignments to QTI

--- src/nti/app/products/courseware/qti/models.py
+++ src/nti/app/products/courseware/qti/models.py
@@ -5,24 +5,186 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
+import os
+
+from bs4 import BeautifulSoup
+
+from pynliner import Pynliner  # TODO add this to buildout deps
+
+from six.moves import urllib_parse
+
+from zope import component
+from zope import interface
+
+from zope.cachedescriptors.property import Lazy
+
+from zope.intid import IIntIds
+
+from nti.app.products.courseware.cartridge.renderer import execute
+from nti.app.products.courseware.cartridge.renderer import get_renderer
+
+from nti.app.products.courseware.qti.interfaces import IQTIAssessment
+from nti.app.products.courseware.qti.interfaces import IQTIItemContent
+
+from nti.assessment.interfaces import IQAssessment
+from nti.assessment.interfaces import IQEditableEvaluation
+from nti.assessment.interfaces import IQuestionSet
+
+from nti.contentlibrary.interfaces import IPersistentFilesystemContentUnit
+
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
 
-class AbstractQTIItem(object):
-
-    def __init__(self, question):
-        self.question = question
-
-
+# TODO ideally, on content backed questions we would parse the questions out of the html
+# rather than the object. The reasoning behind this is that it would allow us
+# to create Canvas text entries for interlaced text content. For now, we are
+# using the object questions as it is an implementation that works for both
+# content backed and ui created questions, and interlaced text does not have
+# an identifying div wrapping it making it difficult to determine a consistent heuristic
+# for ripping it out. As such, all interlaced text will be preserved, but will be
+# displayed in the assignment body. Because we are leaving all other tags in besides
+# object tags, there is a possibility that a future import implementation could
+# bring NTI qti exports back in properly
+@interface.implementer(IQTIAssessment)
 class QTIAssessment(object):
-    pass
 
+    def __init__(self, context):
+        self.context = context
+        self.section = None
+        self.dependencies = {}
 
-class QTIChoice(AbstractQTIItem):
+    @Lazy
+    def identifier(self):
+        intids = component.getUtility(IIntIds)
+        intid = intids.register(self)
+        # Start at A
+        identifier = ''.join([chr(65 + int(i)) for i in str(intid)])
+        return identifier
 
-    def to_xml(self):
+    @Lazy
+    def context_parent(self):
+        return self.context.__parent__
+
+    @Lazy
+    def is_content_backed(self):
+        return not IQEditableEvaluation.providedBy(self.context_parent) \
+               and IPersistentFilesystemContentUnit.providedBy(self.context_parent)
+
+    @Lazy
+    def title(self):
+        return self.context.title
+
+    @Lazy
+    def content_file(self):
+        if self.is_content_backed:
+            return self.context_parent.key
+
+    def content_soup(self, styled=False):
+        if self.is_content_backed:
+            text = self.content_file.read_contents_as_text()
+            if styled:
+                # This inlines external style sheets
+                content = Pynliner()
+                content.root_url = self.content_file.absolute_path
+                assert getattr(content, '_get_url', None)  # If this private method gets refactored, let us know
+                # By default, this tries to retrieve via request, we will override
+                content._get_url = lambda url: open(url).read().decode()
+                content.from_string(text)
+                text = content.run()
+            return BeautifulSoup(text, features='lxml')
+
+    def _is_internal_resource(self, href):
+        return not bool(urllib_parse.urlparse(href).scheme)
+
+    def _parse_content(self):
         """
-        Converts to the appropriate xml format
+        Attempt to take a content backed assignment page contents and strip it down for external usage.
+        Specifically, object references. We will also attempt to
+        build dependencies on any internal resources
         """
+        page_contents = self.content_soup().find_all('div', {'class': 'page-contents'})
+        new_content = BeautifulSoup()
+        to_be_extracted = []  # Because we recurse the structure we need to delay extraction until we are finished
+        for pg in page_contents:  # Most likely len(page_contents) == 1
+            for tag in pg.recursiveChildGenerator():
+                # Check for resource refs and add to dependencies
+                if hasattr(tag, 'name')  and\
+                   tag.name == 'a'       and\
+                   hasattr(tag, 'attrs') and\
+                   'href' in tag.attrs:
+                    href = tag.attrs['href']
+                    if self._is_internal_resource(href):
+                        path_to = os.path.join(self.content_file.bucket.absolute_path, href)
+                        href = os.path.join('dependencies', href)
+                        self.dependencies[path_to] = href
+                        tag.attrs['href'] = os.path.join('$IMS-CC-FILEBASE$', href)
+                # Grab images
+                if hasattr(tag, 'name') and tag.name == 'img' and \
+                        hasattr(tag, 'attrs') and 'src' in tag.attrs:
+                    src = tag.attrs['src']
+                    if self._is_internal_resource(src):
+                        path_to = os.path.join(self.content_file.bucket.absolute_path, src)
+                        src = os.path.join('dependencies', src)
+                        self.dependencies[path_to] = src
+                        tag.attrs['src'] = os.path.join('$IMS-CC-FILEBASE$', src)
+                # Extract question objects
+                if hasattr(tag, 'name') and tag.name == 'object':
+                    if hasattr(tag, 'attrs') and tag.attrs.get('type') == 'application/vnd.nextthought.naquestion':
+                        to_be_extracted.append(tag)
+            new_content.append(pg)
+        for tag in to_be_extracted:
+            tag.extract()
+        return new_content.prettify()
+
+    @Lazy
+    def content(self):
+        if self.is_content_backed:
+            return self._parse_content()
+        else:
+            return self.context.content
+
+    @Lazy
+    def questions(self):
+        if IQuestionSet.providedBy(self.context):
+            return self.context.questions
+        elif IQAssessment.providedBy(self.context):
+            # Get q sets from assignment parts
+            question_sets = (part.question_set for part in self.context.parts)
+            # Coalesce the questions
+            return [question for qs in question_sets for question in qs.questions]
+        else:
+            # Should never happen TODO handle
+            return None
+
+    def get_items(self):
+        items = []
+        for question in self.questions:
+            for part in question:
+                qti_item = IQTIItemContent(part)
+                qi_export = qti_item.to_xml(self.section)
+                items.append(qi_export)
+                self.dependencies.update(qti_item.dependencies)
+        return items
+
+    def export(self):
+        items = self.get_items()
+        renderer = get_renderer('qti_assessment', '.pt')
+        context = {'ident': self.identifier,
+                   'title': self.context.title,
+                   'items': items}
+        return execute(renderer, {'context': context})
+
+
+class CanvasAssessmentMeta(object):
+
+    def __init__(self, assessment):
+        self.assessment = assessment
+
+    def meta(self):
+        renderer = get_renderer('assessment_meta', '.pt')
+        context = {'title': self.assessment.title,
+                   'description': self.assessment.content,
+                   'quiz_id': self.assessment.identifier}
+        return execute(renderer, {'context': context})
--- src/nti/app/products/courseware/qti/views.py
+++ src/nti/app/products/courseware/qti/views.py
@@ -5,6 +5,92 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
+import os
+import shutil
+import tempfile
+
+from pyramid.view import view_config
+from pyramid.view import view_defaults
+
+from nti.app.base.abstract_views import AbstractAuthenticatedView
+
+from nti.app.products.courseware.cartridge.renderer import execute
+from nti.app.products.courseware.cartridge.renderer import get_renderer
+
+from nti.app.products.courseware.qti.interfaces import ICanvasMeta
+from nti.app.products.courseware.qti.interfaces import IQTIAssessment
+
+from nti.assessment import IQAssignment
+from nti.assessment import IQuestionSet
+
+from nti.contenttypes.presentation.interfaces import INTIAssignmentRef
+from nti.contenttypes.presentation.interfaces import INTIQuestionSetRef
+
+from nti.dataserver import authorization as nauth
+
+from nti.ntiids.ntiids import find_object_with_ntiid
+
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
+
+
+@view_defaults(route_name='objects.generic.traversal',
+               request_method='GET',
+               permission=nauth.ACT_CREATE,
+               renderer='rest',
+               name='qti')
+class QTIAssessmentView(AbstractAuthenticatedView):
+    """
+    For convenience, this view also accepts refs
+    """
+
+    @view_config(context=IQuestionSet)
+    @view_config(context=IQAssignment)
+    def _export(self, assignment=None):
+        assignment = assignment if assignment else self.context
+        qti = IQTIAssessment(assignment)
+        export = qti.export()
+        meta_obj = ICanvasMeta(qti)
+        m_exp = meta_obj.meta()
+        archive = tempfile.mkdtemp()
+        lao_dir = os.path.join(archive, unicode(qti.identifier))
+        non_cc = os.path.join(archive, unicode('non_cc_assessments'))
+        os.makedirs(lao_dir)
+        os.makedirs(non_cc)
+        lao_path = os.path.join(lao_dir, '%s.xml' % qti.identifier)
+        with open(lao_path, 'w') as lao:
+            lao.write(export.encode('utf-8'))
+        meta_path = os.path.join(lao_dir, 'assessment_meta.xml')
+        with open(meta_path, 'w') as meta:
+            meta.write(m_exp.encode('utf-8'))
+        renderer = get_renderer('manifest', '.pt', package='nti.app.products.courseware.qti')
+        for source_path, target in qti.dependencies.items():
+            target_path = os.path.join(archive, target)
+            if not os.path.exists(os.path.dirname(target_path)):
+                os.makedirs(os.path.dirname(target_path))
+            shutil.copy(source_path, target_path)
+
+        context = {'dep_href': '%s/assessment_meta.xml' % unicode(qti.identifier),
+                   'assessment_href': '%s/%s.xml' % (unicode(qti.identifier), unicode(qti.identifier)),
+                   'identifier': unicode(qti.identifier),
+                   'dependencies': qti.dependencies.values()}
+        manifest = execute(renderer, {'context': context})
+        man_path = os.path.join(archive, 'imsmanifest.xml')
+        with open(man_path, 'w') as base:
+            base.write(manifest)
+        zipped = shutil.make_archive(qti.title, 'zip', archive)
+        self.request.response.content_encoding = 'identity'
+        self.request.response.content_type = 'application/zip; charset=UTF-8'
+        self.request.response.content_disposition = 'attachment; filename="%s.zip"' % qti.title
+        self.request.response.body_file = open(zipped, "rb")
+        return self.request.response
+
+    @view_config(context=INTIQuestionSetRef)
+    @view_config(context=INTIAssignmentRef)
+    def _export_ref(self):
+        concrete = find_object_with_ntiid(self.context.target)
+        return self._do_export(concrete)
+
+
+
