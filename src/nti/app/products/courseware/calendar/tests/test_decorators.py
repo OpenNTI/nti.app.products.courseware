90f4b3bf855fa75433d32e20d4b091af55279bf6
Alex Yang <alex.yang@nextthought.com>
2018-11-28 10:52:00 -0600

    add create_calendar_event link on the ICourseCalendar

--- src/nti/app/products/courseware/calendar/tests/test_decorators.py
+++ src/nti/app/products/courseware/calendar/tests/test_decorators.py
@@ -17,6 +17,9 @@ from zope import component
 from zope import interface
 
 from nti.app.products.courseware.calendar.decorators import _CourseCalendarLinkDecorator
+from nti.app.products.courseware.calendar.decorators import _CourseCalendarEventCreationLinkDecorator
+
+from nti.app.products.courseware.calendar.interfaces import ICourseCalendar
 
 from nti.app.testing.application_webtest import ApplicationLayerTest
 
@@ -45,4 +48,14 @@ class TestDecorators(ApplicationLayerTest):
         mock_dataserver.current_transaction.add(course)
         external = self._decorate(_CourseCalendarLinkDecorator, course)
         links = [x.rel for x in external['Links'] if x.rel == 'CourseCalendar']
+        assert_that(links, has_length(1))
+
+    @WithMockDSTrans
+    @fudge.patch('nti.app.products.courseware.calendar.decorators.has_permission')
+    def test_course_calendar_event_creation_link_decorator(self, mock_has_permission):
+        mock_has_permission.is_callable().returns(True)
+        course = ContentCourseInstance()
+        mock_dataserver.current_transaction.add(course)
+        external = self._decorate(_CourseCalendarEventCreationLinkDecorator, ICourseCalendar(course))
+        links = [x.rel for x in external['Links'] if x.rel == 'create_calendar_event']
         assert_that(links, has_length(1))
