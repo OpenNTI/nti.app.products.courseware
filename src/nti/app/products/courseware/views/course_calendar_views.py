d10d070a9b98e31d3173a18efb73fd9903b7c050
Alex Yang <alex.yang@nextthought.com>
2018-11-29 11:34:17 -0600

    define course calendar event update view

--- src/nti/app/products/courseware/views/course_calendar_views.py
+++ src/nti/app/products/courseware/views/course_calendar_views.py
@@ -16,11 +16,14 @@ from zope.cachedescriptors.property import Lazy
 
 from nti.app.contenttypes.calendar.views import CalendarEventCreationView
 from nti.app.contenttypes.calendar.views import CalendarEventDeletionView
+from nti.app.contenttypes.calendar.views import CalendarEventUpdateView
 
 from nti.app.products.courseware.calendar.interfaces import ICourseCalendar
 from nti.app.products.courseware.calendar.interfaces import ICourseCalendarEvent
 
 from nti.app.products.courseware.resources.utils import get_course_filer
+
+from nti.contenttypes.courses.interfaces import ICourseInstance
 
 from nti.dataserver import authorization as nauth
 
@@ -34,7 +37,21 @@ class CourseCalendarEventCreationView(CalendarEventCreationView):
 
     @Lazy
     def filer(self):
-        return get_course_filer(self.context.__parent__, self.remoteUser)
+        course = ICourseInstance(self.context, None)
+        return get_course_filer(course, self.remoteUser)
+
+
+@view_config(route_name='objects.generic.traversal',
+             renderer='rest',
+             context=ICourseCalendarEvent,
+             request_method='PUT',
+             permission=nauth.ACT_UPDATE)
+class CourseCalendarEventUpdateView(CalendarEventUpdateView):
+
+    @Lazy
+    def filer(self):
+        course = ICourseInstance(self.context, None)
+        return get_course_filer(course, self.remoteUser)
 
 
 @view_config(route_name='objects.generic.traversal',
