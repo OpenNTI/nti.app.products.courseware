49e12ea5f5dd308554633f9206344d8149131dc2
Alex Yang <alex.yang@nextthought.com>
2018-12-03 00:11:17 -0600

    NTI-6827, add webinar calendar event

---
+++ src/nti/app/products/courseware/webinars/calendar.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import datetime
+
+from nti.schema.field import Object
+
+from zope import component
+from zope import interface
+
+from zope.component.hooks import getSite
+
+from zope.intid.interfaces import IIntIds
+
+from nti.zope_catalog.interfaces import INoAutoIndexEver
+
+from nti.app.products.courseware.calendar.model import CourseCalendarEvent
+from nti.app.products.courseware.calendar.interfaces import ICourseCalendarDynamicEvent
+from nti.app.products.courseware.calendar.interfaces import ICourseCalendarDynamicEventProvider
+
+from nti.app.products.courseware.webinars.interfaces import IWebinarAsset
+from nti.app.products.courseware.webinars.interfaces import ICourseWebinarContainer
+
+from nti.app.products.webinar.interfaces import IWebinar
+
+from nti.contentlibrary.indexed_data import get_library_catalog
+
+from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
+
+from nti.contenttypes.courses.utils import get_parent_course
+
+from nti.contenttypes.courses.interfaces import ICourseInstance
+
+from nti.dataserver.interfaces import IUser
+
+from nti.externalization.persistence import NoPickle
+
+from nti.externalization.datastructures import InterfaceObjectIO
+
+from nti.schema.fieldproperty import createDirectFieldProperties
+
+from nti.site.site import get_component_hierarchy_names
+
+
+class IWebinarCalendarEvent(ICourseCalendarDynamicEvent):
+
+    webinar = Object(IWebinar, required=True)
+
+
+@interface.implementer(IWebinarCalendarEvent)
+class WebinarCalendarEvent(CourseCalendarEvent):
+
+    __external_can_create__ = False
+
+    __external_class_name__ = "WebinarCalendarEvent"
+
+    mimeType = mime_type = "application/vnd.nextthought.webinar.webinarcalendarevent"
+
+    createDirectFieldProperties(IWebinarCalendarEvent)
+
+
+class WebinarCalendarEventIO(InterfaceObjectIO):
+
+    _ext_iface_upper_bound = IWebinarCalendarEvent
+
+
+@component.adapter(IUser, ICourseInstance)
+@interface.implementer(ICourseCalendarDynamicEventProvider)
+class WebinarCalendarDynamicEventProvider(object):
+
+    def __init__(self, user, course):
+        self.user = user
+        self.course = course
+
+    def iter_events(self):
+        res = []
+        for webinar in self._webinars(self.user, self.course):
+            for time_session in webinar.times or ():
+                event = WebinarCalendarEvent(title=webinar.subject or webinar.description or u'Webinar',
+                                             description=webinar.description,
+                                             start_time=time_session.startTime,
+                                             end_time=time_session.endTime,
+                                             webinar=webinar)
+                res.append(event)
+        return res
+
+    def _webinars(self, user, course):
+        catalog = get_library_catalog()
+        intids = component.getUtility(IIntIds)
+        sites = get_component_hierarchy_names()
+        course_ntiids = self._get_course_ntiids(course)
+        rs = catalog.search_objects(sites=sites,
+                                    intids=intids,
+                                    container_ntiids=course_ntiids,
+                                    provided=(IWebinarAsset,))
+        return set([x.webinar for x in rs or () if x.webinar])
+
+    def _get_course_ntiids(self, instance):
+        courses = (instance,)
+        parent_course = get_parent_course(instance)
+        # We want to check our parent course for refs
+        # in the outline, if we have shared outlines.
+        if      parent_course != instance \
+            and instance.Outline == parent_course.Outline:
+            courses = (instance, parent_course)
+        return {ICourseCatalogEntry(x).ntiid for x in courses}
+
+
