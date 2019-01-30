0bfb0d138964d232e80ab61527388659a0f9ae2e
Alex Yang <alex.yang@nextthought.com>
2019-01-30 17:52:36 -0600

    add acl for course calendar

---
+++ src/nti/app/products/courseware/calendar/acl.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from zope import component
+from zope import interface
+
+from zope.cachedescriptors.property import Lazy
+
+from nti.app.products.courseware.calendar.interfaces import ICourseCalendarEvent
+
+from nti.contenttypes.courses.interfaces import ICourseInstance
+
+from nti.dataserver.interfaces import ACE_DENY_ALL
+
+from nti.dataserver.interfaces import IACLProvider
+
+from nti.traversal.traversal import find_interface
+
+logger = __import__('logging').getLogger(__name__)
+
+
+@component.adapter(ICourseCalendarEvent)
+@interface.implementer(IACLProvider)
+class CourseCalendarACLProvider(object):
+
+    def __init__(self, context):
+        self.context = context
+
+    @property
+    def __parent__(self):
+        # See comments in nti.dataserver.authorization_acl:has_permission
+        return self.context.__parent__
+
+    @Lazy
+    def __acl__(self):
+        result = []
+        if self.__parent__ is not None:
+            result.extend(IACLProvider(self.__parent__).__acl__)
+
+        result.append(ACE_DENY_ALL)
+        return result
