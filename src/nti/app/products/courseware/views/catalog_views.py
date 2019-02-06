1b6dea08a1a482e74e4dbc25759b3fbfffeeffbe
Josh Zuech <josh.zuech@nextthought.com>
2019-02-06 15:20:06 -0600

    Check batch fields before setting rels

--- src/nti/app/products/courseware/views/catalog_views.py
+++ src/nti/app/products/courseware/views/catalog_views.py
@@ -1078,13 +1078,14 @@ class CourseCollectionView(_AbstractFilteredCourseView,
 
         # Need to manually add our batch rels
         batch_size, batch_start = self._get_batch_size_start()
-        result['BatchPage'] = batch_start // batch_size + 1
-        prev_batch_start, next_batch_start = self._batch_start_tuple(batch_start,
-                                                                     batch_size,
-                                                                     container_count)
+        if batch_size is not None and batch_start is not None:
+            result['BatchPage'] = batch_start // batch_size + 1
+            prev_batch_start, next_batch_start = self._batch_start_tuple(batch_start,
+                                                                         batch_size,
+                                                                         container_count)
 
-        self._set_batch_links(result, result,
-                              next_batch_start, prev_batch_start)
+            self._set_batch_links(result, result,
+                                  next_batch_start, prev_batch_start)
         return result
 
 
