03f65d5130d5a302fd1e7971fe5793156f958dd0
Alex Yang <601474742@qq.com>
2019-03-01 14:47:12 -0600

    Revert "decorate the catalog entry title on ICourseInstance (#247)"
    This reverts commit 1e4dc3d580d813c30e43a892cb8d31ce0644ee0f.

--- src/nti/app/products/courseware/decorators.py
+++ src/nti/app/products/courseware/decorators.py
@@ -1219,13 +1219,3 @@ class AdminCatalogEntryDecorator(AbstractAuthenticatedRequestAwareDecorator):
                     method='PUT',
                     elements=(VIEW_ENROLLMENT_OPTIONS,))
         _links.append(link)
-
-
-@component.adapter(ICourseInstance)
-@interface.implementer(IExternalObjectDecorator)
-class _CourseInstanceDecorator(AbstractRequestAwareDecorator):
-
-    def _do_decorate_external(self, context, result):
-        entry = ICourseCatalogEntry(context, None)
-        if entry is not None and 'title' not in result:
-            result['title'] = entry.title
