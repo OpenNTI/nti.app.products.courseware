ab1ba524b558559e9c1ec534db81960571d9b83b
Josh Zuech <josh.zuech@nextthought.com>
2019-10-16 15:35:13 -0500

    Add dep

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
     install_requires=[
         'setuptools',
         'BTrees',
+        'beautifulsoup4',
         'isodate',
         'nti.app.products.webinar',
         'nti.app.invitations',
