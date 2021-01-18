6b2b0ff9614cc3c4f609184b7824d7ea60585e61
Josh Zuech <josh.zuech@nextthought.com>
2021-01-18 10:07:32 -0600

    Only award badges if tied to current organization

--- src/nti/app/products/courseware/acclaim/subscribers.py
+++ src/nti/app/products/courseware/acclaim/subscribers.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+    #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 .. $Id$
@@ -24,7 +24,6 @@ logger = __import__('logging').getLogger(__name__)
 
 @component.adapter(ICourseInstance, ICourseCompletedEvent)
 def _award_badge_on_course_completion(course, event):
-    # FIXM: Only award badge if tied to our current integration
     container = ICourseAcclaimBadgeContainer(course, None)
     if not container:
         return
@@ -33,7 +32,17 @@ def _award_badge_on_course_completion(course, event):
         return
     entry_ntiid = ICourseCatalogEntry(course).ntiid
     client = IAcclaimClient(integration)
+    current_organization_id = getattr(integration.organization, 'organization_id', None)
+    if not current_organization_id:
+        logger.warn('No organization tied to acclaim integration')
     for badge in container.values():
+        # Only award badge if tied to our current integration
+        if badge.organization_id != current_organization_id:
+            logger.info('Badge not linked to current organization (%s) (badge_org=%s) (current_org=%s)',
+                        badge.template_id,
+                        badge.organization_id,
+                        current_organization_id)
+            continue
         client.award_badge(event.user,
                            badge.template_id,
                            evidence_ntiid=entry_ntiid)
