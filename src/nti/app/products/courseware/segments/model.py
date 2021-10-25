8ee0efb4a95f6db71a442a624136af78529ee34f
Bobby Hagen <bobby.hagen@nextthought.com>
2021-10-25 15:56:52 -0500

    NTI-11207: Allow segment filtersets for all users in any course.

--- src/nti/app/products/courseware/segments/model.py
+++ src/nti/app/products/courseware/segments/model.py
@@ -5,7 +5,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from nti.contenttypes.courses.interfaces import ICourseInstance
 from zope import interface
 
 from zope.cachedescriptors.property import Lazy
@@ -17,14 +16,13 @@ from nti.app.products.courseware.segments.interfaces import ICourseMembershipFil
 
 from nti.contenttypes.courses import get_enrollment_catalog
 
-from nti.contenttypes.courses.index import IX_COURSE
 from nti.contenttypes.courses.index import IX_SCOPE
-from nti.contenttypes.courses.index import IX_SITE
 from nti.contenttypes.courses.index import IX_STUDENT
 
-from nti.contenttypes.courses.interfaces import ENROLLMENT_SCOPE_NAMES
-
+from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
+
+from nti.contenttypes.courses.utils import get_enrollments_query
 
 from nti.coremetadata.interfaces import IX_USERNAME
 
@@ -60,17 +58,16 @@ class CourseMembershipFilterSet(SchemaConfigured,
 
     @property
     def enrolled_intids(self):
-        course = find_object_with_ntiid(self.course_ntiid)
-        course = ICourseInstance(course, None)
+        scopes = entry_ntiids = None
+        if self.course_ntiid:
+            course = find_object_with_ntiid(self.course_ntiid)
+            course = ICourseInstance(course, None)
+            catalog_entry = ICourseCatalogEntry(course)
+            entry_ntiids = (catalog_entry.ntiid,)
 
-        result = self.enrollment_catalog.family.IF.Set()
-        if course is None:
-            return result
-
-        # In case it hasn't been initialized already
-        course.SharingScopes.initScopes()
-        scopes = course.SharingScopes.keys()
-        catalog_entry = ICourseCatalogEntry(course)
+            # In case it hasn't been initialized already
+            course.SharingScopes.initScopes()
+            scopes = course.SharingScopes.keys()
 
         # TODO: Do we want to be more restrictive than this?  E.g. only
         #  operate on the current site, or the site the "parent" segment is
@@ -78,12 +75,16 @@ class CourseMembershipFilterSet(SchemaConfigured,
         #  this be a part of the definition?  To some
         #  degree this is currently already enforced at the view level.
         site_names = get_component_hierarchy_names()
-        query = {
-            IX_SITE: {'any_of': site_names},
-            IX_COURSE: {'any_of': (catalog_entry.ntiid,)},
-            IX_SCOPE: {'any_of': scopes}
-        }
+        query = get_enrollments_query(catalog=self.enrollment_catalog,
+                                      site_names=site_names,
+                                      entry_ntiids=entry_ntiids)
 
+        # If a course was provided and, hence, we have the scopes available in
+        # that course, then we can restrict the query to only those scopes
+        if scopes is not None:
+            query[IX_SCOPE] = {'any_of': scopes}
+
+        result = self.enrollment_catalog.family.IF.Set()
         for intid in self.enrollment_catalog.apply(query):
             usernames = self.enrollment_catalog[IX_STUDENT].documents_to_values.get(intid)
             for username in usernames or ():
--- src/nti/app/products/courseware/segments/tests/test_models.py
+++ src/nti/app/products/courseware/segments/tests/test_models.py
@@ -7,12 +7,23 @@ from __future__ import print_function
 
 from unittest import TestCase
 
+import fudge
 from hamcrest import all_of
 from hamcrest import assert_that
 from hamcrest import has_entries
+from hamcrest import has_length
 from hamcrest import has_properties
 from hamcrest import is_
 from hamcrest import not_none
+from nti.contenttypes.courses.sharing import CourseInstanceSharingScopes
+
+from zope import component
+
+from zope.component.hooks import getSite
+
+from zope.lifecycleevent import modified
+
+from zope.schema.vocabulary import SimpleVocabulary
 
 from nti.app.products.courseware.segments.interfaces import ENROLLED_IN
 from nti.app.products.courseware.segments.interfaces import ICourseMembershipFilterSet
@@ -22,6 +33,24 @@ from nti.app.products.courseware.segments.tests import SharedConfiguringTestLaye
 
 from nti.app.products.courseware.segments.model import CourseMembershipFilterSet
 
+from nti.app.products.courseware.tests import PersistentInstructedCourseApplicationTestLayer
+
+from nti.app.segments.tests.test_views import SegmentManagementTest
+
+from nti.app.testing.decorators import WithSharedApplicationMockDS
+
+from nti.app.users.utils import set_user_creation_site
+
+from nti.contenttypes.courses.interfaces import ENROLLMENT_SCOPE_VOCABULARY
+from nti.contenttypes.courses.interfaces import ICourseCatalog
+from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
+from nti.contenttypes.courses.interfaces import ICourseEnrollmentManager
+
+from nti.dataserver.tests import mock_dataserver
+
+from nti.dataserver.users import User
+
+from nti.externalization import to_external_object
 from nti.externalization import update_from_external_object
 
 from nti.externalization.internalization import find_factory_for
@@ -72,3 +101,195 @@ class TestCourseMembershipFilterSet(TestCase):
                         "course_ntiid": course_ntiid,
                         "operator": ENROLLED_IN,
                     }))))
+
+
+class TestApplyCourseMembershipFilterSet(SegmentManagementTest):
+
+    layer = PersistentInstructedCourseApplicationTestLayer
+
+    default_origin = 'http://platform.ou.edu'
+
+    def _create_and_enroll(self, username, section=None, scope=None):
+        with mock_dataserver.mock_db_trans():
+            if not User.get_user(username):
+                self._create_user(username)
+
+        with mock_dataserver.mock_db_trans(site_name='platform.ou.edu'):
+            user = User.get_user(username)
+            set_user_creation_site(user, getSite())
+            modified(user)
+
+            cat = component.getUtility(ICourseCatalog)
+            course = cat['Fall2015']['CS 1323']
+            if section:
+                course = course.SubInstances[section]
+            manager = ICourseEnrollmentManager(course)
+            scope = scope or 'ForCreditDegree'
+            manager.enroll(user, scope=scope)
+
+    def get_entry_ntiid(self, section=None):
+        with mock_dataserver.mock_db_trans(site_name='platform.ou.edu'):
+            cat = component.getUtility(ICourseCatalog)
+            course = cat['Fall2015']['CS 1323']
+            if section:
+                course = course.SubInstances[section]
+
+            return ICourseCatalogEntry(course).ntiid
+
+    @WithSharedApplicationMockDS(users=('parent_user',
+                                        'section_user1',
+                                        'section_user2',
+                                        'non_enrolled_user'),
+                                 testapp=True,
+                                 default_authenticate=True)
+    def test_any_course(self):
+        parent_user = u'parent_user'
+        section_user1 = u'section_user1'
+        section_user2 = u'section_user2'
+        non_enrolled_user = 'non_enrolled_user'
+
+        self.init_users(non_enrolled_user, parent_user, section_user1, section_user2)
+
+        enrolled_members_url, not_enrolled_members_url = self._create_initial_course_membership_segments()
+
+        res = self.testapp.get(not_enrolled_members_url).json_body
+        assert_that(res['Items'], has_length(4))
+
+        # Enroll some of the users
+        self._create_and_enroll(parent_user)
+        self._create_and_enroll(section_user1, section='010', scope='Public')
+        self._create_and_enroll(section_user2, section='995', scope='Purchased')
+
+        res = self.testapp.get(enrolled_members_url).json_body
+        assert_that(res['Items'], has_length(3))
+
+        res = self.testapp.get(not_enrolled_members_url).json_body
+        assert_that(res['Items'], has_length(1))
+
+    @WithSharedApplicationMockDS(users=('parent_user',
+                                        'section_user1',
+                                        'section_user2',
+                                        'non_enrolled_user'),
+                                 testapp=True,
+                                 default_authenticate=True)
+    def test_single_course(self):
+        parent_user = u'parent_user'
+        section_user1 = u'section_user1'
+        section_user2 = u'section_user2'
+        non_enrolled_user = 'non_enrolled_user'
+
+        self.init_users(non_enrolled_user, parent_user, section_user1, section_user2)
+
+        entry_ntiid = self.get_entry_ntiid(section='010')
+        enrolled_members_url, not_enrolled_members_url = \
+            self._create_initial_course_membership_segments(entry_ntiid=entry_ntiid)
+
+        res = self.testapp.get(not_enrolled_members_url).json_body
+        assert_that(res['Items'], has_length(4))
+
+        # Enroll some of the users
+        self._create_and_enroll(parent_user)
+        self._create_and_enroll(section_user1, section='010', scope='Public')
+        self._create_and_enroll(section_user2, section='995', scope='Purchased')
+
+        res = self.testapp.get(enrolled_members_url).json_body
+        assert_that(res['Items'], has_length(1))
+
+        res = self.testapp.get(not_enrolled_members_url).json_body
+        assert_that(res['Items'], has_length(3))
+
+    @WithSharedApplicationMockDS(users=('enrolled_user',
+                                        'unenrolled_user'),
+                                 testapp=True,
+                                 default_authenticate=True)
+    def test_course_scopes(self):
+        enrolled_user = u'enrolled_user'
+        unenrolled_learner = u'unenrolled_user'
+        self.init_users(enrolled_user, unenrolled_learner)
+
+        enrolled_members_url, not_enrolled_members_url = \
+            self._create_initial_course_membership_segments(self.get_entry_ntiid())
+
+        all_enrolled_members_url, all_not_enrolled_members_url = \
+            self._create_initial_course_membership_segments()
+
+        res = self.testapp.get(not_enrolled_members_url).json_body
+        assert_that(res['Items'], has_length(2))
+
+        self._create_and_enroll(enrolled_user, scope='Public')
+
+        res = self.testapp.get(enrolled_members_url).json_body
+        assert_that(res['Items'], has_length(1))
+
+        res = self.testapp.get(not_enrolled_members_url).json_body
+        assert_that(res['Items'], has_length(1))
+
+        # Remove test scope from the course
+        with mock_dataserver.mock_db_trans(site_name='platform.ou.edu'):
+            cat = component.getUtility(ICourseCatalog)
+            course = cat['Fall2015']['CS 1323']
+            del course.SharingScopes['Public']
+
+        # Have to get creative here, b/c CourseInstanceSharingScopes
+        # requires any new items to be in ENROLLMENT_SCOPE_VOCABULARY,
+        # restricting additions. Further, removals are restricted by our
+        # need to call initScopes() prior to filter set evaluation, to
+        # ensure they've been initialized at all, and this repopulates any
+        # missing from that vocab.
+        def vocab_without_public(_self):
+            return SimpleVocabulary(
+                [term for term in ENROLLMENT_SCOPE_VOCABULARY
+                 if term.token != 'Public'])
+
+        with fudge.patched_context(CourseInstanceSharingScopes,
+                                   '_vocabulary',
+                                   vocab_without_public):
+
+            res = self.testapp.get(enrolled_members_url).json_body
+            assert_that(res['Items'], has_length(0))
+
+            res = self.testapp.get(not_enrolled_members_url).json_body
+            assert_that(res['Items'], has_length(2))
+
+            res = self.testapp.get(all_enrolled_members_url).json_body
+            assert_that(res['Items'], has_length(1))
+
+            res = self.testapp.get(all_not_enrolled_members_url).json_body
+            assert_that(res['Items'], has_length(1))
+
+    def _create_initial_course_membership_segments(self, entry_ntiid=None):
+        enrolled_members_url = self._create_course_membership_segment(
+            'Enrolled in Any Course',
+            entry_ntiid=entry_ntiid
+        )
+
+        not_enrolled_members_url = self._create_course_membership_segment(
+            'Not Enrolled in Any Course',
+            operator='not enrolled in',
+            entry_ntiid=entry_ntiid
+        )
+
+        # No one enrolled yet
+        res = self.testapp.get(enrolled_members_url).json_body
+        assert_that(res['Items'], has_length(0))
+
+        return enrolled_members_url, not_enrolled_members_url
+
+    def _create_course_membership_segment(self,
+                                          title,
+                                          operator='enrolled in',
+                                          entry_ntiid=None):
+        simple_filter_set = CourseMembershipFilterSet(course_ntiid=entry_ntiid,
+                                                      operator=operator)
+        simple_filter_set = to_external_object(simple_filter_set)
+        segment = self._create_segment(title,
+                                       simple_filter_set=simple_filter_set).json_body
+
+        return self.require_link_href_with_rel(segment, 'members')
+
+    def init_users(self, *users):
+        with mock_dataserver.mock_db_trans(site_name='platform.ou.edu'):
+            for username in users:
+                user = self._get_user(username)
+                set_user_creation_site(user, getSite())
+                modified(user)
