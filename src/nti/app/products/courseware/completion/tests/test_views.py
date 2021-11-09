67152e7352ff5b1bb9b1ccdf9e2496fec2b4ada7
Jack Moon <jackmoon@Jacks-MacBook-Pro.local>
2021-11-09 11:19:49 -0600

    Relocated TestAdminAwardedCompletedViews from nti.app.contenttypes.completion to here for dependency purposes

--- src/nti/app/products/courseware/completion/tests/test_views.py
+++ src/nti/app/products/courseware/completion/tests/test_views.py
@@ -15,12 +15,10 @@ from hamcrest import is_
 from hamcrest import is_not
 from hamcrest import raises
 from hamcrest import calling
-from hamcrest import not_none
+from hamcrest import has_key
 from hamcrest import has_entry
 from hamcrest import has_length
 from hamcrest import assert_that
-from hamcrest import has_property
-from hamcrest import contains_string
 does_not = is_not
 
 import fudge
@@ -32,21 +30,36 @@ from zope import interface
 
 from zope.component.hooks import getSite
 
-from zope.event import notify
-
 from zope.schema.interfaces import ConstraintNotSatisfied
 
 from zope.security.interfaces import IPrincipal
 
 from zope.securitypolicy.interfaces import IPrincipalRoleManager
 
+from nti.app.contenttypes.completion import AWARDED_COMPLETED_ITEMS_PATH_NAME
+
+from nti.app.contenttypes.completion.tests.models import PersistentCompletableItem
+
+from nti.app.products.courseware_admin import VIEW_COURSE_ROLES
+
 from nti.app.products.courseware import VIEW_CERTIFICATE
+
+from nti.app.products.courseware.completion.views import _html_as_rml_fragments
 
 from nti.app.products.courseware.tests import PersistentInstructedCourseApplicationTestLayer
 from nti.app.products.courseware.tests import SharedConfiguringTestLayer
 
+from nti.app.testing.application_webtest import ApplicationLayerTest
+
+from nti.app.testing.decorators import WithSharedApplicationMockDS
+
+from nti.app.users.utils import set_user_creation_site
+
 from nti.contenttypes.completion.completion import CompletedItem
 
+from nti.contenttypes.completion.interfaces import ICompletionContext
+from nti.contenttypes.completion.interfaces import IAwardedCompletedItemContainer
+from nti.contenttypes.completion.interfaces import IPrincipalAwardedCompletedItemContainer
 from nti.contenttypes.completion.interfaces import ICompletionContextCompletionPolicy
 from nti.contenttypes.completion.interfaces import ICompletionContextCompletionPolicyContainer
 
@@ -55,14 +68,12 @@ from nti.contenttypes.completion.policies import CompletableItemAggregateComplet
 from nti.contenttypes.completion.tests.test_models import MockCompletableItem
 
 from nti.contenttypes.courses.interfaces import ICourseEnrollmentManager
+from nti.contenttypes.courses.interfaces import ICourseInstance
 
-from nti.app.testing.application_webtest import ApplicationLayerTest
-
-from nti.app.testing.decorators import WithSharedApplicationMockDS
-
-from nti.app.users.utils import set_user_creation_site
+from nti.coremetadata.interfaces import IContained
 
 from nti.dataserver.authorization import ROLE_SITE_ADMIN
+from nti.dataserver.authorization import ROLE_CONTENT_EDITOR
 
 from nti.dataserver.tests import mock_dataserver
 
@@ -70,7 +81,9 @@ from nti.dataserver.users import User
 
 from nti.ntiids.ntiids import find_object_with_ntiid
 
-from nti.app.products.courseware.completion.views import _html_as_rml_fragments
+from nti.ntiids.oids import to_external_ntiid_oid
+
+from nti.traversal.traversal import find_interface
 
 
 @interface.implementer(IPrincipal)
@@ -234,3 +247,166 @@ class TestRichDescriptionToRML(unittest.TestCase):
         html_fragment = '<script>alert();</script><p>look <img/>at this</p>'
         rml = _html_as_rml_fragments(html_fragment)
         assert_that(rml, is_('<para>look at this</para>'))
+        
+
+class TestAdminAwardedCompletedViews(ApplicationLayerTest):
+    
+    layer = PersistentInstructedCourseApplicationTestLayer
+    
+    default_origin = 'http://platform.ou.edu'
+    
+    course_ntiid = u'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2015_CS_1323'
+    
+    @WithSharedApplicationMockDS(testapp=True, users=True)
+    @fudge.patch('nti.contenttypes.completion.policies.CompletableItemAggregateCompletionPolicy.is_complete')
+    def test_awarded_completed_items(self, mock_is_complete):
+        
+        awarded_user_username = u'rocket.raccoon'
+        
+        test_site_admin_username = u'I.Am.Groot'
+        
+        course_admin_username = u'peter.quill'
+        
+        course_editor_username = u'drax.destroyer'
+        
+        # Setup
+        with mock_dataserver.mock_db_trans(self.ds):
+            awarded_user = self._create_user(awarded_user_username)
+            course_admin = self._create_user(course_admin_username)
+            course_editor = self._create_user(course_editor_username)
+            set_user_creation_site(awarded_user, 'platform.ou.edu')
+            set_user_creation_site(course_admin, 'platform.ou.edu')
+            set_user_creation_site(course_editor, 'platform.ou.edu')
+                   
+        with mock_dataserver.mock_db_trans(self.ds, site_name='platform.ou.edu'):
+            entry = find_object_with_ntiid(self.course_ntiid)
+            course = entry.__parent__
+            user = User.get_user(awarded_user_username)
+            enrollment_manager = ICourseEnrollmentManager(course)
+            enrollment_manager.enroll(user)
+            
+            completion_context = find_interface(entry, ICompletionContext)
+            item1 = PersistentCompletableItem('ntiid1')
+            item2 = PersistentCompletableItem('ntiid2')
+            non_contained_item = PersistentCompletableItem('non_contained_item')
+            item1.containerId = 'container_id'
+            item2.containerId = 'container_id'
+            interface.alsoProvides(completion_context, IContained)
+            user = User.get_user(course_admin_username)
+            for item in (item1, item2):
+                user.addContainedObject(item)
+            item_ntiid1 = to_external_ntiid_oid(item1)
+            item1.ntiid = item_ntiid1
+            item_ntiid2 = to_external_ntiid_oid(item2)
+            item2.ntiid = item_ntiid2
+            non_contained_item_ntiid = to_external_ntiid_oid(non_contained_item)
+            non_contained_item.ntiid = non_contained_item_ntiid
+            
+        with mock_dataserver.mock_db_trans(self.ds, site_name='platform.ou.edu'):
+            
+            principal_role_manager = IPrincipalRoleManager(getSite())
+            principal_role_manager.assignRoleToPrincipal(ROLE_SITE_ADMIN.id,
+                                                         test_site_admin_username)
+            principal_role_manager.assignRoleToPrincipal(ROLE_CONTENT_EDITOR.id,
+                                                         course_admin_username)
+            
+            entry = find_object_with_ntiid(self.course_ntiid)
+            course_oid = to_external_ntiid_oid(ICourseInstance(entry))
+        
+        course_admin_environ = self._make_extra_environ(user=course_admin_username)
+        course_editor_environ = self._make_extra_environ(course_editor_username)
+        user_environ = self._make_extra_environ(awarded_user_username)
+        nt_admin_environ = self._make_extra_environ()
+        
+        # Admin links
+        course = self.testapp.get('/dataserver2/Objects/%s' % course_oid)
+        course_ext = course.json_body
+        course_roles_href = self.require_link_href_with_rel(course_ext, VIEW_COURSE_ROLES)
+        
+        data = dict()
+        data['roles'] = roles = dict()
+        roles['instructors'] = list([course_admin_username])
+        roles['editors'] = list([course_editor_username])
+
+        #Set up instructor
+        self.testapp.put_json(course_roles_href, data)
+        
+        def get_enr():
+            res = self.testapp.get('/dataserver2/users/%s/Courses/EnrolledCourses' % awarded_user_username,
+                                   extra_environ=user_environ)
+            res = res.json_body['Items'][0]
+            return res
+        
+        enr_res = get_enr()
+        
+        with mock_dataserver.mock_db_trans(self.ds, site_name='platform.ou.edu'):
+            awarded_user = User.get_user(awarded_user_username)
+            user_awarded_container = component.getMultiAdapter((awarded_user, completion_context),
+                                                       IPrincipalAwardedCompletedItemContainer)
+            course_awarded_container = IAwardedCompletedItemContainer(completion_context)
+            assert_that(user_awarded_container.get_completed_item_count(), is_(0))
+            assert_that(course_awarded_container.get_completed_item_count(item1), is_(0))
+            
+        award_completed_url = self.require_link_href_with_rel(enr_res, AWARDED_COMPLETED_ITEMS_PATH_NAME)
+        data = {'MimeType': 'application/vnd.nextthought.completion.awardedcompleteditem', 'completable_ntiid': item_ntiid1}
+        
+        # Check permissions
+        self.testapp.post_json(award_completed_url, data, extra_environ=user_environ, status=403)
+        self.testapp.post_json(award_completed_url, data, extra_environ=course_editor_environ, status=403)
+        
+        res = self.testapp.post_json(award_completed_url, data, extra_environ=course_admin_environ)
+
+        with mock_dataserver.mock_db_trans(self.ds, site_name='platform.ou.edu'):
+
+            assert_that(res.json_body, not has_key('Item'))
+            assert_that(res.json_body, not has_key('Principal'))
+            assert_that(res.json_body, has_key('awarder'))
+            
+            assert_that(res.json_body['reason'], is_(None))
+            
+            assert_that(user_awarded_container.get_completed_item_count(), is_(1))
+            assert_that(user_awarded_container[item_ntiid1].awarder.username, is_(course_admin_username))
+            assert_that(user_awarded_container[item_ntiid1].Principal.username, is_(awarded_user_username))
+            assert_that(user_awarded_container[item_ntiid1].Item, is_(item1))
+            
+            assert_that(course_awarded_container.get_completed_item_count(item1), is_(1))
+            assert_that(course_awarded_container.get_completed_item_count(item2), is_(0))
+         
+        # Should work even without MimeType explicitly passed in   
+        data = {'completable_ntiid': item_ntiid2, 'reason': 'Good soup'}
+        res = self.testapp.post_json(award_completed_url, data, extra_environ=course_admin_environ)
+        
+        with mock_dataserver.mock_db_trans(self.ds, site_name='platform.ou.edu'):
+            assert_that(res.json_body['reason'], is_('Good soup'))
+            assert_that(user_awarded_container.get_completed_item_count(), is_(2))
+            assert_that(course_awarded_container.get_completed_item_count(item1), is_(1))
+            assert_that(course_awarded_container.get_completed_item_count(item2), is_(1))
+            
+        # POST completable that already exists; test 409 and being able to overwrite
+        data['reason'] = 'Number one'    
+        res = self.testapp.post_json(award_completed_url, data, extra_environ=course_admin_environ, status=409)
+        
+        overwrite_awarded_link = self.require_link_href_with_rel(res.json_body, 'overwrite')
+        res = self.testapp.post_json(overwrite_awarded_link, data, extra_environ=course_admin_environ)
+        
+        with mock_dataserver.mock_db_trans(self.ds, site_name='platform.ou.edu'):
+            assert_that(res.json_body['reason'], is_('Number one'))
+            assert_that(user_awarded_container.get_completed_item_count(), is_(2))
+            assert_that(course_awarded_container.get_completed_item_count(item1), is_(1))
+            assert_that(course_awarded_container.get_completed_item_count(item2), is_(1))
+            
+        # POST with ntiid that doesn't match an item and with item ntiid that doesn't map to completable in course; both should 422
+        data = {'completable_ntiid': non_contained_item_ntiid}
+        self.testapp.post_json(award_completed_url, data, extra_environ=course_admin_environ, status=422)
+        
+        data = {'completable_ntiid': 'not_a_valid_ntiid'}
+        self.testapp.post_json(award_completed_url, data, extra_environ=course_admin_environ, status=422)
+
+        #Test deletion of AwardedCompletedItem
+        delete_awarded_link = self.require_link_href_with_rel(res.json_body, 'edit')
+        self.testapp.delete(delete_awarded_link)
+        
+        with mock_dataserver.mock_db_trans(self.ds, site_name='platform.ou.edu'):
+            assert_that(user_awarded_container.get_completed_item_count(), is_(1))
+            assert_that(course_awarded_container.get_completed_item_count(item1), is_(1))
+            assert_that(course_awarded_container.get_completed_item_count(item2), is_(0))
